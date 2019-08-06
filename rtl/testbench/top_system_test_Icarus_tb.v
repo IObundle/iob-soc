d4bbd5330accf28152ae8aaaafa8b56fc09aa7eb
pedrompt97 <pedro.m.p.teixeira@tecnico.ulisboa.pt>
2019-08-06 17:53:52 +0100

    Cleaned up uart files. Started testbench implementation
    Former-commit-id: 95a3ff424f64dcc703436af25df335f56124e107

--- rtl/testbench/top_system_test_Icarus_tb.v
+++ rtl/testbench/top_system_test_Icarus_tb.v
@@ -32,6 +32,7 @@ module top_system_tb;
    
    reg reset = 1;
 
+     
    initial begin
       //    if ($test$plusargs("vcd")) begin
       //$dumpfile("top_system_test_Icarus.vcd");
@@ -42,10 +43,44 @@ module top_system_tb;
       repeat (300) @(posedge clk_p);
       reset <= 0;
       
-   end
+   end // initial begin
+ 
+   
+   //
+   // RECEIVER PROCESS
+   //
+   
+   // cpu interface
+   reg [31:0]   rx_data_in;
+   reg [31:0]   rx_data_out;
+   reg [3:0]    rx_address;
+   reg          rx_read;
+   reg          rx_write;
+   reg          rx_sel;
+
+   integer      rxbr, rxfr;
+   integer 	i = 0;
+ 	
+   reg [7:0] 	rxread_reg = 8'b0;
+
+
+   time         start, stop;
+   
+ 
+   initial begin
+      //sync
+      repeat (500) @(posedge clk) #1;
+
+      while(rxread_reg != 8'h11) //wait for DC1 ascii code
+	cpu_rxread(`UART_RX, rxread_reg);
+
+      for(i=0; i<;i++)
+	
+   end // rx process
 
    wire [6:0] led;
    wire       ser_tx, ser_rx;
+   wire       tester_tx, tester_rx;       
    wire       trap;
 
    top_system uut (
@@ -64,5 +99,21 @@ module top_system_tb;
    			$finish;
    		end
    	end
+
+   assign tester_rx = ser_tx;
+   assign ser_rx = tester_tx;
+   
+   simpleuart uarttester(
+			 .ser_tx    (tester_tx),
+			 .ser_rx    (tester_rx),
+			 .clk       (clk),
+			 .resetn    (reset),
+			 .address   (),
+			 .sel       (),
+			 .we        (),
+			 .dat_di    (),
+			 .dat_do    ()
+			 );
+   
 
 endmodule
