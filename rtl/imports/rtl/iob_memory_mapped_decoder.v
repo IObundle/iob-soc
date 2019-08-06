61d1b13b9b55fc72903cd118bb34e42c67979816
pedrompt97 <pedro.m.p.teixeira@tecnico.ulisboa.pt>
2019-08-06 10:19:09 +0100

    Separated memory mapped decoder from axi file (assured that there's only one module per file)
    Former-commit-id: 9469ba3d553a578a26303b68a0594a02e50e2f0e

---
+++ rtl/imports/rtl/iob_memory_mapped_decoder.v
@@ -0,0 +1,28 @@
+module iob_memory_mapped_decoder(
+				 input [31:0] 	  mem_addr,
+				 output reg [1:0] s_sel
+				 );
+   
+   //`include "memory_mapped_par.vh"   
+   
+   
+   always @* begin
+      
+      if(mem_addr < 32'h40000000)  // slave_0
+        begin
+           s_sel <= 2'b00;
+        end 
+      else if(mem_addr < 32'h80000000) // slave_2 and 3
+        begin  
+           if (mem_addr ==  32'h70000000 || mem_addr ==  32'h70000004 || mem_addr ==  32'h70000008 || mem_addr ==  32'h7000000C  ) s_sel <= 2'b10;
+           else s_sel <= 2'b11;
+        end         
+      else /*if( (mem_addr>>2) < 2**MEM_ADDR_PAR_3)*/ // slave_1
+        begin  
+           s_sel <= 2'b01; //DDR, starts at 0x80000000 and ends at  0xBFFFFFFC (0xC0000000-0x4) (1 GB)
+        end              
+   end       
+   
+   
+   
+endmodule
