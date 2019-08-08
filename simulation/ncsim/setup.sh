f4cbbb8570526d629119c22337c8df1eefeb902f
pedrompt97 <pedro.m.p.teixeira@tecnico.ulisboa.pt>
2019-08-08 09:41:45 +0100

    Changed submodule reference back to original uart submodule. Changed boot and simulation. Transfer now completes, but has errors
    Former-commit-id: d87a7844f0804bccd4c41c7509fa88fb878c9f71

--- simulation/ncsim/setup.sh
+++ simulation/ncsim/setup.sh
@@ -3,6 +3,6 @@
 source /opt/ic_tools/init/init-incisive1510-hf002
 #source /opt/ic_tools/init/init-edi14_26_hf000
 
-ncvlog $CFLAGS -incdir $INCLUDE_DIR $SRC -define VCD -define ALTERA -define SIM
+ncvlog $CFLAGS -incdir $INCLUDE_DIR $SRC -define ALTERA -define SIM
 ncelab $EFLAGS worklib.system_tb:module
 ncsim  $SFLAGS worklib.system_tb:module
