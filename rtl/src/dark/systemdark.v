c7ca53a157bba2e749d96868fecbe6eeafab1d52
zelopes14 <jlopes@iobundle.com>
2019-08-13 09:44:35 +0100

    new system with darkrv32
    Former-commit-id: 9e78b867b143631811ceee5537180e3460ec407b

--- rtl/src/dark/systemdark.v
+++ rtl/src/dark/systemdark.v
@@ -34,6 +34,7 @@ module system (
                input 	     sys_s_axi_rvalid,
                output 	     sys_s_axi_rready,
                input [31:0]  sys_s_axi_rdata,
+               input [31:0]  sys_s_axi_rinstr,
                input 	     sys_s_axi_rlast
                );
    
@@ -46,50 +47,63 @@ module system (
    
    
    //////////////////////////////////
-   //// wires //////////////////////   
+   //// wires //////////////////////
+   /////////////////////////////////
+			
    ////////////////////////////////
    //////// PicoRV32 
-   ////////////////////////////// 
-   wire [31:0] 		     wire_m_addr;
+   //////////////////////////////
+   wire [31:0] 		     wire_m_daddr;
+   wire [31:0] 		     wire_m_iaddr;
    wire [31:0] 		     wire_m_wdata; 
    wire [3:0] 		     wire_m_wstrb;
    wire [31:0] 		     wire_m_rdata;
+   wire [31:0] 		     wire_m_rinstr;
    wire 		     wire_m_valid;
    wire 		     wire_m_ready;
+
    ////////////////////////////////
    //////// Slave 0
    /////////////////////////////// 
-   wire [31:0] 		     wire_s_addr_0;
+   wire [31:0] 		     wire_s_daddr_0;
+   wire [31:0] 		     wire_s_iaddr_0;
    wire [31:0] 		     wire_s_wdata_0; 
    wire [3:0] 		     wire_s_wstrb_0;
    wire [31:0] 		     wire_s_rdata_0;
+   wire [31:0] 		     wire_s_rinstr_0;
    wire 		     wire_s_valid_0;
    wire 		     wire_s_ready_0;
    ////////////////////////////////
    //////// Slave 1
    /////////////////////////////// 
-   wire [31:0] 		     wire_s_addr_1;
+   wire [31:0] 		     wire_s_daddr_1;
+   wire [31:0] 		     wire_s_iaddr_1;
    wire [31:0] 		     wire_s_wdata_1; 
    wire [3:0] 		     wire_s_wstrb_1;
    wire [31:0] 		     wire_s_rdata_1;
+   wire [31:0] 		     wire_s_rinstr_1;
    wire 		     wire_s_valid_1;
    wire 		     wire_s_ready_1;
    ////////////////////////////////
    //////// Slave 2
    /////////////////////////////// 
-   wire [31:0] 		     wire_s_addr_2;
+   wire [31:0] 		     wire_s_daddr_2;
+   wire [31:0] 		     wire_s_iaddr_2;
    wire [31:0] 		     wire_s_wdata_2; 
    wire [3:0] 		     wire_s_wstrb_2;
    wire [31:0] 		     wire_s_rdata_2;
+   wire [31:0] 		     wire_s_rinstr_2;
    wire 		     wire_s_valid_2;
    wire 		     wire_s_ready_2;
    ////////////////////////////////
    //////// Slave 3
    /////////////////////////////// 
-   wire [31:0] 		     wire_s_addr_3;
+   wire [31:0] 		     wire_s_daddr_3;
+   wire [31:0] 		     wire_s_iaddr_3;
    wire [31:0] 		     wire_s_wdata_3; 
    wire [3:0] 		     wire_s_wstrb_3;
    wire [31:0] 		     wire_s_rdata_3;
+   wire [31:0] 		     wire_s_rinstr_3;
    wire 		     wire_s_valid_3;
    wire 		     wire_s_ready_3;
    /////////////////////////////////////////////
@@ -120,6 +134,7 @@ module system (
    ////////// Soft Reset Controller ////////////
    ////////////////////////////////////////////
    reg 	mem_sel;
+
    reg [4:0] soft_reset;
 
    
@@ -131,25 +146,26 @@ module system (
 	end
       else 
 	begin
-
+	   
 `ifdef CACHE
-           if ((wire_m_addr == 32'hfffffffc) && (buffer_clear))
-`else       
-             if ((wire_m_addr == 32'hfffffffc))
-`endif        
-            
-               begin
-		  mem_sel <= 1'b1;
-		  soft_reset <= {soft_reset[3:0],soft_reset[4]};
-               end
-             else
-               begin
-		  mem_sel <= mem_sel;
-		  soft_reset <= 5'b10000;
-               end
-	end
-   end
+           if ((wire_m_daddr == 32'hfffffffc) && (buffer_clear))
+`else 
+	       if ((wire_m_daddr == 32'hfffffffc))      
+`endif	   
+		 begin   
+		    mem_sel <= 1'b1;
+		    soft_reset <= {soft_reset[3:0],soft_reset[4]};
+		 end
+	       else
+		 begin
+		    mem_sel <= mem_sel;
+		    soft_reset <= 5'b10000;
 
+		 end 
+	end 
+   end 
+   
+   
    assign sys_mem_sel = mem_sel;
    /////////////////////////////////////////////////////////////////////////////////
    ///////////////REMEMBER/////////////////////////////////////////////////////////
@@ -165,25 +181,27 @@ module system (
    //////////////////////////////////////////////////////////////////////
    
    reg processor_resetn;
+
    always @* processor_resetn <= resetn_int && ~(soft_reset[0]);
+
+   wrapper_cpu core0 (
+		      .clk(clk),
+		      .processor_resetn(processor_resetn),
+		      .resetn_int(resetn_int),
+
+		      //cpu
+		      .m_iaddr(wire_m_iaddr),
+		      .m_daddr(wire_m_daddr),
+		      .m_wdata(wire_wdata),
+		      .m_wstrb(wire_m_wstrb),
+		      .m_rdata(wire_m_rdata),
+		      .m_rinstr(wire_m_rinstr),
+		      .m_valid(wire_m_valid),
+		      .m_ready(wire_m_ready),
+		      .trap(trap)
+		      );
    
-   picorv32 #(
-	      .ENABLE_MUL(0),
-	      .ENABLE_DIV(0)
-	      )
-   picorv32_core (
-		           .clk    (clk       ),
-		           .resetn (processor_resetn),
-		           .trap   (trap      ),
-		           //memory interface
-		           .mem_valid     (wire_m_valid),
-		           .mem_instr     (wire_m_instr),
-		           .mem_ready     (wire_m_ready),
-		           .mem_addr      (wire_m_addr ),
-		           .mem_wdata     (wire_m_wdata),
-		           .mem_wstrb     (wire_m_wstrb),
-		           .mem_rdata     (wire_m_rdata)
-		           );
+
    
    
    wire [1:0] slave_sel;
@@ -197,47 +215,57 @@ module system (
 						.clk          (clk),				
 						/////////////////////////////////////
 						//// master interface //////////////
-						///////////////////////////////////
-						.m_addr  (wire_m_addr),
+						///////////////////////////////////					
+						.m_daddr  (wire_m_daddr),
+						.m_iaddr  (wire_m_iaddr),
 						.m_wdata (wire_m_wdata),	       
 						.m_wstrb (wire_m_wstrb),
 						.m_rdata (wire_m_rdata),
+						.m_rinstr (wire_m_rinstr),
 						.m_valid (wire_m_valid),
 						.m_ready (wire_m_ready),
-					        ///////////////////////////////////
+						///////////////////////////////////
 						//// slave 0  interface //////////
 						/////////////////////////////////
-						.s_addr_0  (wire_s_addr_0),
+						.s_daddr_0  (wire_s_daddr_0),
+						.s_iaddr_0  (wire_s_iaddr_0),
 						.s_wdata_0 (wire_s_wdata_0),	       
 						.s_wstrb_0 (wire_s_wstrb_0),
 						.s_rdata_0 (wire_s_rdata_0),
+						.s_rinstr_0 (wire_s_rinstr_0),
 						.s_valid_0 (wire_s_valid_0),
 						.s_ready_0 (wire_s_ready_0),
 						///////////////////////////////////
 						//// slave 1 interface ///////////
 						/////////////////////////////////
-						.s_addr_1  (wire_s_addr_1),
+						.s_daddr_1  (wire_s_daddr_1),
+						.s_iaddr_1  (wire_s_iaddr_1),
 						.s_wdata_1 (wire_s_wdata_1),	       
 						.s_wstrb_1 (wire_s_wstrb_1),
 						.s_rdata_1 (wire_s_rdata_1),
+						.s_rinstr_1 (wire_s_rinstr_1),
 						.s_valid_1 (wire_s_valid_1),
 						.s_ready_1 (wire_s_ready_1),
 						///////////////////////////////////
 						//// slave 2 interface ///////////
 						/////////////////////////////////
-						.s_addr_2  (wire_s_addr_2),
+						.s_daddr_2  (wire_s_daddr_2),
+						.s_iaddr_2  (wire_s_iaddr_2),
 						.s_wdata_2 (wire_s_wdata_2),	       
 						.s_wstrb_2 (wire_s_wstrb_2),
 						.s_rdata_2 (wire_s_rdata_2),
+						.s_rinstr_2 (wire_s_rinstr_2),
 						.s_valid_2 (wire_s_valid_2),
 						.s_ready_2 (wire_s_ready_2),
 						///////////////////////////////////
 						//// slave 3 interface ///////////
 						/////////////////////////////////
-						.s_addr_3  (wire_s_addr_3),
+						.s_daddr_3  (wire_s_daddr_3),
+						.s_iaddr_3  (wire_s_iaddr_3),
 						.s_wdata_3 (wire_s_wdata_3),	       
 						.s_wstrb_3 (wire_s_wstrb_3),
 						.s_rdata_3 (wire_s_rdata_3),
+						.s_rinstr_3 (wire_s_rinstr_3),
 						.s_valid_3 (wire_s_valid_3),
 						.s_ready_3 (wire_s_ready_3)
 						);
@@ -256,7 +284,7 @@ module system (
    			  //data bus
    			  .clk         (clk             ),
    			  .resetn      (resetn_int      ),
-   			  .address     (wire_s_addr_2[4:2]),
+   			  .address     (wire_s_daddr_2[4:2]),
    			  .sel         (wire_s_valid_2    ),	
    			  .we          (|wire_s_wstrb_2   ),
    			  .dat_di      (wire_s_wdata_2    ),
@@ -278,7 +306,7 @@ module system (
      ///////////////////////////////////
      wire       simpleuart_wait;
    wire [31:0] 	simpleuart_reg_div_do, simpleuart_reg_dat_do;
-
+   
    
    picosoc_uart simpleuart (
 			    //serial i/f
@@ -287,29 +315,24 @@ module system (
 			    //data bus
 			    .clk         (clk             ),
 			    .resetn      (resetn_int      ),
-			    //				 .address     (wire_s_addr_2[3:2]),
-			    //				 .sel         (wire_s_valid_2    ),	
-			    //				 .we          (|wire_s_wstrb_2   ),
-			    //				 .dat_di      (wire_s_wdata_2    ),
-			    //				 .dat_do      (wire_s_rdata_2    )
-			    //	                  );
-			    .reg_div_we  ((wire_s_valid_2 && wire_s_addr_2 [2])? wire_s_wstrb_2 : 4'b 0000),
+
+			    .reg_div_we  ((wire_s_valid_2 && wire_s_daddr_2 [2])? wire_s_wstrb_2 : 4'b 0000),
 			    .reg_div_di  (wire_s_wdata_2),
 			    .reg_div_do  (simpleuart_reg_div_do),
       
-			    .reg_dat_we  ((wire_s_valid_2 && wire_s_addr_2 [3])? wire_s_wstrb_2[0] : 1'b 0),
-			    .reg_dat_re  ((wire_s_valid_2 && wire_s_addr_2 [3]) && !wire_s_wstrb_2),
+			    .reg_dat_we  ((wire_s_valid_2 && wire_s_daddr_2 [3])? wire_s_wstrb_2[0] : 1'b 0),
+			    .reg_dat_re  ((wire_s_valid_2 && wire_s_daddr_2 [3]) && !wire_s_wstrb_2),
 			    .reg_dat_di  (wire_s_wdata_2),
 			    .reg_dat_do  (simpleuart_reg_dat_do),
 			    .reg_dat_wait(simpleuart_wait)
 			    );
    
 
-   wire 	simpleuart_reg_div_sel = wire_s_valid_2 && wire_s_addr_2 [2]; // addr = ...0004
-   wire 	simpleuart_reg_dat_sel = wire_s_valid_2 && wire_s_addr_2 [3]; // addr = ...0008
+   wire 	simpleuart_reg_div_sel = wire_s_valid_2 && wire_s_daddr_2 [2]; // addr = ...0004
+   wire 	simpleuart_reg_dat_sel = wire_s_valid_2 && wire_s_daddr_2 [3]; // addr = ...0008
 
 
-   assign wire_s_ready_2 = (wire_s_valid_2 && wire_s_addr_2 [2]) || ((wire_s_valid_2 && wire_s_addr_2 [3]) && !simpleuart_wait);
+   assign wire_s_ready_2 = (wire_s_valid_2 && wire_s_daddr_2 [2]) || ((wire_s_valid_2 && wire_s_daddr_2 [3]) && !simpleuart_wait);
    assign wire_s_rdata_2 = simpleuart_reg_div_sel ? simpleuart_reg_div_do : simpleuart_reg_dat_sel ? simpleuart_reg_dat_do : 32'h 0000_0000;
    //////////////////////////////////
    /////////////////////////////////	       
@@ -330,10 +353,13 @@ module system (
 		  ) auxiliary_memory (
 				      .clk                (clk                                  ),
 				      .main_mem_write_data(wire_s_wdata_3                       ),
-				      .main_mem_addr      (wire_s_addr_3 [MAIN_MEM_ADDR_W-1:2]),
+				      .main_mem_daddr     (wire_s_daddr_3 [MAIN_MEM_ADDR_W-1:2] ),
 				      .main_mem_en        (wire_s_wstrb_3                       ),
-				      .main_mem_read_data (wire_s_rdata_3                       )                       
+				      .main_mem_read_data (wire_s_rdata_3                       ),
+				      .main_mem_iaddr     (wire_s_iaddr_3 [MAIN_MEM_ADDR_W-1:2] ),
+				      .main_mem_read_instr(wire_s_rinstr_3                      )
 				      );
+
 
    
    reg 		aux_mem_ready;
@@ -358,9 +384,9 @@ module system (
 		  ) boot_memory (
 				 .clk            (clk           ),
 				 .boot_write_data(wire_s_wdata_0),
-				 .boot_addr      (wire_s_addr_0 [BOOT_ADDR_W-1:2]),
 				 .boot_en        (wire_s_wstrb_0),
-				 .boot_read_data (wire_s_rdata_0)                            
+				 .boot_addr      (wire_s_iaddr_0 [BOOT_ADDR_W-1:2]),
+				 .boot_read_data (wire_s_rinstr_0)                            
 				 );
    
 
@@ -378,19 +404,19 @@ module system (
    //// Memory cache ///////////////////////////////////////
    ////////////////////////////////////////////////////////
    
-   memory_cache cache (
+   memory_cache D_cache (
 		       .clk                (clk),
 		       .reset              (~processor_resetn),
 		       .buffer_clear       (buffer_clear),
 		       .cache_write_data   (wire_s_wdata_1),
-		       .cache_addr         (wire_s_addr_1 [29:0]),
+		       .cache_addr         (wire_s_daddr_1 [29:0]),
 		       .cache_wstrb        (wire_s_wstrb_1),
 		       .cache_read_data    (wire_s_rdata_1),
 		       .cpu_ack            (wire_s_valid_1),
 		       .cache_ack          (wire_s_ready_1),
  `ifndef AUX_MEM
 		       //Memory Cache controller signals
-		       .cache_controller_address (wire_s_addr_3 [3:2]),
+		       .cache_controller_address (wire_s_daddr_3 [3:2]),
 		       .cache_controller_requested_data (wire_s_rdata_3),
 		       .cache_controller_cpu_request (wire_s_valid_3),
 		       .cache_controller_acknowledge (wire_s_ready_3),	       
@@ -432,6 +458,60 @@ module system (
 			.mem_ack            (sys_s_ready)*/
 		       );
 
+     memory_cache I_cache (
+		       .clk                (clk),
+		       .reset              (~processor_resetn),
+		       .buffer_clear       (buffer_clear),
+		       .cache_write_data   (wire_s_wdata_1),
+		       .cache_addr         (wire_s_iaddr_1 [29:0]),
+		       .cache_wstrb        (wire_s_wstrb_1),
+		       .cache_read_data    (wire_s_rinstr_1),
+		       .cpu_ack            (wire_s_valid_1),
+		       .cache_ack          (wire_s_ready_1),
+ `ifndef AUX_MEM
+		       //Memory Cache controller signals
+		       .cache_controller_address (wire_s_iaddr_3 [3:2]),
+		       .cache_controller_requested_data (wire_s_rinstr_3),
+		       .cache_controller_cpu_request (wire_s_valid_3),
+		       .cache_controller_acknowledge (wire_s_ready_3),	       
+ `else	       
+		       .cache_controller_address (2'b00),
+		       .cache_controller_requested_data (),
+		       .cache_controller_cpu_request (1 'b0),
+		       .cache_controller_acknowledge (),  
+ `endif              
+		       ///// AXI signals
+		       /// Read            
+		       .AR_ADDR            (sys_s_axi_araddr), 
+		       .AR_LEN             (sys_s_axi_arlen),
+		       .AR_SIZE            (sys_s_axi_arsize),
+		       .AR_BURST           (sys_s_axi_arburst),
+		       .AR_VALID           (sys_s_axi_arvalid), 
+		       .AR_READY           (sys_s_axi_arready),
+		       //.R_ADDR             (wire_R_ADDR), 
+		       .R_VALID            (sys_s_axi_rvalid), 
+		       .R_READY            (sys_s_axi_rready),
+		       .R_DATA             (sys_s_axi_rinstr),
+		       .R_LAST             (sys_s_axi_rlast),            
+		       /// Write
+		       .AW_ADDR            (sys_s_axi_awaddr),
+		       .AW_VALID           (sys_s_axi_awvalid),
+		       .AW_READY           (sys_s_axi_awready), 
+		       //.W_ADDR             (wire_W_ADDR),
+		       .W_VALID            (sys_s_axi_wvalid),
+		       .W_STRB             (sys_s_axi_wstrb), 
+		       .W_READY            (sys_s_axi_wready),
+		       .W_DATA             (sys_s_axi_wdata),
+		       .B_VALID            (sys_s_axi_bvalid),
+		       .B_READY            (sys_s_axi_bready)
+		       /*.mem_write_data     (sys_s_wdata),
+			.mem_wstrb          (sys_s_wstrb),
+			.mem_read_data      (sys_s_rdata),
+			.mem_addr           (sys_s_addr),
+			.mem_valid          (sys_s_valid),
+			.mem_ack            (sys_s_ready)*/
+		       );
+
 `else
    //////////////////////////////////////////////////////////
      //// Open RAM ///////////////////////////////////////////
@@ -442,9 +522,11 @@ module system (
 		  ) main_memory (
 				 .clk                (clk                                  ),
 				 .main_mem_write_data(wire_s_wdata_1                       ),
-				 .main_mem_addr      (wire_s_addr_1 [MAIN_MEM_ADDR_W-1:2]),
+				 .main_mem_daddr     (wire_s_daddr_1 [MAIN_MEM_ADDR_W-1:2] ),
 				 .main_mem_en        (wire_s_wstrb_1                       ),
-				 .main_mem_read_data (wire_s_rdata_1                       )                       
+				 .main_mem_read_data (wire_s_rdata_1                       ),
+				 .main_mem_iaddr     (wire_s_iaddr_1 [MAIN_MEM_ADDR_W-1:2] ),
+				 .main_mem_read_instr(wire_s_rinstr_1                      )
 				 );
 
    
