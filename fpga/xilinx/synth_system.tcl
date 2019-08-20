827de2a2d8611a8b1ed1234283d26aaf44f1ed37
pedrompt97 <pedro.m.p.teixeira@tecnico.ulisboa.pt>
2019-08-20 15:11:58 +0100

    Removed unused and redundant files, modified tcl to include cache and bram IP

--- fpga/xilinx/synth_system.tcl
+++ fpga/xilinx/synth_system.tcl
@@ -1,10 +1,19 @@
+#include
 read_verilog ../../rtl/include/system.vh
 read_verilog ../../submodules/iob-uart/rtl/include/iob-uart.vh
+
+#system
 read_verilog ../../rtl/src/top_system.v
 read_verilog ../../rtl/src/system.v
+read_verilog ../../rtl/src/iob_generic_interconnect.v
+
+#picorV 
 read_verilog ../../submodules/iob-rv32/picorv32.v
-read_verilog ../../rtl/src/iob_generic_interconnect.v 
+
+#uart
 read_verilog ../../submodules/iob-uart/rtl/src/iob-uart.v
+
+#memory
 read_verilog ../../rtl/src/memory/main_memory.v
 read_verilog ../../rtl/src/memory/ddr_memory.v
 read_verilog ../../rtl/src/memory/xalt_1p_mem_no_initialization.v
@@ -12,6 +21,17 @@ read_verilog ../../submodules/iob-rv32/rtl/xalt_1p_mem.v
 read_verilog ../../rtl/src/memory/boot_memory.v
 read_verilog ../../rtl/src/clock/clock_wizard.v
 read_verilog ../../rtl/src/iob_native_memory_mapped_decoder.v
+
+#cache
+read_verilog ../../submodules/iob-cache/rtl/src/cache/afifo.v
+read_verilog ../../submodules/iob-cache/rtl/src/cache/data_memory.v
+read_verilog ../../submodules/iob-cache/rtl/src/cache/memory_cache_v2.v
+read_verilog ../../submodules/iob-cache/rtl/src/cache/tag_memory.v
+read_verilog ../../submodules/iob-cache/rtl/src/cache/valid_memory.v
+read_verilog ../../submodules/iob-cache/rtl/src/cache/write_buffer.v
+read_verilog ../../submodules/iob-cache/rtl/src/cache/xalt_1p_mem_no_initialization_with_reset.v
+
+read_ip -quiet ../../rtl/ip/bram_axi.xcix
 
 read_xdc ./synth_system.xdc
 
--- rtl/include/system.vh
+++ rtl/include/system.vh
@@ -1,8 +1,8 @@
 `define XILINX
-//`define CACHE
+`define CACHE
 //`define DDR //Using the DDR requires CACHE
 //`define CLK_200MHZ //Only when not using DDR, comment this to usea 100 MHz clock
-//`define AUX_MEM //Required the 100 MHZ clock (and not using DDR) to use this memory
+`define AUX_MEM //Required the 100 MHZ clock (and not using DDR) to use this memory
 //`define PICOSOC_UART
 
 `define MAIN_MEM_BASE 2'd1
