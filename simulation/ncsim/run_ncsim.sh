6cc06125b44c4310bc28c5f2ed001abade5a85c9
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2019-11-26 17:18:12 +0000

    ncim problems fixed and testbench simplified
    Former-commit-id: a6c8c06a6c9741c12383f3d0699d0b8d8910f0d0

--- simulation/ncsim/run_ncsim.sh
+++ simulation/ncsim/run_ncsim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /opt/ic_tools/init/init-incisive1510-hf002
-ncvlog $CFLAGS $SRC -define VCD
+ncvlog $CFLAGS $SRC 
 ncelab $EFLAGS worklib.system_tb:module
 ncsim  $SFLAGS worklib.system_tb:module
