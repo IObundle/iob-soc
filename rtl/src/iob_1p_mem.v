d09c96c2eb5f0efbf0af0c540f30aa96df5a7e77
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2019-12-17 23:49:20 +0000

    fix sp605 fpga
    Former-commit-id: 122abbb1ed7a5d485724d7c9b44e4633fca2912f

--- rtl/src/iob_1p_mem.v
+++ rtl/src/iob_1p_mem.v
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
 
 module iob_1p_mem #(
-		     parameter [20*8-1:0] MEM_INIT_FILE={20*8{1'b0}},
-		     parameter DATA_W=8,
-		     parameter ADDR_W=14
-		     )
+		    parameter MEM_INIT_FILE=0,
+		    parameter DATA_W=8,
+		    parameter ADDR_W=14
+		    )
    (
     input                     clk,
     input                     en, 
@@ -15,7 +15,7 @@ module iob_1p_mem #(
     );
 
    //this allows ISE 14.7 to work; do not remove
-   parameter [20*8-1:0] mem_init_file_int = MEM_INIT_FILE;
+   parameter mem_init_file_int = MEM_INIT_FILE;
 
    // Declare the RAM
    reg [DATA_W-1:0] 	      ram[2**ADDR_W-1:0];
