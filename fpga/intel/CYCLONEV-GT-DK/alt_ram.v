6555e543948dce69c880c598f6979d786ecc1062
P_Miranda <pedro_miranda2@hotmail.com>
2020-04-15 23:39:22 +0100

    updated iob-mem submodule and respective iob_tdp_mem module instantiation, to use without USE_BOOT requires uncommenting tdp line to initialize RAM
    Former-commit-id: 84fa52cae87664e9ab7e13b96e7521d19d41a4d9

--- fpga/intel/CYCLONEV-GT-DK/alt_ram.v
+++ fpga/intel/CYCLONEV-GT-DK/alt_ram.v
@@ -26,93 +26,86 @@ module ram #(
    // byte memories
    // byte 0
    parameter file_name_0 = (FILE == "none")? "none": {FILE, "_0", ".dat"};
-   bytewrite_tdp_ram_rf  #(
-                  .FILE(file_name_0),
-                  .NUM_COL(1),
-		  .COL_WIDTH(8),
-                  .ADDR_WIDTH(ADDR_W))
+   iob_t2p_mem  #(
+                  .MEM_INIT_FILE(file_name_0),
+		  .DATA_W(8),
+                  .ADDR_W(ADDR_W))
    main_mem_byte0
      (
-      .clkA           (clk),
-      .enaA            (valid),
-      .weA            (wstrb[0]),
-      .addrA          (addr),
-      .doutA      (rdata[7:0]),
-      .dinA       (wdata[7:0]),
-      .clkB           (clk),
-      .enaB             (i_en[0]),
-      .addrB          (i_addr),
-      .weB            (1'b0),
-      .dinB           (wdata[7:0]),
-      .doutB          (i_data[7:0])
+      .clk           (clk),
+      .en_a            (valid),
+      .we_a            (wstrb[0]),
+      .addr_a          (addr),
+      .q_a      (rdata[7:0]),
+      .data_a       (wdata[7:0]),
+      .en_b             (i_en[0]),
+      .addr_b          (i_addr),
+      .we_b            (1'b0),
+      .data_b           (wdata[7:0]),
+      .q_b          (i_data[7:0])
       );
 
    //byte 1
    parameter file_name_1 = (FILE == "none")? "none": {FILE, "_1", ".dat"};
-   bytewrite_tdp_ram_rf  #(
-                  .FILE(file_name_1),
-                  .NUM_COL(1),
-		  .COL_WIDTH(8),
-                  .ADDR_WIDTH(ADDR_W))
+   iob_t2p_mem  #(
+                  .MEM_INIT_FILE(file_name_1),
+		  .DATA_W(8),
+                  .ADDR_W(ADDR_W))
    main_mem_byte1
      (
-      .clkA           (clk),
-      .enaA            (valid),
-      .weA            (wstrb[1]),
-      .addrA          (addr),
-      .doutA      (rdata[15:8]),
-      .dinA       (wdata[15:8]),
-      .clkB           (clk),
-      .enaB             (i_en[1]),
-      .addrB          (i_addr),
-      .weB            (1'b0),
-      .dinB           (wdata[15:8]),
-      .doutB          (i_data[15:8])
+      .clk           (clk),
+      .en_a            (valid),
+      .we_a            (wstrb[1]),
+      .addr_a          (addr),
+      .q_a      (rdata[15:8]),
+      .data_a       (wdata[15:8]),
+      .en_b             (i_en[1]),
+      .addr_b          (i_addr),
+      .we_b            (1'b0),
+      .data_b           (wdata[15:8]),
+      .q_b          (i_data[15:8])
       );
    
    // byte 2
    parameter file_name_2 = (FILE == "none")? "none": {FILE, "_2", ".dat"};
-   bytewrite_tdp_ram_rf  #(
-                  .FILE(file_name_2),
-                  .NUM_COL(1),
-		  .COL_WIDTH(8),
-                  .ADDR_WIDTH(ADDR_W))
+   iob_t2p_mem  #(
+                  .MEM_INIT_FILE(file_name_2),
+		  .DATA_W(8),
+                  .ADDR_W(ADDR_W))
    main_mem_byte2
      (
-      .clkA           (clk),
-      .enaA            (valid),
-      .weA            (wstrb[2]),
-      .addrA          (addr),
-      .doutA      (rdata[23:16]),
-      .dinA       (wdata[23:16]),
-      .clkB           (clk),
-      .enaB             (i_en[2]),
-      .addrB          (i_addr),
-      .weB            (1'b0),
-      .dinB           (wdata[23:16]),
-      .doutB          (i_data[23:16])
+      .clk           (clk),
+      .en_a            (valid),
+      .we_a            (wstrb[2]),
+      .addr_a          (addr),
+      .q_a      (rdata[23:16]),
+      .data_a       (wdata[23:16]),
+      .en_b             (i_en[2]),
+      .addr_b          (i_addr),
+      .we_b            (1'b0),
+      .data_b           (wdata[23:16]),
+      .q_b          (i_data[23:16])
       );
+   
    //byte 3
    parameter file_name_3 = (FILE == "none")? "none": {FILE, "_3", ".dat"};
-   bytewrite_tdp_ram_rf  #(
-                  .FILE(file_name_3),
-                  .NUM_COL(1),
-		  .COL_WIDTH(8),
-                  .ADDR_WIDTH(ADDR_W))
+   iob_t2p_mem  #(
+                  .MEM_INIT_FILE(file_name_3),
+		  .DATA_W(8),
+                  .ADDR_W(ADDR_W))
    main_mem_byte3
      (
-      .clkA           (clk),
-      .enaA            (valid),
-      .weA            (wstrb[3]),
-      .addrA          (addr),
-      .doutA      (rdata[31:24]),
-      .dinA       (wdata[31:24]),
-      .clkB           (clk),
-      .enaB             (i_en[3]),
-      .addrB          (i_addr),
-      .weB            (1'b0),
-      .dinB           (wdata[31:24]),
-      .doutB          (i_data[31:24])
+      .clk           (clk),
+      .en_a            (valid),
+      .we_a            (wstrb[3]),
+      .addr_a          (addr),
+      .q_a      (rdata[31:24]),
+      .data_a       (wdata[31:24]),
+      .en_b             (i_en[3]),
+      .addr_b          (i_addr),
+      .we_b            (1'b0),
+      .data_b           (wdata[31:24]),
+      .q_b          (i_data[31:24])
       );
 
    //reply with ready 
