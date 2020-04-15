4ccca7015c9dd3cae700c22141712e4ab45698df
P_Miranda <pedro_miranda2@hotmail.com>
2020-04-15 22:53:17 +0100

    int_mem uses alt_ram, which is composed of true dual port memories, B ports are not being used still, testbench works correctly
    Former-commit-id: 4d10c8136a6fb012264ac110f8dbf457c8a87eab

--- fpga/intel/CYCLONEV-GT-DK/alt_ram.v
+++ fpga/intel/CYCLONEV-GT-DK/alt_ram.v
@@ -18,68 +18,97 @@ module ram #(
           output reg           ready
 	  );
    
-
    // byte memories
    // byte 0
    parameter file_name_0 = (FILE == "none")? "none": {FILE, "_0", ".dat"};
-   iob_1p_mem  #(
+   bytewrite_tdp_ram_rf  #(
                   .FILE(file_name_0),
-                  .DATA_W(8),
-                  .ADDR_W(ADDR_W))
+                  .NUM_COL(1),
+		  .COL_WIDTH(8),
+                  .ADDR_WIDTH(ADDR_W))
    main_mem_byte0
      (
-      .clk           (clk),
-      .en            (valid),
-      .we            (wstrb[0]),
-      .addr          (addr),
-      .data_out      (rdata[7:0]),
-      .data_in       (wdata[7:0])
+      .clkA           (clk),
+      .enaA            (valid),
+      .weA            (wstrb[0]),
+      .addrA          (addr),
+      .doutA      (rdata[7:0]),
+      .dinA       (wdata[7:0]),
+      .clkB           (clk),
+      .enaB             (1'b0),
+      .addrB          (addr),
+      .weB            (1'b0),
+      .dinB           (wdata[7:0]),
+      .doutB          ()
       );
 
    //byte 1
    parameter file_name_1 = (FILE == "none")? "none": {FILE, "_1", ".dat"};
-   iob_1p_mem  #(
+   bytewrite_tdp_ram_rf  #(
                   .FILE(file_name_1),
-                  .DATA_W(8),
-                  .ADDR_W(ADDR_W))
-   main_mem_byte1 (
-                   .clk           (clk),
-                   .en            (valid),
-                   .we            (wstrb[1]),
-                   .addr          (addr),
-                   .data_out      (rdata[15:8]),
-                   .data_in       (wdata[15:8])
-                   );
-
+                  .NUM_COL(1),
+		  .COL_WIDTH(8),
+                  .ADDR_WIDTH(ADDR_W))
+   main_mem_byte1
+     (
+      .clkA           (clk),
+      .enaA            (valid),
+      .weA            (wstrb[1]),
+      .addrA          (addr),
+      .doutA      (rdata[15:8]),
+      .dinA       (wdata[15:8]),
+      .clkB           (clk),
+      .enaB             (1'b0),
+      .addrB          (addr),
+      .weB            (1'b0),
+      .dinB           (wdata[15:8]),
+      .doutB          ()
+      );
+   
    // byte 2
    parameter file_name_2 = (FILE == "none")? "none": {FILE, "_2", ".dat"};
-   iob_1p_mem  #(
+   bytewrite_tdp_ram_rf  #(
                   .FILE(file_name_2),
-                  .DATA_W(8),
-                  .ADDR_W(ADDR_W))
-   main_mem_byte2 (
-                   .clk           (clk),
-                   .en            (valid),
-                   .we            (wstrb[2]),
-                   .addr          (addr),
-                   .data_out      (rdata[23:16]),
-                   .data_in       (wdata[23:16])
-                   );
-
+                  .NUM_COL(1),
+		  .COL_WIDTH(8),
+                  .ADDR_WIDTH(ADDR_W))
+   main_mem_byte2
+     (
+      .clkA           (clk),
+      .enaA            (valid),
+      .weA            (wstrb[2]),
+      .addrA          (addr),
+      .doutA      (rdata[23:16]),
+      .dinA       (wdata[23:16]),
+      .clkB           (clk),
+      .enaB             (1'b0),
+      .addrB          (addr),
+      .weB            (1'b0),
+      .dinB           (wdata[23:16]),
+      .doutB          ()
+      );
    //byte 3
    parameter file_name_3 = (FILE == "none")? "none": {FILE, "_3", ".dat"};
-   iob_1p_mem  #(
+   bytewrite_tdp_ram_rf  #(
                   .FILE(file_name_3),
-                  .DATA_W(8),
-                  .ADDR_W(ADDR_W))
-   main_mem_byte3 (
-                   .clk           (clk),
-                   .en            (valid),
-                   .we            (wstrb[3]),
-                   .addr          (addr),
-                   .data_out      (rdata[31:24]),
-                   .data_in       (wdata[31:24])
-                   );
+                  .NUM_COL(1),
+		  .COL_WIDTH(8),
+                  .ADDR_WIDTH(ADDR_W))
+   main_mem_byte3
+     (
+      .clkA           (clk),
+      .enaA            (valid),
+      .weA            (wstrb[3]),
+      .addrA          (addr),
+      .doutA      (rdata[31:24]),
+      .dinA       (wdata[31:24]),
+      .clkB           (clk),
+      .enaB             (1'b0),
+      .addrB          (addr),
+      .weB            (1'b0),
+      .dinB           (wdata[31:24]),
+      .doutB          ()
+      );
 
    //reply with ready 
    always @(posedge clk, posedge rst)
