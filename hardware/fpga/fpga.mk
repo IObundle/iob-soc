bbe18b5b18b9e1688a3b0a488649577013353091
Joao D Lopes <joao.d.lopes91@gmail.com>
2020-06-26 18:13:19 +0100

    fix FPGA framework; update ignore files after new repo organization; fix system.mk; update iob-cache and iob-uart submodules

--- hardware/fpga/fpga.mk
+++ hardware/fpga/fpga.mk
@@ -2,19 +2,6 @@ include $(ROOT_DIR)/hardware/hardware.mk
 
 VSRC+=verilog/top_system.v
 
-
-all: run
-
-firmware.bin: $(FIRM_DIR)/firmware.hex
-	cp $(FIRM_DIR)/firmware.bin .
-
-firmware.dat: $(FIRM_DIR)/firmware.hex
-	cp $< .
-	$(PYTHON_DIR)/hex_split.py firmware
-
-boot.dat: $(BOOT_DIR)/boot.hex
-	cp $< ./boot.dat
-
 ld-sw:
 	cp firmware.bin $(LD_SW_DIR)
 	make -C $(LD_SW_DIR)
