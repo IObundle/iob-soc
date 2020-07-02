f5cea2ade4ea8bd259135f4db20666e9b2eee64d
Chris Edgley <christopher@iobundle.com>
2020-07-02 16:49:57 +0100

    small change to sys.v+latest TIMER repo

--- hardware/src/system.v
+++ hardware/src/system.v
@@ -324,17 +324,16 @@ module system
    // TIMER
    //
 
-   time_counter #(.COUNTER_WIDTH(32))
-           timer (
-		  .clk (clk),
-		  /rst (rst),
+   iob_timer timer (
+		    .clk (clk),
+		    .rst (reset),
 
-		  //cpu interface
-		  .valid(slaves_req[`valid(`TIMER)]),
-		  .address(slaves_req[`address(`TIMER,`TIMER_ADDR_W+2,2)]),
-		  .wdata(slaves_req[`wdata(`TIMER)]),
-		  .rdata(slaves_resp[`rdata(`TIMER)]),
-		  .ready(slaves_resp[`ready(`TIMER)])
-		  );
-			   
+		    //cpu interface
+		    .valid(slaves_req[`valid(`TIMER)]),
+		    .address(slaves_req[`address(`TIMER,`TIMER_ADDR_W+2,2)]),
+		    .wdata(slaves_req[`wdata(`TIMER)]),
+		    .rdata(slaves_resp[`rdata(`TIMER)]),
+		    .ready(slaves_resp[`ready(`TIMER)])
+		    );
+	   
 endmodule
