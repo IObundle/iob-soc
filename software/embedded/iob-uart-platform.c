713d444a5b4fad65d7c5d40490e1a32d88d2980d
Chris Edgley <christopher@iobundle.com>
2020-07-15 18:44:46 +0100

    Macro name MEMSET->IO_SET conformity

--- software/embedded/iob-uart-platform.c
+++ software/embedded/iob-uart-platform.c
@@ -10,47 +10,47 @@ void uart_init(int base_address, int div) {
   base = base_address;
 
   //pulse soft reset 
-  MEMSET(base, UART_SOFT_RESET, 1);
-  MEMSET(base, UART_SOFT_RESET, 0);
+  IO_SET(base, UART_SOFT_RESET, 1);
+  IO_SET(base, UART_SOFT_RESET, 0);
 
   //Set the division factor div
   //div should be equal to round (fclk/baudrate)
   //E.g for fclk = 100 Mhz for a baudrate of 115200 we should uart_setdiv(868)
-  MEMSET(base, UART_DIV, div);
-  MEMSET(base, UART_RXEN, 1);
+  IO_SET(base, UART_DIV, div);
+  IO_SET(base, UART_RXEN, 1);
 }
 
 int uart_getdiv()
 {
-  return (MEMGET(base, UART_DIV));
+  return (IO_GET(base, UART_DIV));
 }
 
 //tx functions
 void uart_txwait() {
-  while(MEMGET(base, UART_WRITE_WAIT));
+  while(IO_GET(base, UART_WRITE_WAIT));
 }
 
 int uart_txstatus() {
-  return (!MEMGET(base, UART_WRITE_WAIT));
+  return (!IO_GET(base, UART_WRITE_WAIT));
 }
 
 inline void uart_putc(char c) {
-  while(MEMGET(base, UART_WRITE_WAIT));
-  MEMSET(base, UART_DATA, (int)c);
+  while(IO_GET(base, UART_WRITE_WAIT));
+  IO_SET(base, UART_DATA, (int)c);
 }
 
 
 void uart_rxwait() {
-  while(!MEMGET(base, UART_READ_VALID));
+  while(!IO_GET(base, UART_READ_VALID));
 }
 
 int uart_rxstatus() {
-  return (MEMGET(base, UART_READ_VALID));
+  return (IO_GET(base, UART_READ_VALID));
 }
 
 inline char uart_getc() {
-  while(!MEMGET(base, UART_READ_VALID));
-  return( (char) MEMGET(base, UART_DATA));
+  while(!IO_GET(base, UART_READ_VALID));
+  return( (char) IO_GET(base, UART_DATA));
 }
 
 void uart_itoa(int value, char* str, int base){
