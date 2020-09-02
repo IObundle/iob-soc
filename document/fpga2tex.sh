08179120015b95224177e7c3884161e5f1062e0e
pedrompt97 <pedro.m.p.teixeira@tecnico.ulisboa.pt>
2020-09-02 19:07:46 +0100

    Fixed issue where fpga2tex.sh would not detect numbers with few digits

--- document/fpga2tex.sh
+++ document/fpga2tex.sh
@@ -11,7 +11,7 @@ if [ $INTEL = 1 ]; then \
 LOG="top_system.fit.summary";\
 RES="alt_results.tex";\
 scp $COMPILE_SERVER:$HW_DIR/fpga/CYCLONEV-GT-DK/output_files/$LOG . ;\
-ALM=`grep ALM $LOG |grep -o '[0-9]*,[0-9]* \/' | sed s/'\/'//g`;\
+ALM=`grep ALM $LOG |grep -o '[0-9]*,\?[0-9]* \/' | sed s/'\/'//g`;\
 FF=`grep registers $LOG |grep -o '[0-9]*' | sed s/'\/'//g`;\
 DSP=`grep DSP $LOG |grep -o '[0-9]* \/' | sed s/'\/'//g`;\
 BRAM=`grep RAM $LOG |grep -o '[0-9]* \/' | sed s/'\/'//g`;\
@@ -34,8 +34,8 @@ if [ $XILINX = 1 ]; then \
 LOG="vivado.log" ;\
 RES="xil_results.tex" ;\
 scp $COMPILE_SERVER:$HW_DIR/fpga/AES-KU040-DB-G/$LOG . ;\
-LUT=`grep -o 'LUTs\ *| [0-9]*' vivado.log | sed s/'| L'/L/g | sed s/\|/'\&'/g` ;\
-FF=`grep -o 'Registers\ *| [0-9]*' vivado.log | sed s/'| L'/L/g | sed s/\|/'\&'/g` ;\
+LUT=`grep -o 'LUTs\ *|\ * [0-9]*' vivado.log | sed s/'| L'/L/g | sed s/\|/'\&'/g` ;\
+FF=`grep -o 'Registers\ *|\ * [0-9]*' vivado.log | sed s/'| L'/L/g | sed s/\|/'\&'/g` ;\
 DSP=`grep -o 'DSPs\ *|\ * [0-9]*' vivado.log | sed s/'| L'/L/g | sed s/\|/'\&'/g` ;\
 BRAM=`grep -o 'Block RAM Tile \ *|\ * [0-9]*' vivado.log | sed s/'| L'/L/g | sed s/\|/'\&'/g | sed s/lock\ //g | sed s/Tile//g` ;\
 PIN=`grep -o 'Bonded IOB\ *|\ * [0-9]*' vivado.log | sed s/'| L'/L/g | sed s/\|/'\&'/g | sed s/'Bonded IOB'/PIN/g` ;\
