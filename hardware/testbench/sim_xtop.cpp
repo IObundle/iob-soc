05720e09777a827469bf2ffd320a4f906bbc9960
Lahiru <lahiru.rasnayake@ntnu.no>
2021-02-04 17:09:06 +0100

    test_bench_code

---
+++ hardware/testbench/sim_xtop.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Vsystem.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+unsigned int main_time = 0; 
+
+double sc_time_stamp () {
+  return main_time;
+}
+
+int main(int argc, char **argv, char **env)
+{
+  Verilated::commandArgs(argc, argv);
+  Verilated::traceEverOn(true);
+  Vsystem* top = new Vsystem;
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  
+  top->trace (tfp, 1);
+  tfp->open ("waves.vcd");
+ 
+  top->clk = 0;
+  top->resetn = 0;
+  top->eval();
+  tfp->dump(main_time);
+  main_time++;
+  
+  top->clk=1;
+  top->resetn=0;
+  top->eval();
+  tfp->dump(main_time);
+  main_time++;
+        
+  top->clk=0;
+  top->resetn=1;
+  top->eval();
+  tfp->dump(main_time);
+  main_time++;        
+
+  top->clk=1;
+  top->resetn=1;
+  top->eval();
+  tfp->dump(main_time);
+  main_time++;        
+
+  tfp-close();
+  delete top;
+  exit(0);
+
+}
