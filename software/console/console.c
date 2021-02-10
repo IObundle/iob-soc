19ce9edf51ff99f54172047e9d8afdbc590974d0
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2021-02-10 19:08:02 +0000

    console abstracts from comm medium: preparing socket comm

--- software/console/console.c
+++ software/console/console.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>   // File Control Definitions
-#include <termios.h> // POSIX Terminal Control Definitions
-#include <unistd.h>  // UNIX Standard Definitions
-#include <errno.h>   // ERROR Number Definitions
-#include <time.h>
+#include "console.h"
+
+//to do: is this really a uart thing ?
+//seems to be bootloader related and not uart related
+
 #include "iob-uart-ascii.h"
 
-#define PROGNAME "IOb-Console"
 
-static int serial_fd;
+#define PROGNAME "IOb-Console"
 
 //print error
 void cnsl_perror (char * mesg) {
@@ -18,83 +17,23 @@ void cnsl_perror (char * mesg) {
   exit(1);
 }
 
-void cnsl_getchar(char *byte) {
-  int nbytes;
-  do {
-    nbytes = (int) read(serial_fd, byte, 1);
-  } while (!(nbytes > 0));
-}
-
-void cnsl_putchar(char byte) {
-  int nbytes;
-  do nbytes = (int) write(serial_fd, &byte, 1);
-  while (nbytes <= 0);
-}
-
-
-// send file to target
-void sendFile(char *name) {
-  FILE *fp;
-  int file_size;
-  char *buf;
-  int i;
-  
-  //open data file
-  fp = fopen(name, "rb");
-  if (!fp)
-    cnsl_perror("sendFile: Can't open file %s\n");
-  
-  //get file size
-  fseek(fp, 0L, SEEK_END);
-  file_size = ftell(fp);
-  rewind(fp);
-
-  //allocate space for internal file buffer
-  if( (buf = malloc(file_size)) == NULL)
-    cnsl_perror("memory allocation failed\n");
-
-  printf(PROGNAME); printf(": starting transfer of %s (%d bytes)...\n",name, file_size);
-
-  //send file size
-  while (write(serial_fd, &file_size, 4) <= 0);
-  
-  //read file into buffer
-  if (fread(buf, sizeof(char), file_size, fp) <= 0)
-    cnsl_perror("can't read file\n");
-    
-  //send buffer
-  for(i=0;i<file_size;i++)
-    cnsl_putchar(buf[i]);
-          
-  //DEBUG
-  //printf("buffer[%u] = %x\n", i, byte);
-  
-  printf (PROGNAME); printf(": file transfer complete\n");
-  
-  free(buf);
-  fclose(fp);
-
-}
-
-void recvFile(char *name) {
+//receive file from target
+void cnsl_recvfile(char *name) {
   FILE *fp;
   unsigned int file_size = 0;
-  
-  int nbytes;
   char *buf;
   unsigned int i;
 
   //open data file
   fp = fopen(name, "wb");
   if (!fp)
-    cnsl_perror("recvFile: Can't open file\n");
+    cnsl_perror("can't open file to store received file\n");
 
   
-  printf(PROGNAME); printf(": starting %s reception...\n", name);  
+  printf(PROGNAME); printf(": receiving file...%s\n", name);  
   
   //receive file size
-  do nbytes = (int) read(serial_fd, &file_size, sizeof(int));
-  while (nbytes <= 0);
+  file_size = cnsl_getint();
 
   //allocate space for internal file buffer
   if( (buf = malloc(file_size)) == NULL)
@@ -102,34 +41,34 @@ void recvFile(char *name) {
 
   
   //receive file into buffer
-  for (i=0; i<file_size;i=i+nbytes)
+  for (i=0; i<file_size; i++)
     cnsl_getchar( &buf[i]);
 
   //save buffer into file
   if( fwrite(buf, sizeof(char), file_size, fp) <= 0)
-    cnsl_perror("recvFile: failed to write file\n");
+    cnsl_perror("failed to write file\n");
 
   //DEBUG
   //printf("buffer[%u] = %x\n", i, byte);
     
-  printf (PROGNAME); printf(": file reception complete. %d bytes received\n", file_size);
+  printf (PROGNAME); printf(": file of size %d bytes received\n", file_size);
 
   free(buf);
   fclose(fp);
 
 }
 
-//load firmware file into target
-void cnsl_sendfile() {
+//send file to target
+void cnsl_sendfile(char *name) {
   FILE *fp;
   int file_size;
   char *buf;
   int i;
   
   //open file to sent
-  fp = fopen("firmware.bin", "rb");
+  fp = fopen(name, "rb");
   if (!fp)
-    cnsl_perror("sendFile: Can't open firmware.bin\n");
+    cnsl_perror("can't open file to send%s\n");
   
   //get file size
   fseek(fp, 0L, SEEK_END);
@@ -140,10 +79,10 @@ void cnsl_sendfile() {
   if( (buf = malloc(file_size)) == NULL)
     cnsl_perror("memory allocation failed\n");
   
-  printf(PROGNAME); printf(": starting to load firmware with %d bytes...\n", file_size);
+  printf(PROGNAME); printf(": sending file of size %d bytes...\n", file_size);
   
   //send file size
-  while (write(serial_fd, &file_size, 4) <= 0);
+  cnsl_putint(file_size);
   
   //read file into buffer
   if (fread(buf, sizeof(char), file_size, fp) <= 0)
@@ -153,122 +92,23 @@ void cnsl_sendfile() {
   for(i=0;i<file_size;i++)
     cnsl_putchar(buf[i]);
   
-  printf (PROGNAME); printf(": file loading complete\n");
+  printf (PROGNAME); printf(": file sent\n");
   
   free(buf);
   fclose(fp);
-}
-
-int openSerialPort(char *serialPort) {
-  struct termios SerialPortSettings;
-  int fd;
-  
-  // Taken from: https://github.com/xanthium-enterprises/Serial-Port-Programming-on-Linux/blob/master/USB2SERIAL_Write/Transmitter%20(PC%20Side)/SerialPort_write.c
-  
-  printf("\n");
-  printf("+-----------------------------------------------+\n");
-  printf("|                   ");printf(PROGNAME);printf("                 |\n");
-  printf("+-----------------------------------------------+\n");
-  
-  //------------------------------- Opening the Serial Port -------------------------------//
-  
-  // ttyUSB0 is the FT232 based USB2SERIAL Converter    //
-  // O_RDWR Read/Write access to serial port            //
-  // O_NOCTTY - No terminal will control the process    //
-  // O_NDELAY - Non Blocking Mode, does not care about- //
-  // -the status of DCD line, Open() returns immediatly //
-  
-  fd = open(serialPort, O_RDWR | O_NOCTTY | O_NDELAY);
-  if (fd == -1)
-    cnsl_perror("can't open serial port\n");
-  
-  //set attributes of the serial port using termios structure
-  
-  //get current configuration of the serial interface
-  if (tcgetattr(fd, &SerialPortSettings) < 0)
-    cnsl_perror("can't get configuration of serial port\n");
-
-
-  
-  //set baud rate to 115200
-  if (cfsetispeed(&SerialPortSettings, B115200) < 0 ||
-      cfsetospeed(&SerialPortSettings, B115200) < 0)
-    cnsl_perror("can't set baud rate of serial port\n");
-
-  
-  //                                                             //
-  // Input flags - Turn off input processing                     //
-  //                                                             //
-  // convert break to null byte, no CR to NL translation,        //
-  // no NL to CR translation, don't mark parity errors or breaks //
-  // no input parity check, don't strip high bit off,            //
-  // no XON/XOFF software flow control both i/p and o/p          //
-  //                                                             //
-  SerialPortSettings.c_iflag &= ~(IGNBRK | BRKINT | ICRNL | INLCR |
-                                  PARMRK | INPCK | ISTRIP | IXON |
-                                  IXOFF | IXANY);
-  
-  //                                                             //
-  // Output flags - Turn off output processing                   //
-  //                                                             //
-  // no CR to NL translation, no NL to CR-NL translation,        //
-  // no NL to CR translation, no column 0 CR suppression,        //
-  // no Ctrl-D suppression, no fill characters, no case mapping, //
-  // no local output processing                                  //
-  //                                                             //
-  // config.c_oflag &= ~(OCRNL | ONLCR | ONLRET |                //
-  //                     ONOCR | ONOEOT| OFILL | OLCUC | OPOST); //
-  SerialPortSettings.c_oflag = 0;
-  
-  //                                                 //
-  // No line processing                              //
-  //                                                 //
-  // echo off, echo newline off, canonical mode off, //
-  // extended input processing off, signal chars off //
-  //                                                 //
-  SerialPortSettings.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN | ISIG);
-  
-  //                                                   //
-  // Turn off character processing                     //
-  //                                                   //
-  // clear current char size mask, no parity checking, //
-  // no output processing, force 8 bit input, set      //
-  // 1 stop bit and enable receiver                    //
-  //                                                   //
-  SerialPortSettings.c_cflag &= ~(CRTSCTS | CSTOPB | CSIZE | PARENB | CLOCAL);
-  SerialPortSettings.c_cflag |= CS8 | CREAD;
-  
-  //                                                //
-  // One input byte is enough to return from read() //
-  // Inter-character timer off                      //
-  //                                                //
-  SerialPortSettings.c_cc[VMIN]  = 1;
-  SerialPortSettings.c_cc[VTIME] = 0;
-  
-  // Apply new configuration
-  if (tcsetattr(fd, TCSANOW, &SerialPortSettings)) {
-    close(fd);
-    cnsl_perror("\n  ERROR ! in Setting attributes\n");
-  } else {
-    printf("\n  BaudRate = 115200 \n  StopBits = 1 \n  Parity   = None\n\n");
-  }
-  
-  //------------------------------- Write data to serial port -----------------------------//
-  
-  return fd;
 }
 
 void usage(char *message){
   cnsl_perror("usage: ./console -s <serial port> [ -f <firmware file> ]\n");
 }
 
-
 //
 // MAIN ROUTINE
 //
 
 int main(int argc, char* argv[]) {
-  char *serialPort = 0;
+
+  char *devname = 0;
   char *fwFile = 0;
   int i;
   
@@ -278,7 +118,7 @@ int main(int argc, char* argv[]) {
   for (i = 1; i < argc; i++) {
     if (argv[i][0] == '-' && !argv[i][2]) {
       if (argv[i][1] == 's') {
-        serialPort = argv[++i];
+        devname = argv[++i];
       } else if (argv[i][1] == 'f') {
         fwFile = "firmware.bin";
       } else usage("PROGNAME: unexpected argument\n");
@@ -286,7 +126,7 @@ int main(int argc, char* argv[]) {
   }
   
   //open serial port
-  serial_fd = openSerialPort(serialPort);
+  cnsl_open(devname);
 
   //server loop
   char byte;
@@ -295,7 +135,7 @@ int main(int argc, char* argv[]) {
   while (1) {
 
     //get byte from target
-    cnsl_getchar(&byte);
+    byte = cnsl_getchar();
 
     //process command
     switch (byte) {
@@ -305,7 +145,7 @@ int main(int argc, char* argv[]) {
         gotENQ = 1;
         if(fwFile) {
           cnsl_putchar(FRX);
-          cnsl_sendfile();
+          cnsl_sendfile(fwFile);
         } else
           cnsl_putchar(ACK);
       }
@@ -324,6 +164,6 @@ int main(int argc, char* argv[]) {
 
   }
   
-  close(serial_fd);
-
+  cnsl_close();
+  
 }
