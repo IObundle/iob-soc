49cb40534e802d75b0b468a57c11759554b05e74
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2021-05-29 15:07:57 +0100

    fixes for remote fpga compile

--- hardware/fpga/fpga.mk
+++ hardware/fpga/fpga.mk
@@ -14,8 +14,8 @@ all: sw build load run
 
 run:
 ifeq ($(BOARD_SERVER),)
-	if [ $(FPGA_NORUN) == 0 ]; then make -C $(CONSOLE_DIR) run; fi
-else
+	if [ $(NORUN) = 0 ]; then make -C $(CONSOLE_DIR) run; fi
+else ifeq ($(NORUN),0)
 	ssh $(BOARD_USER)@$(BOARD_SERVER) 'if [ ! -d $(REMOTE_ROOT_DIR) ]; then mkdir -p $(REMOTE_ROOT_DIR); fi'
 	rsync -avz --exclude .git $(ROOT_DIR) $(BOARD_USER)@$(BOARD_SERVER):$(REMOTE_ROOT_DIR) 
 	ssh $(BOARD_USER)@$(BOARD_SERVER) 'cd $(REMOTE_ROOT_DIR)/hardware/fpga/$(BOARD); make run INIT_MEM=$(INIT_MEM) TEST_LOG="$(TEST_LOG)"'
@@ -27,11 +27,11 @@ endif
 
 load:
 ifeq ($(BOARD_SERVER),)
-	if [ $(FPGA_NORUN) == 0 -a ! -f load.log ]; then ./prog.sh > load.log; fi
-else
+	if [ $(NORUN) = 0 -a ! -f load.log ]; then ./prog.sh > load.log; fi
+else ifeq ($(NORUN),0)
 	ssh $(BOARD_USER)@$(BOARD_SERVER) 'if [ ! -d $(REMOTE_ROOT_DIR) ]; then mkdir -p $(REMOTE_ROOT_DIR); fi'
 	rsync -avz --exclude .git $(ROOT_DIR) $(BOARD_USER)@$(BOARD_SERVER):$(REMOTE_ROOT_DIR) 
-	ssh $(BOARD_USER)@$(BOARD_SERVER) 'cd $(REMOTE_ROOT_DIR)/hardware/fpga/$(BOARD); make load FPGA_NORUN=$(FPGA_NORUN)'
+	ssh $(BOARD_USER)@$(BOARD_SERVER) 'cd $(REMOTE_ROOT_DIR)/hardware/fpga/$(BOARD); make load'
 endif
 
 
@@ -40,12 +40,12 @@ build: $(FPGA_OBJ)
 
 $(FPGA_OBJ): $(wildcard *.sdc) $(VSRC) $(VHDR) firmware.hex boot.hex
 ifeq ($(FPGA_SERVER),)
-	if [ $(FPGA_NORUN) == 0 ]; then ./build.sh "$(INCLUDE)" "$(DEFINE)" "$(VSRC)"; fi
-else
+	if [ $(NORUN) = 0 ]; then ./build.sh "$(INCLUDE)" "$(DEFINE)" "$(VSRC)"; fi
+else ifeq ($(NORUN),0)
 	ssh $(FPGA_USER)@$(FPGA_SERVER) 'if [ ! -d $(REMOTE_ROOT_DIR) ]; then mkdir -p $(REMOTE_ROOT_DIR); fi'
 	rsync -avz --exclude .git $(ROOT_DIR) $(FPGA_USER)@$(FPGA_SERVER):$(REMOTE_ROOT_DIR)
 	ssh $(FPGA_USER)@$(FPGA_SERVER) 'cd $(REMOTE_ROOT_DIR)/hardware/fpga/$(BOARD); make build INIT_MEM=$(INIT_MEM) USE_DDR=$(USE_DDR) RUN_EXTMEM=$(RUN_EXTMEM) BOARD=$(BOARD)'
-	if [ $(FPGA_NORUN) == 0 ]; then scp $(FPGA_USER)@$(FPGA_SERVER):$(REMOTE_ROOT_DIR)/$(BOARD_DIR)/$(FPGA_OBJ) $(BOARD_DIR);
+	if [ $(NORUN) = 0 ]; then scp $(FPGA_USER)@$(FPGA_SERVER):$(REMOTE_ROOT_DIR)/hardware/fpga/$(BOARD)/$(FPGA_OBJ) $(BOARD_DIR); fi
 endif
 
 
