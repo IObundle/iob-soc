f96c7a66d6cbe6a7a84317be0c6f72b509ebd44c
Joao D Lopes <joao.d.lopes91@gmail.com>
2021-07-23 18:01:13 +0100

    fix Vivado path

--- hardware/fpga/AES-KU040-DB-G/prog.sh
+++ hardware/fpga/AES-KU040-DB-G/prog.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-source $XILINXPATH/Vivado/settings64.sh
+source $XILINXPATH/settings64.sh
 vivado -nojournal -log vivado.log -mode batch -source ld-hw.tcl
--- hardware/fpga/BASYS3/build.sh
+++ hardware/fpga/BASYS3/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source $XILINXPATH/Vivado/2020.1/settings64.sh
+source $XILINXPATH/settings64.sh
 vivado -nojournal -log vivado.log -mode batch -source synth_system.tcl -tclargs "$1" "$2" "$3"
