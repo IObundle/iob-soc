f1776d8196fb81e272cc6ca60a50c1f4f516de20
Joao D Lopes <joao.d.lopes91@gmail.com>
2021-10-05 22:47:48 +0100

    move general targets from umc130 Makefile to asic.mk; add first files for umc130 synthesis; update .gitignore file

--- hardware/asic/asic.mk
+++ hardware/asic/asic.mk
@@ -14,3 +14,25 @@ CASE=TC
 # Memory sizes in log2
 BOOTROM_W:=$(shell echo '$(BOOTROM_ADDR_W)-2' | bc)
 SRAM_W:=$(shell echo '$(SRAM_ADDR_W)-2' | bc)
+
+BOOTROM_WORDS:=$(shell echo '2^($(BOOTROM_W))' | bc)
+SRAM_WORDS:=$(shell echo '2^($(SRAM_W))' | bc)
+
+VSRC+=bootrom.v sram.v
+
+all: mems synth
+
+mems: bootrom sram
+ifneq ($(ASIC_SERVER),)
+	scp $(ASIC_USER)@$(ASIC_SERVER):$(REMOTE_ROOT_DIR)/hardware/asic/$(ASIC_NODE)/$(ASIC_MEMS) .
+endif
+
+bootrom: sw gen-bootrom
+
+sram: gen-sram
+
+synth: system_synth.v
+
+.PHONY: all \
+	mems bootrom sram \
+	synth
