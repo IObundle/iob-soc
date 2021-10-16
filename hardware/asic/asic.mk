3894983014e2ece93d9e485176ce3af40f0a9c28
Joao D Lopes <joao.d.lopes91@gmail.com>
2021-10-16 19:46:35 +0100

    fix sram.v generation for ASIC; improve asic.mk; cleanup datasheet files from ASIC memories; transfer ASIC reports from server to local machine; use CASE variable to filter libraries to use on synthesis for ASIC

--- hardware/asic/asic.mk
+++ hardware/asic/asic.mk
@@ -1,11 +1,12 @@
 #DEFINES
-
-#ddr controller address width
-DEFINE+=$(defmacro)DDR_ADDR_W=24
-
-include $(ROOT_DIR)/hardware/hardware.mk
+ASIC=1
 
 CASE=TC
+
+#ddr controller address width
+DDR_ADDR_W=24
+
+include $(ROOT_DIR)/hardware/hardware.mk
 
 # Memory sizes in log2
 BOOTROM_W:=$(shell echo '$(BOOTROM_ADDR_W)-2' | bc)
@@ -41,13 +42,16 @@ bootrom.v:
 	$(MEM_DIR)/software/python/memakerwrap.py fsc0l_d sp $(BOOTROM_W) 32 1 > $@
 
 sram.v:
-	$(MEM_DIR)/software/python/memakerwrap.py fsc0l_d sj 0 1 $(SRAM_W) 8 4 1 > $@
+	$(MEM_DIR)/software/python/memakerwrap.py fsc0l_d sj 0 1 $(SRAM_W) 8 4 16 > $@
 
 #
 # Synthesis
 #
 
 synth: system_synth.v
+ifneq ($(ASIC_SERVER),)
+	scp $(ASIC_USER)@$(ASIC_SERVER):$(REMOTE_ROOT_DIR)/hardware/asic/$(ASIC_NODE)/$(ASIC_REPORTS) .
+endif
 
 #
 # Testing
@@ -64,11 +68,10 @@ test1: clean
 #
 
 clean-remote: hw-clean
-	@rm -f $(ASIC_MEM_LEFS) $(ASIC_MEM_LIBS) $(ASIC_MEM_SIM_MODELS)
 ifneq ($(ASIC_SERVER),)
 	ssh $(ASIC_USER)@$(ASIC_SERVER) 'if [ ! -d $(REMOTE_ROOT_DIR) ]; then mkdir -p $(REMOTE_ROOT_DIR); fi'
 	rsync -avz --exclude .git $(ROOT_DIR) $(ASIC_USER)@$(ASIC_SERVER):$(REMOTE_ROOT_DIR)
-	ssh $(ASIC_USER)@$(ASIC_SERVER) 'cd $(REMOTE_ROOT_DIR)/hardware/asic/$(ASIC_NODE); make $@ ASIC_NODE=$(ASIC_NODE)'
+	ssh $(ASIC_USER)@$(ASIC_SERVER) 'cd $(REMOTE_ROOT_DIR)/hardware/asic/$(ASIC_NODE); make clean'
 endif
 
 #clean test log only when asic testing begins
@@ -77,7 +80,7 @@ clean-testlog:
 ifneq ($(ASIC_SERVER),)
 	ssh $(ASIC_USER)@$(ASIC_SERVER) 'if [ ! -d $(REMOTE_ROOT_DIR) ]; then mkdir -p $(REMOTE_ROOT_DIR); fi'
 	rsync -avz --exclude .git $(ROOT_DIR) $(ASIC_USER)@$(ASIC_SERVER):$(REMOTE_ROOT_DIR)
-	ssh $(ASIC_USER)@$(ASIC_SERVER) 'cd $(REMOTE_ROOT_DIR)/hardware/asic/$(ASIC_NODE); make $@ ASIC_NODE=$(ASIC_NODE)'
+	ssh $(ASIC_USER)@$(ASIC_SERVER) 'cd $(REMOTE_ROOT_DIR)/hardware/asic/$(ASIC_NODE); make $@'
 endif
 
 .PHONY: all \
