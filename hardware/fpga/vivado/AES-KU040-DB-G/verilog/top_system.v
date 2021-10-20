c8ebe52f2104259d80b9a05791fb312187d70e19
Joao D Lopes <joao.d.lopes91@gmail.com>
2021-10-20 17:38:53 +0100

    add missing axi signals for full AXI4-full protocol support

--- hardware/fpga/vivado/AES-KU040-DB-G/verilog/top_system.v
+++ hardware/fpga/vivado/AES-KU040-DB-G/verilog/top_system.v
@@ -419,7 +419,7 @@ module top_system(
       .m_axi_wready  (sys_wready),
       
       //write response
-      //.m_axi_bid     (sys_bid),
+      .m_axi_bid     (sys_bid),
       .m_axi_bresp   (sys_bresp),
       .m_axi_bvalid  (sys_bvalid),
       .m_axi_bready  (sys_bready),
@@ -438,7 +438,7 @@ module top_system(
       .m_axi_arready (sys_arready),
 
       //read   
-      //.m_axi_rid     (sys_rid),
+      .m_axi_rid     (sys_rid),
       .m_axi_rdata   (sys_rdata),
       .m_axi_rresp   (sys_rresp),
       .m_axi_rlast   (sys_rlast),
