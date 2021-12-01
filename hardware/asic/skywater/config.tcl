2093602112ad214103d65156dd448a43f76ad4b3
sharjeel <52481908+microSharjeel@users.noreply.github.com>
2021-12-01 15:53:32 +0500

    Create config.tcl

---
+++ hardware/asic/skywater/config.tcl
@@ -0,0 +1,14 @@
+# User config
+set ::env(DESIGN_NAME) system_core
+
+# Change if needed
+set ::env(VERILOG_FILES) "src/*.v"
+
+# Fill this
+set ::env(CLOCK_PERIOD) "10"
+set ::env(CLOCK_PORT) "clk"
+
+set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
+if { [file exists $filename] == 1} {
+	source $filename
+}
