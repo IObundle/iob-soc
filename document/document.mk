4b695f64e909bce8fe0d1ea1681461937060d816
Joao D Lopes <joao.d.lopes91@gmail.com>
2021-12-02 17:49:22 +0000

    update TEX submodule; update sources according new TEX version

--- document/document.mk
+++ document/document.mk
@@ -2,7 +2,7 @@ include $(UART_DIR)/config.mk
 
 #block diagram verilog source
 BD_VSRC=uart_core.v
-MODULE=$(shell make -C $(UART_DIR) corename | grep -v make)
+CORENAME=$(shell make -C $(UART_DIR) corename | grep -v make)
 
 INTEL ?=1
 INT_FAMILY ?=CYCLONEV-GT
