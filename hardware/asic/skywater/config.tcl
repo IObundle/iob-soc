0c1bb1e0f8472bcca69110c67f01ac44984832e3
c <khilji@kth.se>
2022-01-26 23:17:48 +0500

    Resolved incl. files issue and Macro not found issue in Macro placement

--- hardware/asic/skywater/config.tcl
+++ hardware/asic/skywater/config.tcl
@@ -1,19 +1,8 @@
 # User config
 set ::env(DESIGN_NAME) system
 # Change if needed
-set ::env(VERILOG_FILES) "\
-    designs/system/src/system.v\
-    designs/system/src/boot_ctr.v\
-    designs/system/src/int_mem.v\
-    designs/system/src/iob_sp_ram_be.v\
-    designs/system/src/iob_sp_rom.v\
-    designs/system/src/iob_picorv32.v\
-    designs/system/src/iob_uart.v\
-    designs/system/src/merge.v\
-    designs/system/src/picorv32.v\
-    designs/system/src/split.v\
-    designs/system/src/sram.v\
-    designs/system/src/uart_core.v"
+set ::env(VERILOG_INCLUDE_DIRS) "designs/system/inc"
+set ::env(VERILOG_FILES) "designs/system/src/*.v"
 set ::env(CLOCK_PERIOD) "10"
 set ::env(CLOCK_PORT) "clk"
 set ::env(DESIGN_IS_CORE) 0
--- hardware/asic/skywater/run_interactive.tcl
+++ hardware/asic/skywater/run_interactive.tcl
@@ -4,7 +4,7 @@ prep -design system config file /system/config.tcl -tag soc -overwrite
 run_yosys
 run_sta
 init_floorplan
-add_macro_placement ram 5.59000 168.23 N
+add_macro_placement int_mem0.int_sram.main_mem_byte.ram 5.59000 168.23 N
 manual_macro_placement f
 place_io
 tap_decap_or
