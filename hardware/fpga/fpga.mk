6b6e4c7bca03cc2eac61d869fc80a6a03bd69fdb
Joao D Lopes <joao.d.lopes91@gmail.com>
2022-01-27 19:35:49 +0000

    fix USER variable when execute queue-out-remote in FPGA Makefiles

--- hardware/fpga/fpga.mk
+++ hardware/fpga/fpga.mk
@@ -78,7 +78,7 @@ queue-out:
 queue-out-remote:
 	ssh $(BOARD_USER)@$(BOARD_SERVER) \
 	'make -C $(REMOTE_ROOT_DIR)/hardware/fpga/$(TOOL)/$(BOARD) queue-out;\
-	if [ "`pgrep -u $(USER) console`" ]; then killall -q -u $(USER) -9 console; fi'
+	if [ "`pgrep -u $(BOARD_USER) console`" ]; then killall -q -u $(BOARD_USER) -9 console; fi'
 
 #
 # Testing
