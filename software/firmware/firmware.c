8dc7007d0772b1abc19b5b405fa06cc5f59380a1
Artur Nóbrega <arturnobrega1999@gmail.com>
2022-01-29 02:08:51 +0000

    Changed firmware to only run REGFILEIF tests if the system was built with that peripheral (system was built with tester or user included REGFILEIF in peripheral list)

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -4,14 +4,19 @@
 #include "periphs.h"
 #include "iob-uart.h"
 #include "printf.h"
+#ifdef REGFILEIF_TESTER_BASE
 #include "iob-regfileif.h"
+#endif
 
 int main()
 {
   //init uart
   uart_init(UART0_BASE,FREQ/BAUD);   
-  regfileif_setbaseaddr(REGFILEIF_TESTER_BASE);   
   uart_puts("\n\n\nHello world!\n\n\n");
+
+  //run REGFILEIF tests if the system was built with it (and the Tester)
+  #ifdef REGFILEIF_TESTER_BASE
+  regfileif_setbaseaddr(REGFILEIF_TESTER_BASE);   
 
   regfileif_writereg(0, 666);
   regfileif_writereg(1, 667);
@@ -22,6 +27,7 @@ int main()
   printf("%d \n", regfileif_readreg(1));
   printf("%d \n", regfileif_readreg(2));
   printf("%d \n", regfileif_readreg(3));
+  #endif
 
   uart_finish();
 }
