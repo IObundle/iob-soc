3c25981a55c6c755db106460958020aa6e291769
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-02-06 16:15:41 +0000

    centralizing compiler and linker flags

--- software/software.mk
+++ software/software.mk
@@ -7,7 +7,9 @@ DEFINE+=$(defmacro)FREQ=$(FREQ)
 
 #compiler settings
 TOOLCHAIN_PREFIX:=riscv64-unknown-elf-
-CFLAGS=-Os -nostdlib -march=$(MFLAGS) -mabi=ilp32
+CFLAGS=-Os -nostdlib -march=$(MFLAGS) -mabi=ilp32 --specs=nano.specs
+LFLAGS+= -Wl,-Bstatic,-T,../template.lds,--strip-debug
+LLIBS=-lgcc -lc -lnosys
 
 MFLAGS=$(MFLAGS_BASE)$(MFLAG_M)$(MFLAG_C)
 
