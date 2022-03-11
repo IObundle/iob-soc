8dcc181d30f3da4a23beb94cc3e421969a7c767f
Joao D Lopes <joao.d.lopes91@gmail.com>
2022-03-11 20:13:53 +0000

    fix console when receive file name via FPGA; remove dummy test target; update test.expected files for FPGAs; cleanup clean targets for FPGAs

--- hardware/fpga/quartus/quartus.mk
+++ hardware/fpga/quartus/quartus.mk
@@ -13,9 +13,8 @@ post-build:
 	mv output_files/top_system.sof $(FPGA_OBJ)
 	mv output_files/top_system.fit.summary $(FPGA_LOG)
 
-
 clean: clean-remote
-	rm -rf db incremental_db output_files *.summary *.rpt *.smsg *.txt *.done \
-	*.jdi *.pin *.sof *.sld *.qpf *.qsf *~ system.v
+	@rm -rf db/ incremental_db/ output_files/ \
+	*.qdf *.sof *.sld *.qpf *.qsf *.txt
 
 .PHONY: pre-build post-build clean
