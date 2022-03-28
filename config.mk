77cf8d702ea32c9d1ac5181c1f89012bcbf4aa71
Artur Nóbrega <arturnobrega1999@gmail.com>
2022-03-28 15:25:37 +0100

    Add warning when missing register configuration file

--- config.mk
+++ config.mk
@@ -13,6 +13,11 @@ REGFILEIF_HW_DIR:=$(REGFILEIF_DIR)/hardware
 #MAKE SW ACCESSIBLE REGISTER
 MKREGS:=$(shell find $(LIB_DIR) -name mkregs.py)
 
+#Check that configuration file exists
+ifeq (,$(wildcard $(ROOT_DIR)/iob_regfileif_swreg.vh))
+    $(error Missing 'iob_regfileif_swreg.vh' configuration file in root directory!)
+endif
+
 # VERSION
 VERSION ?=V0.1
 $(TOP_MODULE)_version.txt:
