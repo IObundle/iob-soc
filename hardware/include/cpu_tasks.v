8b26edda868a77198b75ee5303cdb1ef7f20b5a2
Pedro Antunes <pedronmantunes@gmail.com>
2022-03-30 19:49:27 +0100

    adress macros are already defined before

--- hardware/include/cpu_tasks.v
+++ hardware/include/cpu_tasks.v
@@ -3,16 +3,6 @@
 //
 //this is a temporary solution
 
-//address macros
-`define UART_SOFTRESET_ADDR 0
-`define UART_DIV_ADDR 1
-`define UART_TXDATA_ADDR 2
-`define UART_TXEN_ADDR 3
-`define UART_TXREADY_ADDR 4
-`define UART_RXDATA_ADDR 5
-`define UART_RXEN_ADDR 6
-`define UART_RXREADY_ADDR 7
-
 //file seek macros
 `define SEEK_SET 0
 `define SEEK_CUR 1
