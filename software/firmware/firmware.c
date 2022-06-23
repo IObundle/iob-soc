a46eed558883cf874c14a963cf0c664246697396
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-06-23 18:56:23 +0100

    fix pc-emul issues

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "system.h"
 #include "periphs.h"
 #include "iob-uart.h"
@@ -7,8 +6,18 @@
 int main()
 {
   //init uart
-  uart_init(UART_BASE,FREQ/BAUD);   
+  uart_init(UART_BASE,FREQ/BAUD);
+
+  //test puts
   uart_puts("\n\n\nHello world!\n\n\n");
+
+  //test printf with floats 
   printf("Value of Pi = %f\n\n", 3.1415);
+
+  //test file receive
+  char *buf = malloc(10000);
+  int file_size = 0;
+  file_size = uart_recvfile("Makefile", &buf);
+
   uart_finish();
 }
