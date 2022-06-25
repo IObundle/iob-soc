e08e3a48b42238bef87866f97ee4c71cb328bf97
Artur Nóbrega <arturnobrega1999@gmail.com>
2022-06-25 18:59:57 +0100

    Update AXISTREAMIN driver

--- software/iob-axistream-in.h
+++ software/iob-axistream-in.h
@@ -7,9 +7,11 @@
 //Set AXISTREAMIN base address
 void axistream_in_init(int base_address);
 
-//Get value from FIFO (returns true if this is last byte from stream)
-bool axistream_in_pop(char *returnValue);
+//Get value from FIFO
+uint32_t axistream_in_pop();
 
 //Signal when FIFO empty
 bool axistream_in_empty();
 
+//Returns if last value of FIFO was the end of frame (by TLAST signal) and get rstrb from that value
+bool axistream_in_was_last(char *rstrb);
