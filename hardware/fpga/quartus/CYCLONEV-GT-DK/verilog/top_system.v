f4f15910ebd87ec5cd863eb281e926a385b45cb0
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-07-06 04:28:02 +0100

    switch to soft memory controller

--- hardware/fpga/quartus/CYCLONEV-GT-DK/verilog/top_system.v
+++ hardware/fpga/quartus/CYCLONEV-GT-DK/verilog/top_system.v
@@ -13,21 +13,21 @@ module top_system
    input         uart_rxd,
 
 `ifdef USE_DDR
-   output [13:0] ddr3a_a, //SSTL15  //Address
-   output [2:0]  ddr3a_ba, //SSTL15  //Bank Address
-   output        ddr3a_rasn, //SSTL15  //Row Address Strobe
-   output        ddr3a_casn, //SSTL15  //Column Address Strobe
-   output        ddr3a_wen, //SSTL15  //Write Enable
-   output [1:0]  ddr3a_dm, //SSTL15  //Data Write Mask
-   inout [15:0]  ddr3a_dq, //SSTL15  //Data Bus
-   output        ddr3a_clk_n, //SSTL15  //Diff Clock - Neg
-   output        ddr3a_clk_p, //SSTL15  //Diff Clock - Pos
-   output        ddr3a_cke, //SSTL15  //Clock Enable
-   output        ddr3a_csn, //SSTL15  //Chip Select
-   inout [1:0]   ddr3a_dqs_n, //SSTL15  //Diff Data Strobe - Neg
-   inout [1:0]   ddr3a_dqs_p, //SSTL15  //Diff Data Strobe - Pos
-   output        ddr3a_odt, //SSTL15  //On-Die Termination Enable
-   output        ddr3a_resetn, //SSTL15  //Reset
+   output [13:0] ddr3b_a, //SSTL15  //Address
+   output [2:0]  ddr3b_ba, //SSTL15  //Bank Address
+   output        ddr3b_rasn, //SSTL15  //Row Address Strobe
+   output        ddr3b_casn, //SSTL15  //Column Address Strobe
+   output        ddr3b_wen, //SSTL15  //Write Enable
+   output [1:0]  ddr3b_dm, //SSTL15  //Data Write Mask
+   inout [15:0]  ddr3b_dq, //SSTL15  //Data Bus
+   output        ddr3b_clk_n, //SSTL15  //Diff Clock - Neg
+   output        ddr3b_clk_p, //SSTL15  //Diff Clock - Pos
+   output        ddr3b_cke, //SSTL15  //Clock Enable
+   output        ddr3b_csn, //SSTL15  //Chip Select
+   inout [1:0]   ddr3b_dqs_n, //SSTL15  //Diff Data Strobe - Neg
+   inout [1:0]   ddr3b_dqs_p, //SSTL15  //Diff Data Strobe - Pos
+   output        ddr3b_odt, //SSTL15  //On-Die Termination Enable
+   output        ddr3b_resetn, //SSTL15  //Reset
    input         rzqin,
 `endif
    output        trap
@@ -62,21 +62,21 @@ module top_system
       .reset_reset_n (resetn),
       .oct_rzqin (rzqin),
 
-      .memory_mem_a (ddr3a_a),
-      .memory_mem_ba (ddr3a_ba),
-      .memory_mem_ck (ddr3a_clk_p),
-      .memory_mem_ck_n (ddr3a_clk_n),
-      .memory_mem_cke (ddr3a_cke),
-      .memory_mem_cs_n (ddr3a_csn),
-      .memory_mem_dm (ddr3a_dm),
-      .memory_mem_ras_n (ddr3a_rasn),
-      .memory_mem_cas_n (ddr3a_casn),
-      .memory_mem_we_n (ddr3a_wen),
-      .memory_mem_reset_n (ddr3a_resetn),
-      .memory_mem_dq (ddr3a_dq),
-      .memory_mem_dqs (ddr3a_dqs_p),
-      .memory_mem_dqs_n (ddr3a_dqs_n),
-      .memory_mem_odt (ddr3a_odt),
+      .memory_mem_a (ddr3b_a),
+      .memory_mem_ba (ddr3b_ba),
+      .memory_mem_ck (ddr3b_clk_p),
+      .memory_mem_ck_n (ddr3b_clk_n),
+      .memory_mem_cke (ddr3b_cke),
+      .memory_mem_cs_n (ddr3b_csn),
+      .memory_mem_dm (ddr3b_dm),
+      .memory_mem_ras_n (ddr3b_rasn),
+      .memory_mem_cas_n (ddr3b_casn),
+      .memory_mem_we_n (ddr3b_wen),
+      .memory_mem_reset_n (ddr3b_resetn),
+      .memory_mem_dq (ddr3b_dq),
+      .memory_mem_dqs (ddr3b_dqs_p),
+      .memory_mem_dqs_n (ddr3b_dqs_n),
+      .memory_mem_odt (ddr3b_odt),
       
       .axi_bridge_0_s0_awid (m_axi_awid),
       .axi_bridge_0_s0_awaddr (m_axi_awaddr),
