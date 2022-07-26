2216ae4e3abffe2d1074f49a4f15e9718fdbcd8d
Pedro Antunes <pedronmantunes@gmail.com>
2022-07-26 23:28:50 +0100

    Simplify reset sequence; Fix not showing reset trace;

--- hardware/simulation/verilator/system_tb.cpp
+++ hardware/simulation/verilator/system_tb.cpp
@@ -23,9 +23,6 @@ void Timer(unsigned int ns){
     if(!(main_time%(CLK_PERIOD/2))){
       dut->clk = !(dut->clk);
       dut->eval();
-#ifdef VCD
-      tfp->dump(main_time);
-#endif
     }
     // To add a new clk follow the example
     //if(!(main_time%(EXAMPLE_CLK_PERIOD/2))){
@@ -95,21 +92,13 @@ int main(int argc, char **argv, char **env){
 
   dut->clk = 0;
   dut->reset = 0;
-  dut->eval();
-#ifdef VCD
-  tfp->dump(main_time);
-#endif
 
   // Reset sequence
-  for(int i = 0; i<5; i++){
-    dut->clk = !(dut->clk);
-    if(i==2 || i==4) dut->reset = !(dut->reset);
-    dut->eval();
-#ifdef VCD
-    tfp->dump(main_time);
-#endif
-    main_time += CLK_PERIOD/2;
-  }
+  Timer(100);
+  dut->reset = 1;
+  Timer(100);
+  dut->reset = 0;
+
   dut->uart_valid = 0;
   dut->uart_wstrb = 0;
   inituart();
