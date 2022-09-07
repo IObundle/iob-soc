1ad05459ed030230512e907f4d90c89b3ac4dae4
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-09-07 08:51:02 +0100

    simplify fpga folder

--- hardware/fpga/DE10-LITE/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-nios=$QUARTUSPATH/nios2eds/nios2_command_shell.sh
-$nios quartus_sh -t top_system.tcl "$1" "$2" "$3"
-$nios quartus_map top_system
-$nios quartus_fit --read_settings_files=off -write_settings_files=off top_system -c top_system
-$nios quartus_sta top_system -c top_system --do_report_timing
-$nios quartus_asm top_system
--- hardware/fpga/DE10-LITE/prog.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-nios=$QUARTUSPATH/nios2eds/nios2_command_shell.sh
-$nios quartus_pgm -m jtag -c 1 -o "p;output_files/top_system.sof"
