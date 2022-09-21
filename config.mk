e82fa6ea467c39493076e6cc12048daacb5f209e
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-09-21 21:55:59 +0100

    towards solving the system configuration conundrum

--- config.mk
+++ config.mk
@@ -11,20 +11,16 @@ SOC_DEFINE+=USE_COMPRESSED=1
 SOC_DEFINE+=E=31 #select secondary data memory
 SOC_DEFINE+=P=30 #select peripheral space
 SOC_DEFINE+=B=29 #select boot controller
-
-#PERIPHERAL LIST
-#list with corename of peripherals to be attached to peripheral bus.
-#to include multiple instances, write the corename of the peripheral multiple times.
-#to pass verilog parameters to each instance, type the parameters inside parenthesis.
-#Example: 'PERIPHERALS ?=UART[1,\"textparam\"] UART UART' will create 3 UART instances, 
-#         the first one will be instantiated with verilog parameters 1 and "textparam", 
-#         the second and third will use default parameters.
-PERIPHERALS ?=UART
+#PRE-INIT MEMORY WITH PROGRAM AND DATA
+SOC_DEFINE+=INIT_MEM=1
+SOC_DEFINE+=RUN_EXTMEM=0
+SOC_DEFINE+=DCACHE_ADDR_W=24
 
 #PERIPHERAL IDs
 #assign a sequential ID to each peripheral
 #the ID is used as an instance name index in the hardware and as a base address in the software
 SOC_DEFINE+=$(shell $(LIB_DIR)/scripts/submodule_utils.py get_defines "$(PERIPHERALS)" "")
+
 SOC_DEFINE+=N_SLAVES=$(shell $(LIB_DIR)/scripts/submodule_utils.py get_n_slaves "$(PERIPHERALS)") #peripherals
 SOC_DEFINE+=N_SLAVES_W=$(shell $(LIB_DIR)/scripts/submodule_utils.py get_n_slaves_w "$(PERIPHERALS)")
 
@@ -34,34 +30,5 @@ GET_DIRS= $(eval ROOT_DIR_TMP=.)\
           $(if $(filter %_DIR, $V),\
           $(eval TMP_VAR:=$(subst ROOT_DIR,ROOT_DIR_TMP,$(value $V)))$V=$(TMP_VAR);))
 
-#PRE-INIT MEMORY WITH PROGRAM AND DATA
-INIT_MEM ?=1
 
-#DDR
-USE_DDR ?=0
-RUN_EXTMEM ?=0
-
-#RISC-V HARD MULTIPLIER AND DIVIDER INSTRUCTIONS
-USE_MUL_DIV=1
-
-#RISC-V COMPRESSED INSTRUCTIONS
-USE_COMPRESSED=1
-
-#default baud and system clock frequency
-SIM_BAUD = 2500000
-SIM_FREQ =50000000
-
-#default baud and frequency if not given
-BAUD ?=$(SIM_BAUD)
-FREQ ?=$(SIM_FREQ)
-
-# #CPU ARCHITECTURE
-DATA_W := 32
-# ADDR_W := 32
-
-#DATA CACHE ADDRESS WIDTH (tag + index + offset)
-DCACHE_ADDR_W:=24
-
-FIRM_ADDR_W=15
-BOOTROM_ADDR_W=12
 
--- config_setup.mk
+++ config_setup.mk
@@ -13,5 +13,14 @@ CACHE_DIR ?= $(SOC_DIR)/submodules/CACHE
 UART_DIR ?= $(SOC_DIR)/submodules/UART
 LIB_DIR ?= $(SOC_DIR)/submodules/LIB
 
+#PERIPHERAL LIST
+#list with corename of peripherals to be attached to peripheral bus.
+#to include multiple instances, write the corename of the peripheral multiple times.
+#to pass verilog parameters to each instance, type the parameters inside parenthesis.
+#Example: 'PERIPHERALS ?=UART[1,\"textparam\"] UART UART' will create 3 UART instances, 
+#         the first one will be instantiated with verilog parameters 1 and "textparam", 
+#         the second and third will use default parameters.
+PERIPHERALS ?=UART
+
 # default configuration
 CONFIG ?= base
