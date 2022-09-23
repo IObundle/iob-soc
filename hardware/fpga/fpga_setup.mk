e1b107f3f747d74240d597aa1a23fe7c0747920b
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-09-23 08:32:33 +0100

    choose board at build time

--- hardware/fpga/fpga_setup.mk
+++
@@ -1,7 +0,0 @@
-include $(LIB_DIR)/hardware/iob_reset_sync/hw_setup.mk
-
-#SOURCES
-SRC+=$(BUILD_FPGA_DIR)/verilog/iob_soc_fpga_wrapper.v
-$(BUILD_FPGA_DIR)/%.vh: $(FPGA_DIR)/$(BOARD)/iob_soc_fpga_wrapper.v
-	cp $< $@
-
