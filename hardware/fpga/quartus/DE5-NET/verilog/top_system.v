aa0ce30ceb055da5dffb5d909b1f6266110a8456
Behzad Joudat <joudat.behzad@gmail.com>
2022-10-01 14:04:02 +0100

    all the documentation for de5-net and design samples as well as the changes we did for the uart

--- hardware/fpga/quartus/DE5-NET/verilog/top_system.v
+++ hardware/fpga/quartus/DE5-NET/verilog/top_system.v
@@ -4,18 +4,35 @@
 module top_system(
 		  input        clk,
 		  input        resetn,
-		  input        PCIE_PERST_n,
-		  input        PCIE_REFCLK_p,
-		  input [7:0]  PCIE_RX_p,
-		  input        PCIE_SMBCLK,
-		  output       PCIE_SMBDAT,
-		  output [7:0] PCIE_TX_p,
-		  output       PCIE_WAKE_n
+		  /*		  input        PCIE_PERST_n,
+		   input        PCIE_REFCLK_p,
+		   input [7:0]  PCIE_RX_p,
+		   input        PCIE_SMBCLK,
+		   output       PCIE_SMBDAT,
+		   output [7:0] PCIE_TX_p,
+		   output       PCIE_WAKE_n,
+		  */		  input uart_rxd,
+		  output       uart_txd,
+		  output reg   rs422_re_n,
+		  output reg   rs422_de,
+		  output reg   rs422_te,
 		  
-  // output [0:0] led_bracket,
+		  output [0:0] led_bracket
   // output [0:0] led_board
 		  
   );
+  
+   wire 		       uart_rts;
+
+   
+   always @* begin
+      rs422_te = 1;
+      rs422_de = 1;
+      rs422_re_n = 0;
+      
+   end
+      
+
    //
    // CLOCK MANAGEMENT
    //
@@ -23,11 +40,12 @@ module top_system(
    //system clock
    wire 			sys_clk = clk;
    // assign led_board[0] = 0;
-   // assign led_bracket[0] = 0;
+    assign led_bracket[0] = 0;
    //
    // RESET MANAGEMENT
    //
 
+   
    //system reset
 
    wire                         sys_rst;
@@ -50,9 +68,9 @@ module top_system(
    		  .rst         (sys_rst),
    		  .trap          (),
    		  //UART
-   		  .uart_txd      (),
-   		  .uart_rxd      (),
-   		  .uart_rts      (),
+   		  .uart_txd      (uart_txd),
+   		  .uart_rxd      (uart_rxd),
+   		  .uart_rts      (uart_rts),
    		  .uart_cts      (1'b1)
    		  );
 
