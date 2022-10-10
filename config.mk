b876eaa1bc7e7503234e28f5e0d33e8b5a9567e3
Behzad Joudat <joudat.behzad@gmail.com>
2022-10-10 12:40:40 +0100

    the changes for pcie repo to do make sim in iob-soc

--- config.mk
+++ config.mk
@@ -36,7 +36,7 @@ INIT_MEM ?=1
 #PERIPHERAL LIST
 #must match respective submodule CORE_NAME in the core.mk file of the submodule
 #PERIPHERALS:=UART
-PERIPHERALS ?=UART
+PERIPHERALS ?=UART PCIE
 
 #RISC-V HARD MULTIPLIER AND DIVIDER INSTRUCTIONS
 USE_MUL_DIV ?=1
@@ -63,7 +63,7 @@ DOC ?= pb
 
 #IOB LIBRARY
 UART_HW_DIR:=$(UART_DIR)/hardware
-
+PCIE_HW_DIR:=$(PCIE_DIR)hardware
 ####################################################################
 # DERIVED FROM PRIMARY PARAMETERS: DO NOT CHANGE BELOW THIS POINT
 ####################################################################
@@ -88,6 +88,7 @@ UART_DIR=$(ROOT_DIR)/submodules/UART
 LIB_DIR=$(ROOT_DIR)/submodules/LIB
 MEM_DIR=$(ROOT_DIR)/submodules/MEM
 AXI_DIR=$(ROOT_DIR)/submodules/AXI
+PCIE_DIR=$(ROOT_DIR)/submodules/PCIE
 
 #sw paths
 SW_DIR:=$(ROOT_DIR)/software
