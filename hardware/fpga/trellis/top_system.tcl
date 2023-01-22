5979e934a0891fc0d2165f93a75d2677d2a570aa
awyxx <robyawyx@gmail.com>
2023-01-22 11:18:59 +0000

    updated trellis top_system script

--- hardware/fpga/trellis/top_system.tcl
+++ hardware/fpga/trellis/top_system.tcl
@@ -5,7 +5,7 @@ set project_name top_system
 # the problem is that ram block isn't being instantiated as BRAM, 
 # but as (many) individual flip-flops per bit.
 # and because of this, the synthesis is taking **literally** ages
-# so if you want to compile the bitstream, you have to comment stuff from sram.v for now
+# so if you want to compile the bitstream, you have to comment stuff from sram.v **for now**
 
 set INCLUDE [lindex $argv 0]
 set DEFINE [lindex $argv 1]
@@ -19,22 +19,24 @@ foreach path [split $INCLUDE \ ] {
     }
 }
 
-#------ Synthesis ------#
+#------ Synthesis (yosys script) ------#
 puts "\n-> Synthesizing design...\n"
 set yosys_script "$project_name.ys"
 set yosys_script_handle [open $yosys_script "w"]
+puts $yosys_script_handle "read -define $DEFINE \n"
 puts $yosys_script_handle "verilog_defaults -add $INCLUDE_PATH \n"
 puts $yosys_script_handle "read_verilog $VSRC \n"
-puts $yosys_script_handle "synth_ecp5 -abc9 -top $project_name -json $project_name.json \n"
+puts $yosys_script_handle "synth_ecp5 -no-rw-check -top $project_name -json $project_name.json \n"
 close $yosys_script_handle
-exec yosys -D $DEFINE -T $yosys_script -t -q -ql "${project_name}_synthesis.log"
+exec yosys -T $yosys_script -t -q -ql "${project_name}_synthesis.log"
 
 #------ Place & Route ------#
 puts "-> Synthesis done! Place & Route now\n"
-exec -ignorestderr nextpnr-ecp5 --ignore-loops --25k --package CABGA256 --speed 6 --freq 25 \
-                  --json $project_name.json --textcfg $project_name.config  \
+exec -ignorestderr nextpnr-ecp5 \
+                    --ignore-loops --25k --package CABGA256 --speed 6 --freq 25 \
+                    --json $project_name.json --textcfg $project_name.config \
                     --lpf $project_name.lpf --lpf-allow-unconstrained -ql "${project_name}_pnr.log"
-# FIXME: $pin_mapping_file should be auto generated for each board revision (6.0/8.0, 7.1, etc..)
+# TODO: $pin_mapping_file should be auto generated for each board revision (6.0/8.0, 7.1, etc..)
 # FIXME: does '--ignore-loops' break the system? 
 
 #------ Bitstream Generation ------#
