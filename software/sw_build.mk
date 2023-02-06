7de41f126b24702207abdf63566b806bc6a8e575
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-02-06 04:40:35 +0000

    feat(software): Append sw_build.mk of Tester to the UUT.

--- software/sw_build.mk
+++ software/sw_build.mk
@@ -3,13 +3,13 @@
 GET_PARAM = $(shell grep $(1) $(2) | rev | cut -d" " -f1 | rev)
 
 #Function to obtain parameter named $(1) from iob_soc_tester_conf.vh
-GET_CONF_PARAM = $(call GET_PARAM,$(1),../src/iob_soc_tester_conf.vh)
+GET_TESTER_CONF_PARAM = $(call GET_PARAM,IOB_SOC_TESTER_$(1),../src/iob_soc_tester_conf.vh)
 
 iob_soc_tester_boot.hex: ../../software/embedded/iob_soc_tester_boot.bin
-	../../scripts/makehex.py $< $(call GET_CONF_PARAM,BOOTROM_ADDR_W) > $@
+	../../scripts/makehex.py $< $(call GET_TESTER_CONF_PARAM,BOOTROM_ADDR_W) > $@
 
 iob_soc_tester_firmware.hex: iob_soc_tester_firmware.bin
-	../../scripts/makehex.py $< $(call GET_CONF_PARAM,SRAM_ADDR_W) > $@
+	../../scripts/makehex.py $< $(call GET_TESTER_CONF_PARAM,SRAM_ADDR_W) > $@
 	../../scripts/hex_split.py iob_soc_tester_firmware .
 
 iob_soc_tester_firmware.bin: ../../software/embedded/iob_soc_tester_firmware.bin
--- software/sw_setup.py
+++ software/sw_setup.py
@@ -14,3 +14,19 @@ if not filecmp.cmp(src_file, dst_file):
 
     f1.close()
     f2.close()
+
+
+src_file = setup_module.setup_dir+"/software/sw_build.mk"
+dst_file = setup_module.build_dir+"/software/sw_build.mk"
+
+# Make sure files are not equal
+# They may be equal if the python scripts already copied it
+if not filecmp.cmp(src_file, dst_file):
+    # Append the Tester's sw_build.mk to the top system/core's sw_build.mk
+    f1 = open(src_file, 'r')
+    f2 = open(dst_file, 'a+')    
+
+    f2.write(f1.read())
+
+    f1.close()
+    f2.close()
