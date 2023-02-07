1ce67aec7f999a7fd0766d9e4e8783cb7ea370c6
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2023-02-07 12:08:49 +0000

    progress on FPGA building; stuck at INIT_MEM=0 and RUN_EXTMEM=0

--- hardware/fpga/quartus/postmap.tcl
+++ hardware/fpga/quartus/postmap.tcl
@@ -1,4 +1,4 @@
-if { $USE_EXTMEM > 0 } {
+if { $USE_EXTMEM == "1" } {
     source "quartus/$BOARD/alt_ddr3/synthesis/submodules/alt_ddr3_mem_if_ddr3_emif_0_p0_parameters.tcl"
 
     source "quartus/$BOARD/alt_ddr3/synthesis/submodules/alt_ddr3_mem_if_ddr3_emif_0_p0_pin_assignments.tcl"
