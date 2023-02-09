f436176879203c6243e76bc45e18748d67801b99
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-02-09 16:24:49 +0000

    feat(sim_setup): Prevent duplicate includes/targets in sim_build.mk

--- hardware/simulation/sim_setup.py
+++ hardware/simulation/sim_setup.py
@@ -35,14 +35,24 @@ dst_file = setup_module.build_dir+"/hardware/simulation/sim_build.mk"
 # Make sure files are not equal
 # They may be equal if the python scripts already copied it
 if not filecmp.cmp(src_file, dst_file):
+    # Read tester sim_build.mk
+    with open(src_file, 'r') as file:
+        tester_sim_build = file.readlines()
+    # Read currently existing sim_build.mk
+    with open(dst_file, 'r') as file:    
+        existing_sim_build = file.readlines()
+    # Remove include sw_build.mk from tester sim_build.mk if the existing sim_build.mk already has it
+    if "include ../../software/sw_build.mk\n" in existing_sim_build:
+        tester_sim_build.remove("include ../../software/sw_build.mk\n")
+    # Remove iob_uart_swreg.h target if it already exists
+    if "iob_uart_swreg.h: ../../software/esrc/iob_uart_swreg.h\n" in existing_sim_build:
+        idx = tester_sim_build.index("iob_uart_swreg.h: ../../software/esrc/iob_uart_swreg.h\n")
+        # Remove 2 lines, as the current target iob_uart_swreg.h is defined by two lines
+        tester_sim_build.pop(idx)
+        tester_sim_build.pop(idx)
     # Append the Tester's sim_build.mk to the top system/core's sim_build.mk
-    f1 = open(src_file, 'r')
-    f2 = open(dst_file, 'a+')    
-
-    f2.write(f1.read())
-
-    f1.close()
-    f2.close()
+    with open(dst_file, 'w') as file:    
+        file.writelines(existing_sim_build+tester_sim_build)
 
 #Override 'NAME' makefile variable with tester name
 #This is useful when we are building the tester with another core that may have set NAME variable.
