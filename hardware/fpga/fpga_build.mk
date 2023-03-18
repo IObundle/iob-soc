da8fd457b1279672b4269817d15c3c934520c9cd
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2023-03-18 01:33:10 +0000

    include uut_build.mk in sim and fpga

--- hardware/fpga/fpga_build.mk
+++ hardware/fpga/fpga_build.mk
@@ -3,15 +3,10 @@ include ../../software/sw_build.mk
 
 IS_FPGA=1
 
-
 QUARTUS_SEED=10
 
-TEST_LIST:=test1
-test1:
-	make -C ../../ fw-clean BOARD=$(BOARD)
-	make -C ../../ fpga-clean BOARD=$(BOARD)
-	make run BOARD=$(BOARD)
-	diff run.log $(FPGA_TOOL)/$(BOARD)/test.expected
-
-# Undefine FPGA_TOP var in case it was set by UUT.
+# Undefine FPGA_TOP, as it was set by UUT.
 undefine FPGA_TOP
+
+
+include uut_build.mk
--- hardware/simulation/sim_build.mk
+++ hardware/simulation/sim_build.mk
@@ -18,6 +18,5 @@ endif
 
 CONSOLE_CMD=../../scripts/console.py -L
 
-TEST_LIST+=test1
-test1:
-	make -C ../../ fw-clean SIMULATOR=$(SIMULATOR) && make -C ../../ sim-clean SIMULATOR=$(SIMULATOR) && make run SIMULATOR=$(SIMULATOR) TEST_LOG="| tee -a test.log"
+
+include uut_build.mk
