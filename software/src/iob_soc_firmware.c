837ffb9c9d6af4dea0a180c8d8e2f515046bbe9f
PMiranda <pmiranda.iobundle@gmail.com>
2023-05-09 11:24:09 +0100

    feat(format): update LIB. Add clang format - update LIB submodule - add .clang-format configuration file to repository - format C/C++ files

--- software/src/iob_soc_firmware.c
+++ software/src/iob_soc_firmware.c
@@ -1,14 +1,14 @@
 #include "bsp.h"
-#include "iob_soc_system.h"
-#include "iob_soc_periphs.h"
-#include "iob_soc_conf.h"
 #include "iob-uart.h"
-#include "printf.h"
+#include "iob_soc_conf.h"
+#include "iob_soc_periphs.h"
+#include "iob_soc_system.h"
 #include "iob_str.h"
+#include "printf.h"
 
 char *send_string = "Sending this string as a file to console.\n"
                     "The file is then requested back from console.\n"
-                    "The sent file is compared to the received file to confirm " 
+                    "The sent file is compared to the received file to confirm "
                     "correct file transfer via UART using console.\n"
                     "Generating the file in the firmware creates an uniform "
                     "file transfer between pc-emul, simulation and fpga without"
@@ -17,71 +17,70 @@ char *send_string = "Sending this string as a file to console.\n"
 // copy src to dst
 // return number of copied chars (excluding '\0')
 int string_copy(char *dst, char *src) {
-    if (dst == NULL || src == NULL) {
-        return -1;
-    }
-    int cnt = 0;
-    while(src[cnt] != 0){
-        dst[cnt] = src[cnt];
-        cnt++;
-    }
-    dst[cnt] = '\0';
-    return cnt;
+  if (dst == NULL || src == NULL) {
+    return -1;
+  }
+  int cnt = 0;
+  while (src[cnt] != 0) {
+    dst[cnt] = src[cnt];
+    cnt++;
+  }
+  dst[cnt] = '\0';
+  return cnt;
 }
 
 // 0: same string
 // otherwise: different
 int compare_str(char *str1, char *str2, int str_size) {
-    int c = 0;
-    while(c < str_size) {
-        if (str1[c] != str2[c]){
-            return str1[c] - str2[c];
-        }
-        c++;
+  int c = 0;
+  while (c < str_size) {
+    if (str1[c] != str2[c]) {
+      return str1[c] - str2[c];
     }
-    return 0;
+    c++;
+  }
+  return 0;
 }
 
-int main()
-{
-char pass_string[] = "Test passed!";
-char fail_string[] = "Test failed!";
+int main() {
+  char pass_string[] = "Test passed!";
+  char fail_string[] = "Test failed!";
 
-  //init uart
-  uart_init(UART0_BASE,FREQ/BAUD);   
+  // init uart
+  uart_init(UART0_BASE, FREQ / BAUD);
 
-  //test puts
+  // test puts
   uart_puts("\n\n\nHello world!\n\n\n");
 
-  //test printf with floats 
+  // test printf with floats
   printf("Value of Pi = %f\n\n", 3.1415);
 
-  //test file send
+  // test file send
   char *sendfile = malloc(1000);
   int send_file_size = 0;
   send_file_size = string_copy(sendfile, send_string);
   uart_sendfile("Sendfile.txt", send_file_size, sendfile);
 
-  //test file receive
+  // test file receive
   char *recvfile = malloc(10000);
   int file_size = 0;
   file_size = uart_recvfile("Sendfile.txt", recvfile);
 
-  //compare files
+  // compare files
   if (compare_str(sendfile, recvfile, send_file_size)) {
-      printf("FAILURE: Send and received file differ!\n");
+    printf("FAILURE: Send and received file differ!\n");
   } else {
-      printf("SUCCESS: Send and received file match!\n");
+    printf("SUCCESS: Send and received file match!\n");
   }
 
   free(sendfile);
   free(recvfile);
 
-//#ifdef USE_EXTMEM
-//  if(memory_access_failed)
-//      uart_sendfile("test.log", iob_strlen(fail_string), fail_string);
-//      uart_finish();
-//#endif
+  //#ifdef USE_EXTMEM
+  //  if(memory_access_failed)
+  //      uart_sendfile("test.log", iob_strlen(fail_string), fail_string);
+  //      uart_finish();
+  //#endif
   uart_sendfile("test.log", iob_strlen(pass_string), pass_string);
 
   uart_finish();
