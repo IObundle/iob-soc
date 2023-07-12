bc9928b927f4af533ad5e3387247fc7e5d8ec0af
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-07-12 04:30:02 +0100

    feat(python): Update python methods according to new LIB

--- iob_gpio.py
+++ iob_gpio.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 import os
-import sys
 
 from iob_module import iob_module
-from setup import setup
 
 # Submodules
 from iob_reg import iob_reg
@@ -17,26 +15,15 @@ class iob_gpio(iob_module):
     setup_dir=os.path.dirname(__file__)
 
     @classmethod
-    def _run_setup(cls):
+    def _specific_setup(cls):
         # Hardware headers & modules
         iob_module.generate("iob_s_port")
         iob_module.generate("iob_s_portmap")
         iob_reg.setup()
         iob_reg_e.setup()
 
-        cls._setup_confs()
-        cls._setup_ios()
-        cls._setup_regs()
-        cls._setup_block_groups()
-
         # Verilog modules instances
         # TODO
-
-        # Copy sources of this module to the build directory
-        super()._run_setup()
-
-        # Setup core using LIB function
-        setup(cls)
 
 
     @classmethod
