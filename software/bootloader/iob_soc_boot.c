cda77889c1345f6b44a7a8a1b9cb4fb751b54e19
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2023-08-01 12:44:38 +0100

    update files

--- software/bootloader/iob_soc_boot.c
+++ software/bootloader/iob_soc_boot.c
@@ -1,7 +1,7 @@
 #include "build_configuration.h"
+#include "iob-uart.h"
 #include "iob_soc_conf.h"
 #include "iob_soc_periphs.h"
-#include "iob-uart.h"
 
 #ifdef USE_EXTMEM
 #include "iob-cache.h"
@@ -11,54 +11,54 @@
 
 int main() {
 
-  //init uart 
-  uart_init(UART0_BASE, FREQ/BAUD);
+  // init uart
+  uart_init(UART0_BASE, FREQ / BAUD);
 
-  //connect with console
-   do {
-    if(IOB_UART_GET_TXREADY())
-      uart_putc((char) ENQ);
-  } while(!IOB_UART_GET_RXREADY());
+  // connect with console
+  do {
+    if (IOB_UART_GET_TXREADY())
+      uart_putc((char)ENQ);
+  } while (!IOB_UART_GET_RXREADY());
 
-  //welcome message
-  uart_puts (PROGNAME);
-  uart_puts (": connected!\n");
-    
+  // welcome message
+  uart_puts(PROGNAME);
+  uart_puts(": connected!\n");
+
 #ifdef USE_EXTMEM
-  uart_puts (PROGNAME);
+  uart_puts(PROGNAME);
   uart_puts(": System is using external memory\n");
 #endif
 
   // sync with console
-  while(uart_getc() != ACK){
-    uart_puts (PROGNAME);
+  while (uart_getc() != ACK) {
+    uart_puts(PROGNAME);
     uart_puts(": Waiting for Console ACK.\n");
   }
 
   // address to copy firmware to
-  char *prog_start_addr = (char *) (1<<31);
-   
+  char *prog_start_addr = (char *)(1 << 31);
+
 #ifndef INIT_MEM
-  //receive firmware from host 
+  // receive firmware from host
   int file_size = 0;
   char r_fw[] = "iob_soc_firmware.bin";
   file_size = uart_recvfile(r_fw, prog_start_addr);
-  uart_puts (PROGNAME);
-  uart_puts (": Loading firmware...\n");
+  uart_puts(PROGNAME);
+  uart_puts(": Loading firmware...\n");
 
-  if(file_size == 0) {
-    uart_puts (PROGNAME);
-    uart_puts (": Error, file size is 0.\n");
+  if (file_size == 0) {
+    uart_puts(PROGNAME);
+    uart_puts(": Error, file size is 0.\n");
   }
 #endif
-  
-  //run firmware
-  uart_puts (PROGNAME);
-  uart_puts (": Reset CPU to run user program...\n");
+
+  // run firmware
+  uart_puts(PROGNAME);
+  uart_puts(": Reset CPU to run user program...\n");
   uart_txwait();
 
 #ifdef USE_EXTMEM
-  while( !cache_wtb_empty() );
+  while (!cache_wtb_empty())
+    ;
 #endif
-  
 }
