f7d0c692565e53e73fd622ba91fe2c5bf6b5774e
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-11-27 20:04:18 +0000

    feat(bsp): Move simulation/bsp.vh and software/bsp.h to the `src/` directory.

--- submodules/LIB/build.mk
+++ submodules/LIB/build.mk
@@ -5,7 +5,7 @@
 
 include config_build.mk
 
-BSP_H ?= software/bsp.h
+BSP_H ?= software/src/bsp.h
 SIM_DIR = hardware/simulation
 BOARD_DIR = $(shell find -name $(BOARD) -type d -print -quit)
 
@@ -16,7 +16,7 @@ $(BSP_H):
 	if [[ $(MAKECMDGOALS) == *fpga* ]]; then \
 		cp $(BOARD_DIR)/bsp.vh $@; \
 	else \
-		cp $(SIM_DIR)/bsp.vh $@; \
+		cp $(SIM_DIR)/src/bsp.vh $@; \
 	fi; sed -i 's/`/#/g' $@;
 
 
