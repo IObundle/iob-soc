553ece1d69325e234cda9e287db4727376099600
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2023-12-09 11:46:06 +0000

    rewrite edge detect to use iob_reg

--- submodules/LIB/hardware/modules/iob_edge_detect/hardware/src/iob_edge_detect.v
+++ submodules/LIB/hardware/modules/iob_edge_detect/hardware/src/iob_edge_detect.v
@@ -3,63 +3,68 @@
 module iob_edge_detect
 #(
   parameter EDGE_TYPE = "rising", // "rising", "falling", "both"
-  parameter OUT_TYPE = "pulse" // "step", "pulse"
+  parameter OUT_TYPE = "step" // "step", "pulse"
 ) (
 `include "clk_en_rst_s_port.vs"
    input rst_i,
    input  bit_i,
    output detected_o
 );
-
    wire   bit_int;
-   reg    bit_int_q;
+   wire   bit_int_q;
+   wire   detected_nxt;
 
    generate
-      if (EDGE_TYPE == "rising") begin : gen_rising
-         assign bit_int = bit_i;
-      end else if (EDGE_TYPE == "falling") begin : gen_falling
+      if (EDGE_TYPE == "falling") begin : gen_falling
          assign bit_int = ~bit_i;
+         iob_reg_r   
+           #(
+             .DATA_W(1),
+             .RST_VAL(1'b1)
+             )
+         bit_reg
+           (
+`include "clk_en_rst_s_s_portmap.vs"
+            .rst_i(rst_i),
+            .data_i(bit_int),
+            .data_o(bit_int_q)
+            );
+         
       end else begin : gen_default_rising
          assign bit_int = bit_i;
+
+         iob_reg_r   
+           #(
+             .DATA_W(1),
+             .RST_VAL(1'b0)
+             )
+         bit_reg
+           (
+`include "clk_en_rst_s_s_portmap.vs"
+            .rst_i(rst_i),
+            .data_i(bit_int),
+            .data_o(bit_int_q)
+            );
       end
    endgenerate
 
-   reg enabled;
-
-   generate if (OUT_TYPE == "pulse") begin : gen_pulse
-      assign detected_o = bit_int & ((~bit_int_q) & enabled);
-   end else if (OUT_TYPE == "step") begin : gen_step
-      reg detected_q;
-      assign detected_o = detected_q | (bit_int & ((~bit_int_q) & enabled));
-
-      always @(posedge clk_i, posedge arst_i) begin
-         if(arst_i) begin
-            detected_q <= 1'b0;
-         end else if (cke_i) begin
-            if(rst_i) begin
-               detected_q <= 1'b0;
-            end else begin
-               detected_q <= detected_o;
-            end
-         end
+   generate 
+      if (OUT_TYPE == "pulse") begin : gen_pulse
+         assign detected_nxt = bit_int & ~bit_int_q;
+      end else  begin : gen_step
+         assign detected_nxt = detected_o | (bit_int & ~bit_int_q);
       end
-   end else begin
-      assign detected_o = 1'b0;
-   end endgenerate
+   endgenerate
 
-   always @(posedge clk_i, posedge arst_i) begin
-      if(arst_i) begin
-         bit_int_q <= 1'b0;
-         enabled <= 1'b0;
-      end else if (cke_i) begin
-         if(rst_i) begin
-            bit_int_q <= 1'b0;
-            enabled <= 1'b0;
-         end else begin
-            bit_int_q <= bit_int;
-            enabled <= 1'b1;
-         end
-      end
-   end
+   iob_reg_r
+     #(
+       .DATA_W(1)
+       ) detected_reg 
+       (
+`include "clk_en_rst_s_s_portmap.vs"
+        .rst_i(rst_i),
+        .data_i(detected_nxt),
+        .data_o(detected_o)
+        );
 
 endmodule
