51b48e45a52c3373ff1b22c02810bc27f76f22f3
PMiranda <pmiranda.iobundle@gmail.com>
2023-12-13 15:55:22 +0000

    fix(reset_sync): simplify reset_sync

--- submodules/LIB/hardware/modules/sync/iob_reset_sync/hardware/src/iob_reset_sync.v
+++ submodules/LIB/hardware/modules/sync/iob_reset_sync/hardware/src/iob_reset_sync.v
@@ -11,34 +11,26 @@ module iob_reset_sync (
    wire [1:0] sync;
 
    localparam RST_POL = `IOB_REG_RST_POL;
+   localparam RST_VAL = `IOB_REG_RST_POL ? 2'd3 : 2'd0;
 
    generate
       if (RST_POL == 0) begin: gen_rst_pol_0
          assign data = {sync[0], 1'b1};
-         iob_r #(
-               .DATA_W  (2),
-               .RST_VAL (2'd0)
-               )     
-         reg1 (
-               .clk_i (clk_i),
-               .arst_i (arst_i),
-               .data_i(data),
-               .data_o(sync)
-               );
       end else begin: gen_rst_pol_1
          assign data = {sync[0], 1'b0};
-         iob_r #(
-               .DATA_W  (2),
-               .RST_VAL (2'd3)
-               )     
-         reg1 (
-               .clk_i (clk_i),
-               .arst_i (arst_i),
-               .data_i(data),
-               .data_o(sync)
-               );
       end
    endgenerate
+
+   iob_r #(
+       .DATA_W  (2),
+       .RST_VAL (RST_VAL)
+       )     
+   reg1 (
+       .clk_i (clk_i),
+       .arst_i (arst_i),
+       .data_i(data),
+       .data_o(sync)
+       );
    
    assign arst_o = sync[1];
    
