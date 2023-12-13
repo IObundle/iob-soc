c147247c0db24180c89cc80a7a7e95d1971c8b1b
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2023-12-13 22:48:23 +0000

    feat: add suffixes to ios

--- hardware/src/iob_gpio.v
+++ hardware/src/iob_gpio.v
@@ -11,7 +11,7 @@ module iob_gpio # (
 
    `include "iob_wire.vs"
 
-   assign iob_avalid = iob_avalid_i;
+   assign iob_valid = iob_valid_i;
    assign iob_addr = iob_addr_i;
    assign iob_wdata = iob_wdata_i;
    assign iob_wstrb = iob_wstrb_i;
