0aae5b2dc52f66ee85a29f660da5e99bd9870037
PMiranda <pmiranda.iobundle@gmail.com>
2023-12-13 14:55:40 +0000

    fix(fpga): cyclonev fpga wrapper, reset_sync - only declare clk_interconnect and arst_interconnect signals if   Cyclonev is using external memory - fix reset sync module:     - declare sync as `wire` instead of `reg`     - instantiate `iob_r` with correct `RST_VAL` in each `RST_POL` case

--- submodules/LIB/hardware/modules/sync/iob_reset_sync/hardware/src/iob_reset_sync.v
+++ submodules/LIB/hardware/modules/sync/iob_reset_sync/hardware/src/iob_reset_sync.v
@@ -8,29 +8,38 @@ module iob_reset_sync (
 );
 
    wire [1:0] data;
-   reg [1:0] sync;
+   wire [1:0] sync;
 
    localparam RST_POL = `IOB_REG_RST_POL;
 
    generate
       if (RST_POL == 0) begin: gen_rst_pol_0
          assign data = {sync[0], 1'b1};
+         iob_r #(
+               .DATA_W  (2),
+               .RST_VAL (2'd0)
+               )     
+         reg1 (
+               .clk_i (clk_i),
+               .arst_i (arst_i),
+               .data_i(data),
+               .data_o(sync)
+               );
       end else begin: gen_rst_pol_1
          assign data = {sync[0], 1'b0};
+         iob_r #(
+               .DATA_W  (2),
+               .RST_VAL (2'd3)
+               )     
+         reg1 (
+               .clk_i (clk_i),
+               .arst_i (arst_i),
+               .data_i(data),
+               .data_o(sync)
+               );
       end
    endgenerate
    
-   iob_r #(
-           .DATA_W  (2),
-           .RST_VAL (2'b0)
-           )     
-   reg1 (
-         .clk_i (clk_i),
-         .arst_i (arst_i),
-         .data_i(data),
-         .data_o(sync)
-         );
-
    assign arst_o = sync[1];
    
 endmodule
