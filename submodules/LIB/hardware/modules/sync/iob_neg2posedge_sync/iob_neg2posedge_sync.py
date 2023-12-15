91a138a6282ed4b57852f75cb6f80b87abbe07e8
AndreMerendeira <andre.merendeira@gmail.com>
2023-12-15 16:01:23 +0000

    [neg2posedge] fixed missing include of reg and regn. Formated file

--- submodules/LIB/hardware/modules/sync/iob_neg2posedge_sync/iob_neg2posedge_sync.py
+++ submodules/LIB/hardware/modules/sync/iob_neg2posedge_sync/iob_neg2posedge_sync.py
@@ -2,8 +2,8 @@ import os
 
 from iob_module import iob_module
 
-from iob_r import iob_r
-from iob_rn import iob_rn
+from iob_reg import iob_reg
+from iob_regn import iob_regn
 
 
 class iob_neg2posedge_sync(iob_module):
@@ -17,7 +17,7 @@ class iob_neg2posedge_sync(iob_module):
         super()._create_submodules_list(
             [
                 {"interface": "clk_rst_s_port"},
-                iob_r,
-                iob_rn,
+                iob_reg,
+                iob_regn,
             ]
         )
