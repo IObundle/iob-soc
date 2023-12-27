95999af2a443246ad81e799d0c3bc243f96a4529
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2023-12-27 17:37:57 +0000

    add soft reset function

--- axistream_out/software/src/iob-axistream-out.c
+++ axistream_out/software/src/iob-axistream-out.c
@@ -1,5 +1,10 @@
 #include "iob-axistream-out.h"
 
+void iob_axis_out_reset() {
+  IOB_AXISTREAM_OUT_SET_SOFT_RESET(1);
+  IOB_AXISTREAM_OUT_SET_SOFT_RESET(0);
+}
+
 uint32_t iob_axis_write(uint32_t value) {
   if (IOB_AXISTREAM_OUT_GET_FIFO_FULL()) {
     return 0;
