9376c3ed7a1c9f32d3ddebbb44d0fa31cedcf94b
vasco <vascoalexluz2@gmail.com>
2024-01-09 18:08:05 +0000

    updating the python file

--- submodules/LIB/scripts/caravel_setup.py
+++ submodules/LIB/scripts/caravel_setup.py
@@ -2,7 +2,25 @@ import os
 import shutil
 import sys
 import json
+import re
 import importlib
+
+
+def find_module_instantiations(verilog_file_path):
+    module_names = []
+    module_pattern = r"^(?!module).*\b\w+\s*#\s*\([\s\S]*?;\s*$"
+    with open(verilog_file_path, "r") as file:
+        verilog_code = file.read()
+        # Search for module instantiations using regex
+        matches = re.findall(module_pattern, verilog_code, re.MULTILINE)
+        # Extract module names from the matches
+        for match in matches:
+            # Capture the module name pattern
+            module_name = match.split()[0] + ".v"
+            module_names.append(module_name)
+    return module_names
+
+
 
 
 if len(sys.argv) >= 2:
@@ -38,60 +56,39 @@ target_path = os.path.join(target_path, "verilog", "rtl")
 open_lane_dir = os.path.join(build_dir, "caravel_project", "openlane")
 user_proj_dir = os.path.join(open_lane_dir, "user_proj_example")
 
+
+required_modules = []
+temporary_models = []
+
+
 if os.path.exists(source_path):
     v_top_modules = [file for file in os.listdir(source_path) if file.endswith(".v")]
+    print("hamburgues")
     # Copy each '.v' file from source_path to target_path
     for file_name in v_top_modules:
+
         source_file = os.path.join(source_path, file_name)
         target_file = os.path.join(target_path, file_name)
         shutil.copyfile(source_file, target_file)
         module_name = os.path.splitext(file_name)[0]
+
         temporary_dir = os.path.join(open_lane_dir, module_name)
         iob_soc_src_path = os.path.join(build_dir, "hardware", "src")
         iob_bp = os.path.join(build_dir, "hardware", "simulation", "src")
+
         v_files_sim = [
-            file
-            for file in os.listdir(iob_bp)
-            if file.endswith((".v", ".vh")) and file != "iob_soc_tb.v"
+            file for file in os.listdir(iob_bp) if file.endswith((".v", ".vh"))
         ]
+
         for file_name in v_files_sim:
             source_file_path = os.path.join(iob_bp, file_name)
             destination_file_path = os.path.join(iob_soc_src_path, file_name)
             # Copy the file from source to destination
             shutil.copy(source_file_path, destination_file_path)
-        v_files = [
-            file
-            for file in os.listdir(iob_soc_src_path)
-            if file.endswith((".v", ".vh")) and "ram" not in file
-        ]
-        # now it is made just for two directoried, but this can be done as the bootstrap, and import everithing
 
-        if not os.path.exists(temporary_dir):
-            os.makedirs(temporary_dir)
-            print(f"Directory '{temporary_dir}' created.")
-            # Copy contents of user_proj_example to temporary_dir
-            for item in os.listdir(user_proj_dir):
-                s = os.path.join(user_proj_dir, item)
-                d = os.path.join(temporary_dir, item)
-                if os.path.isdir(s):
-                    shutil.copytree(s, d, symlinks=True)
-                else:
-                    shutil.copy2(s, d)
-            json_temp = os.path.join(temporary_dir, "config.json")
-            if os.path.exists(json_temp):
-                with open(json_temp, "r") as json_file:
-                    data = json.load(json_file)
-                    data["DESIGN_NAME"] = module_name
-                    data["VERILOG_FILES"] = [
-                        file
-                        for file in data["VERILOG_FILES"]
-                        if "user_proj_example.v" not in file
-                    ]
-                    data["VERILOG_FILES"].append(target_file)
-                    for verig in v_files:
-                        temp = os.path.join(iob_soc_src_path, verig)
-                        data["VERILOG_FILES"].append(temp)
-                with open(json_temp, "w") as json_file:
-                    json.dump(data, json_file, indent=4)
-        else:
-            print("Temporary directory already exists.")
+        temporary_models = find_module_instantiations(target_file)
+
+        while temporary_models != []:
+            destination_file_path = os.path.join(iob_soc_src_path, file_name)
+            print(temporary_models)
+            temporary_models = []
