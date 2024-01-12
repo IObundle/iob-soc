c944fb37d65b9d3853d8d24682554b8aceec1c0b
AndreMerendeira <andre.merendeira@gmail.com>
2024-01-12 13:54:27 +0000

    [top] removed deprecated iob IF connections

--- submodules/LIB/hardware/modules/iob_timer/hardware/src/iob_timer.v
+++ submodules/LIB/hardware/modules/iob_timer/hardware/src/iob_timer.v
@@ -10,14 +10,6 @@ module iob_timer #(
 
    `include "iob_wire.vs"
 
-   assign iob_valid = iob_valid_i;
-   assign iob_addr = iob_addr_i;
-   assign iob_wdata = iob_wdata_i;
-   assign iob_wstrb = iob_wstrb_i;
-   assign iob_rvalid_o = iob_rvalid;
-   assign iob_rdata_o = iob_rdata;
-   assign iob_ready_o = iob_ready;
-
    //Dummy iob_ready_nxt_o and iob_rvalid_nxt_o to be used in swreg (unused ports)
    wire iob_ready_nxt;
    wire iob_rvalid_nxt;
