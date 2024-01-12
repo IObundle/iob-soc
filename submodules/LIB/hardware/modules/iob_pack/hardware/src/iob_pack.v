2d0cd1d0b6a8a3950dd28f6d026de4badf19e9aa
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2024-01-12 20:24:05 +0000

    progress pack/unpack

--- submodules/LIB/hardware/modules/iob_pack/hardware/src/iob_pack.v
+++ submodules/LIB/hardware/modules/iob_pack/hardware/src/iob_pack.v
@@ -1,63 +1,63 @@
 `timescale 1ns / 1ps
 
 module iob_pack #(
-   parameter R_DATA_W = 21,
-   parameter W_DATA_W = 21
+   parameter RDATA_W = 21,
+   parameter WDATA_W = 21
 ) (
 `include "clk_en_rst_s_port.vs"
    input                      rst_i,
    
-   input [$clog2(R_DATA_W):0] word_width_i,
+   input [$clog2(RDATA_W):0] word_width_i,
    input                      wrap_i,
 
-   input [ W_DATA_W-1:0]      r_data_i,
+   input [ WDATA_W-1:0]      r_data_i,
    input                      r_ready_i,
    output                     r_read_o,
 
-   output [R_DATA_W-1:0]      w_data_o,
+   output [RDATA_W-1:0]      w_data_o,
    output                     w_write_o,
    input                      w_ready_i
    );
+
+   //read data word width with the right number of bits
+   localparam [$clog2(RDATA_W):0] RDATA_W_INT = {1'b1, {$clog2(RDATA_W){1'b0}}};
 
    //read and write fifos
    reg read_fifo;
    reg write_fifo;
 
    // word register
-   wire [(2*R_DATA_W)-1:0] data;
-   reg [(2*R_DATA_W)-1:0]  data_nxt;
+   wire [(2*RDATA_W)-1:0] data;
+   reg [(2*RDATA_W)-1:0]  data_nxt;
    
    
    // shift data to write and read
-   wire [2*W_DATA_W-1:0]   w_data_shifted;
+   wire [2*RDATA_W-1:0]   data_shifted;
    
    //program counter (fsm state)
    reg [1:0]               pcnt_nxt;
    wire [1:0]              pcnt;
    
    //word width accumulator
-   wire [$clog2(R_DATA_W):0] acc;
-   reg [$clog2(R_DATA_W):0]  acc_nxt;
+   wire [$clog2(RDATA_W):0] acc;
+   reg [$clog2(RDATA_W):0]  acc_nxt;
    
    //shift value
-   wire [$clog2(R_DATA_W)-1:0] shift_val;
-   reg [$clog2(R_DATA_W)-1:0]  shift_val_nxt;
-   
+   wire [$clog2(RDATA_W)-1:0] shift_val;
    
    //output data
-   assign w_data_shifted = data >> (wrap_i? 1'b0: shift_val);
-   assign w_data_o = w_data_shifted[R_DATA_W-1-:W_DATA_W];
+   assign data_shifted = data >> (wrap_i? 1'b0: shift_val);
+   assign w_data_o = data_shifted[RDATA_W-1-:WDATA_W];
    assign w_write_o = write_fifo;
    assign r_read_o = read_fifo; 
-   
-   
+   assign shift_val = acc - RDATA_W_INT;
+
    //program
    always @* begin
 
       pcnt_nxt = pcnt + 1'b1;
       read_fifo = 1'b0;
       data_nxt = data;
-      shift_val_nxt = shift_val;
       write_fifo = 1'b0;
       acc_nxt = acc;
 
@@ -71,36 +71,39 @@ module iob_pack #(
            end
         end
 
-        1: begin //shift and load data until word width is reached
-           if (acc < W_DATA_W ) begin
+        default: begin
+           if ( acc_nxt < RDATA_W_INT ) begin
               acc_nxt = acc + word_width_i;
-              data_nxt = (data << word_width_i) | r_data_i;           
-              pcnt_nxt = 2'd0;
+              data_nxt = (data << word_width_i) | r_data_i;
+              //read data from input FIFO
+              if (!r_ready_i) begin
+                 pcnt_nxt = 0;
+              end else begin
+                 read_fifo = 1'b1;
+                 pcnt_nxt = pcnt;
+              end
            end else begin
-              //restart accumulator
-              acc_nxt =  wrap_i? {$clog2(R_DATA_W)+1{1'b0}}: acc -(1'b1 << $clog2(R_DATA_W));
-              //save shift value
-              shift_val_nxt = (wrap_i? {$clog2(R_DATA_W){1'b0}} : acc -(1'b1 << $clog2(R_DATA_W)));
+               //write data to output FIFO
+               if (!w_ready_i) begin
+                  pcnt_nxt = pcnt;
+               end else begin
+                  write_fifo = 1'b1;
+                  //(re)start accumulator
+                  acc_nxt =  wrap_i? {$clog2(RDATA_W)+1{1'b0}} : acc - RDATA_W_INT;
+                  pcnt_nxt = pcnt;
+               end
            end
-        end
-
-        default: begin //write data to output FIFO
-           if (!w_ready_i) begin
-              pcnt_nxt = pcnt;
-           end else begin
-              write_fifo = 1'b1;
-              pcnt_nxt = 2'd1; //go process next word
-           end
-        end
+         end
       endcase
       
-   end
+   end // always @ *
+   
 
    
    //word width accumulator register
    iob_reg_r #(
-      .DATA_W ($clog2(R_DATA_W)+1),
-      .RST_VAL({$clog2(R_DATA_W)+1{1'b0}})
+      .DATA_W ($clog2(RDATA_W)+1),
+      .RST_VAL({$clog2(RDATA_W)+1{1'b0}})
    ) acc_reg (
 `include "clk_en_rst_s_s_portmap.vs"
       .rst_i     (rst_i),
@@ -110,8 +113,8 @@ module iob_pack #(
 
    //data word register
    iob_reg_r #(
-      .DATA_W(2*R_DATA_W),
-      .RST_VAL({2*R_DATA_W{1'b0}})
+      .DATA_W(2*RDATA_W),
+      .RST_VAL({2*RDATA_W{1'b0}})
    ) data_reg_inst (
 `include "clk_en_rst_s_s_portmap.vs"
       .rst_i(rst_i),
@@ -130,17 +133,6 @@ module iob_pack #(
       .data_o(pcnt)
    );
    
-    //shift value register
-   iob_reg_r #(
-      .DATA_W($clog2(R_DATA_W)),
-      .RST_VAL({$clog2(R_DATA_W){1'b0}})
-   ) shift_val_reg_inst (
-`include "clk_en_rst_s_s_portmap.vs"
-      .rst_i(rst_i),
-      .data_i(shift_val_nxt),
-      .data_o(shift_val)
-   );
-
 endmodule
 
  
--- submodules/LIB/hardware/modules/iob_unpack/hardware/src/iob_unpack.v
+++ submodules/LIB/hardware/modules/iob_unpack/hardware/src/iob_unpack.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 
 module iob_unpack #(
-   parameter W_DATA_W = 21,
-   parameter R_DATA_W = 21,
+   parameter WDATA_W = 21,
+   parameter RDATA_W = 21,
    parameter WORD_W = 21
 ) (
 `include "clk_en_rst_s_port.vs"
@@ -11,45 +11,47 @@ module iob_unpack #(
    input [WORD_W:0]      word_width_i,
    input                 wrap_i,
 
-   input [ R_DATA_W-1:0] r_data_i,
+   input [ RDATA_W-1:0] r_data_i,
    input                 r_ready_i,
    output                r_read_o,
 
-   output [W_DATA_W-1:0] w_data_o,
+   output [WDATA_W-1:0] w_data_o,
    output                w_write_o,
    input                 w_ready_i
    );
+
+   //read data word width with the right number of bits
+   localparam [$clog2(RDATA_W):0] RDATA_W_INT = {1'b1, {$clog2(RDATA_W){1'b0}}};
 
    //read and write fifos
    reg read_fifo;
    reg write_fifo;
    
    // word register
-   wire [2*R_DATA_W-1:0]      data;
-   reg [2*R_DATA_W-1:0]       data_nxt;
+   wire [2*RDATA_W-1:0]      data;
+   reg [2*RDATA_W-1:0]       data_nxt;
 
    // shift data to write and read
-   wire [2*W_DATA_W-1:0]      w_data_shifted;
+   wire [2*RDATA_W-1:0]      data_shifted;
 
    //program counter (fsm state)
    reg [1:0]                  pcnt_nxt;
    wire [1:0]                 pcnt;
 
    //word width accumulator
-   wire [$clog2(R_DATA_W):0]  acc;
-   reg [$clog2(R_DATA_W):0]   acc_nxt;
+   wire [$clog2(RDATA_W):0]  acc;
+   reg [$clog2(RDATA_W):0]   acc_nxt;
 
    //shift value
-   wire [$clog2(R_DATA_W)-1:0] shift_val;
-   reg [$clog2(R_DATA_W)-1:0] shift_val_nxt;
+   wire [$clog2(RDATA_W)-1:0] shift_val;
 
-   
+
    //output data
-   assign w_data_shifted = data >> (wrap_i? 1'b0: shift_val);
-   assign w_data_o = w_data_shifted[R_DATA_W-1-:W_DATA_W];
+   assign data_shifted = data >> (wrap_i? 1'b0: -shift_val);
+   assign w_data_o = data_shifted[RDATA_W-1-:WDATA_W];
    assign w_write_o = write_fifo;
    assign r_read_o = read_fifo;
-   
+   assign shift_val = acc - RDATA_W_INT;
 
    //program (fsm)
    always @* begin
@@ -57,7 +59,6 @@ module iob_unpack #(
       pcnt_nxt = pcnt + 1'b1;
       read_fifo = 1'b0;
       data_nxt = data;
-      shift_val_nxt = shift_val;
       write_fifo = 1'b0;
       acc_nxt = acc;
       
@@ -65,59 +66,39 @@ module iob_unpack #(
         
         0: begin //wait to read data from input FIFO
            if (!r_ready_i) begin
-              pcnt_nxt = pcnt;
+              pcnt_nxt = 1'b0;
            end else begin
               read_fifo = 1'b1;
            end
         end
 
-        1: begin //restart accumulator
-           acc_nxt =  wrap_i? {$clog2(R_DATA_W)+1{1'b0}}: acc -(1'b1 << $clog2(R_DATA_W));
+        1: begin //load data
+           //(re)start accumulator
+           acc_nxt =  wrap_i? {$clog2(RDATA_W)+1{1'b0}} : acc - RDATA_W_INT;
            //shift and load data
-           data_nxt = (data << acc) | r_data_i;
-           //save shift value
-           shift_val_nxt = (wrap_i? {$clog2(R_DATA_W){1'b0}} : acc -(1'b1 << $clog2(R_DATA_W)));
+           data_nxt = (data << (RDATA_W_INT-acc)) | r_data_i;
         end
 
         default: begin //write data to output FIFO and shift data until all data is written
-           if (!w_ready_i) begin
+           if ( acc < RDATA_W_INT ) begin
               pcnt_nxt = pcnt;
-           end else begin
-              if (acc < W_DATA_W) begin
+              if (w_ready_i) begin
                  write_fifo = 1'b1;
                  acc_nxt = acc + word_width_i;
-                 pcnt_nxt = pcnt;
                  data_nxt = data << word_width_i;
-               end else begin
+              end
+           end else begin
+              if (r_ready_i) begin
+                 read_fifo = 1'b1;
+                 pcnt_nxt = 2'd1;
+              end else begin
                  pcnt_nxt = 2'd0;
               end
-           end
-        end
+           end // else: !if( acc < RDATA_W_INT )
+        end // default
       endcase
-   end
-
+   end // always @ *
    
-   //word width accumulator register
-   iob_reg_r #(
-      .DATA_W ($clog2(R_DATA_W)+1),
-      .RST_VAL({$clog2(R_DATA_W)+1{1'b0}})
-   ) acc_reg (
-`include "clk_en_rst_s_s_portmap.vs"
-      .rst_i     (rst_i),
-      .data_i    (acc_nxt),
-      .data_o    (acc)
-   );
-
-   //data word register
-   iob_reg_r #(
-      .DATA_W(2*R_DATA_W),
-      .RST_VAL({2*R_DATA_W{1'b0}})
-   ) data_reg_inst (
-`include "clk_en_rst_s_s_portmap.vs"
-      .rst_i(rst_i),
-      .data_i(data_nxt),
-      .data_o(data)
-   );
 
    //program counter register
    iob_reg_r #(
@@ -129,16 +110,27 @@ module iob_unpack #(
       .data_i(pcnt_nxt),
       .data_o(pcnt)
    );
-
-   //shift value register
+   
+   //word width accumulator register
    iob_reg_r #(
-      .DATA_W($clog2(R_DATA_W)),
-      .RST_VAL({$clog2(R_DATA_W){1'b0}})
-   ) shift_val_reg_inst (
+      .DATA_W ($clog2(RDATA_W)+1),
+      .RST_VAL({$clog2(RDATA_W)+1{1'b0}})
+   ) acc_reg (
+`include "clk_en_rst_s_s_portmap.vs"
+      .rst_i     (rst_i),
+      .data_i    (acc_nxt),
+      .data_o    (acc)
+   );
+
+   //data word register
+   iob_reg_r #(
+      .DATA_W(2*RDATA_W),
+      .RST_VAL({2*RDATA_W{1'b0}})
+   ) data_reg_inst (
 `include "clk_en_rst_s_s_portmap.vs"
       .rst_i(rst_i),
-      .data_i(shift_val_nxt),
-      .data_o(shift_val)
+      .data_i(data_nxt),
+      .data_o(data)
    );
      
 endmodule
