fc94620eb7a704309276e8e40b145c3ab2866448
AndreMerendeira <andre.merendeira@gmail.com>
2024-01-15 14:05:39 +0000

    [apb] fixed apb converter and desconverter

--- submodules/LIB/hardware/modules/apb2iob/hardware/src/apb2iob.v
+++ submodules/LIB/hardware/modules/apb2iob/hardware/src/apb2iob.v
@@ -19,20 +19,10 @@ module apb2iob #(
    // IOb master interface
    `include "iob_m_port.vs"
 );
-
-   wire apb_ready_nxt;
-   iob_reg #(
-      .DATA_W (1),
-      .RST_VAL(1'b0)
-   ) apb_ready_reg_inst (
-      `include "clk_en_rst_s_s_portmap.vs"
-      .data_i(apb_ready_nxt),
-      .data_o(apb_ready_o)
-   );
-   assign apb_ready_nxt = (~apb_ready_o) & iob_ready_i & apb_sel_i & apb_enable_i;
+   assign apb_ready_o = iob_ready_i;
    assign apb_rdata_o = iob_rdata_i;
 
-   assign iob_valid_o = apb_sel_i & apb_enable_i & (~apb_ready_o);
+   assign iob_valid_o = apb_sel_i & apb_enable_i;
    assign iob_addr_o   = apb_addr_i;
    assign iob_wdata_o  = apb_wdata_i;
    assign iob_wstrb_o  = apb_wstrb_i;
