a91a0189d48e8199b443c0f59e943de2dedc1e20
AndreMerendeira <andre.merendeira@gmail.com>
2024-01-25 13:24:55 +0000

    [mkregs] reverted changes made to WAIT_RVALID

--- submodules/LIB/scripts/mkregs.py
+++ submodules/LIB/scripts/mkregs.py
@@ -641,10 +641,8 @@ class mkregs:
                         end
 
                         default: begin  // WAIT_RVALID
-                            if(rvalid_int & !iob_rvalid_o) begin
+                            if(rvalid_int) begin
                                 rvalid_nxt = 1'b1;
-                            end
-                            if (iob_rvalid_o) begin
                                 state_nxt = WAIT_REQ;
                             end
                         end
