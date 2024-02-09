c2bbcb41b1637aae1a05699cc5523380b5847f33
vasco <vascoalexluz2@gmail.com>
2024-02-09 11:23:25 +0000

    iob_ram_dp added

--- hardware/common_src/iob_soc_wrapper.v
+++ hardware/common_src/iob_soc_wrapper.v
@@ -44,6 +44,20 @@ iob_soc#(
       output             [  ADDR_W-1:0] addr_i,
       output             [  DATA_W-1:0] d_i,
       input              [  DATA_W-1:0] d_o,
+   `ifdef IOB_MEM_NO_READ_ON_WRITE
+      // Port A
+      output                            enA_i,
+      output             [DATA_W/8-1:0] weA_i,
+      output             [ADDR_W-1:0]   addrA_i,
+      output             [DATA_W-1:0]   dA_i,
+      input              [ DATA_W-1:0]  dA_o,
+      // Port B
+      output                            enB_i,
+      output             [DATA_W/8-1:0] weB_i,
+      output             [  ADDR_W-1:0] addrB_i,
+      output             [  DATA_W-1:0] dB_i,
+      input              [DATA_W-1 : 0] dB_o,
+
    `include "iob_soc_io.vs"
 );
 
@@ -103,5 +117,38 @@ iob_soc#(
       assign d_o = d_o_int;
    `endif
    `else  // !`ifdef USE_SPRAM
+
+   `ifdef IOB_MEM_NO_READ_ON_WRITE
+      localparam COL_W = DATA_W / 4;
+      localparam NUM_COL = DATA_W / COL_W;
+      localparam file_suffix = {"7", "6", "5", "4", "3", "2", "1", "0"};
+
+      genvar index;
+      generate
+         for (index = 0; index < NUM_COL; index = index + 1) begin : ram_col
+            localparam mem_init_file_int = (HEXFILE != "none") ?
+               {HEXFILE, "_", file_suffix[8*(index+1)-1-:8], ".hex"} : "none";
+            iob_ram_dp #(
+               .HEXFILE             (mem_init_file_int),
+               .ADDR_W              (ADDR_W),
+               .DATA_W              (COL_W),
+               .MEM_NO_READ_ON_WRITE(MEM_NO_READ_ON_WRITE)
+            ) ram (
+               .clk_i(clk_i),
+
+               .enA_i  (enA_i),
+               .addrA_i(addrA_i),
+               .dA_i   (dA_i[index*COL_W+:COL_W]),
+               .weA_i  (weA_i[index]),
+               .dA_o   (dA_o[index*COL_W+:COL_W]),
+
+               .enB_i  (enB_i),
+               .addrB_i(addrB_i),
+               .dB_i   (dB_i[index*COL_W+:COL_W]),
+               .weB_i  (weB_i[index]),
+               .dB_o   (dB_o[index*COL_W+:COL_W])
+            );
+         end
+      endgenerate
 
 endmodule
