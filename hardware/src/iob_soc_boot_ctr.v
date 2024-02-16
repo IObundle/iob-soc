9d94d734a312c923bf00001d02ac47f19556a4b8
vasco <vascoalexluz2@gmail.com>
2024-02-16 21:07:35 +0000

    sram_w_valid without reg and working

--- hardware/src/iob_soc_boot_ctr.v
+++ hardware/src/iob_soc_boot_ctr.v
@@ -127,25 +127,52 @@ module iob_soc_boot_ctr #(
    // WRITE SRAM
    //
 
-   reg                    sram_w_valid;
+   wire                    sram_w_valid;
+   wire                    sram_w_valid_nxt;
 
-   reg [SRAM_ADDR_W-2-1:0] sram_w_addr;
+   assign sram_w_valid_nxt = boot_o ? rom_r_valid : 1'b0;
+
+   reg [SRAM_ADDR_W-2-1:0] sram_w_addr; 
+
+
+
+   iob_reg_r #(
+      .DATA_W (1),
+      .RST_VAL(1'b0)
+   )sram_w_valid_reg (
+      .clk_i (clk_i),
+      .arst_i(arst_i),
+      .cke_i (1'b1),
+      .rst_i (1'b0),
+      .data_i(sram_w_valid_nxt),
+      .data_o(sram_w_valid)
+   );
+
+
+
+
+
+
+
+
+
+
 
 
    
 
    always @(posedge clk_i, posedge arst_i)
       if (arst_i) begin
-         sram_w_valid <= 1'b0;
+         //sram_w_valid <= 1'b0;
          sram_w_addr   <= -{1'b1, {(BOOTROM_ADDR_W - 2) {1'b0}}};
          sram_wstrb_o    <= {DATA_W / 8{1'b1}};
       end 
       else if (boot_o) begin
-         sram_w_valid <= rom_r_valid;
+         //sram_w_valid <= rom_r_valid;
          sram_w_addr   <= -{1'b1, {(BOOTROM_ADDR_W - 2) {1'b0}}} + rom_r_addr;
          sram_wstrb_o    <= {DATA_W / 8{rom_r_valid}};
       end else begin
-         sram_w_valid <= 1'b0;
+         //sram_w_valid <= 1'b0;
          sram_w_addr   <= -{1'b1, {(BOOTROM_ADDR_W - 2) {1'b0}}};
          sram_wstrb_o    <= {DATA_W / 8{1'b1}};
       end
