96bf5eb02ce67ecb5a78b30b6a54ab8e7abaa212
PMiranda <pmiranda.iobundle@gmail.com>
2024-02-28 15:19:22 +0000

    fix(lib): add main to lib roms and fifos

--- lib/hardware/modules/fifo/iob_fifo_sync/iob_fifo_sync.py
+++ lib/hardware/modules/fifo/iob_fifo_sync/iob_fifo_sync.py
@@ -1,4 +1,4 @@
-import os
+import sys
 
 from iob_module import iob_module
 
@@ -22,3 +22,14 @@ class iob_fifo_sync(iob_module):
             iob_utils(),
             (iob_ram_2p(), {"purpose": "simulation"}),
         ]
+
+
+if __name__ == "__main__":
+    # Create an iob_fifo_sync ip core
+    iob_fifo_sync_core = iob_fifo_sync()
+    if "clean" in sys.argv:
+        iob_fifo_sync_core.clean_build_dir()
+    elif "print" in sys.argv:
+        iob_fifo_sync_core.print_build_dir()
+    else:
+        iob_fifo_sync_core._setup()
