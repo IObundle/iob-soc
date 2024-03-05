44bc22e1c44535418b41a81fdecc54fb353ca4e7
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-03-05 19:25:51 +0000

    feat(iob_soc): Use objects instead of lists

--- iob_soc.py
+++ iob_soc.py
@@ -7,6 +7,9 @@ from iob_module import iob_module
 from iob_block_group import iob_block_group
 from iob_soc_utils import pre_setup_iob_soc, post_setup_iob_soc
 
+from iob_conf import iob_conf
+from iob_port import iob_port, iob_interface
+
 # Submodules
 from iob_picorv32 import iob_picorv32
 from iob_cache import iob_cache
@@ -78,177 +81,177 @@ class iob_soc(iob_module):
     ]
     confs: list = [
         # macros
-        {
-            "name": "INIT_MEM",
-            "type": "M",
-            "val": "INIT_MEM" in sys.argv,
-            "min": "0",
-            "max": "1",
-            "descr": "Enable MUL and DIV CPU instructions",
-        },
-        {
-            "name": "USE_EXTMEM",
-            "type": "M",
-            "val": "USE_EXTMEM" in sys.argv,
-            "min": "0",
-            "max": "1",
-            "descr": "Enable MUL and DIV CPU instructions",
-        },
-        {
-            "name": "USE_MUL_DIV",
-            "type": "M",
-            "val": "1",
-            "min": "0",
-            "max": "1",
-            "descr": "Enable MUL and DIV CPU instructions",
-        },
-        {
-            "name": "USE_COMPRESSED",
-            "type": "M",
-            "val": "1",
-            "min": "0",
-            "max": "1",
-            "descr": "Use compressed CPU instructions",
-        },
-        {
-            "name": "E",
-            "type": "M",
-            "val": "31",
-            "min": "1",
-            "max": "32",
-            "descr": "Address selection bit for external memory",
-        },
-        {
-            "name": "B",
-            "type": "M",
-            "val": "20",
-            "min": "1",
-            "max": "32",
-            "descr": "Address selection bit for boot ROM",
-        },
+        iob_conf(
+            name="INIT_MEM",
+            type="M",
+            val="INIT_MEM" in sys.argv,
+            min="0",
+            max="1",
+            descr="Enable MUL and DIV CPU instructions",
+        ),
+        iob_conf(
+            name="USE_EXTMEM",
+            type="M",
+            val="USE_EXTMEM" in sys.argv,
+            min="0",
+            max="1",
+            descr="Enable MUL and DIV CPU instructions",
+        ),
+        iob_conf(
+            name="USE_MUL_DIV",
+            type="M",
+            val="1",
+            min="0",
+            max="1",
+            descr="Enable MUL and DIV CPU instructions",
+        ),
+        iob_conf(
+            name="USE_COMPRESSED",
+            type="M",
+            val="1",
+            min="0",
+            max="1",
+            descr="Use compressed CPU instructions",
+        ),
+        iob_conf(
+            name="E",
+            type="M",
+            val="31",
+            min="1",
+            max="32",
+            descr="Address selection bit for external memory",
+        ),
+        iob_conf(
+            name="B",
+            type="M",
+            val="20",
+            min="1",
+            max="32",
+            descr="Address selection bit for boot ROM",
+        ),
         # parameters
-        {
-            "name": "BOOTROM_ADDR_W",
-            "type": "P",
-            "val": "12",
-            "min": "1",
-            "max": "32",
-            "descr": "Boot ROM address width",
-        },
-        {
-            "name": "SRAM_ADDR_W",
-            "type": "P",
-            "val": "15",
-            "min": "1",
-            "max": "32",
-            "descr": "SRAM address width",
-        },
-        {
-            "name": "MEM_ADDR_W",
-            "type": "P",
-            "val": "24",
-            "min": "1",
-            "max": "32",
-            "descr": "Memory bus address width",
-        },
+        iob_conf(
+            name="BOOTROM_ADDR_W",
+            type="P",
+            val="12",
+            min="1",
+            max="32",
+            descr="Boot ROM address width",
+        ),
+        iob_conf(
+            name="SRAM_ADDR_W",
+            type="P",
+            val="15",
+            min="1",
+            max="32",
+            descr="SRAM address width",
+        ),
+        iob_conf(
+            name="MEM_ADDR_W",
+            type="P",
+            val="24",
+            min="1",
+            max="32",
+            descr="Memory bus address width",
+        ),
         # mandatory parameters (do not change them!)
-        {
-            "name": "ADDR_W",
-            "type": "F",
-            "val": "32",
-            "min": "1",
-            "max": "32",
-            "descr": "Address bus width",
-        },
-        {
-            "name": "DATA_W",
-            "type": "F",
-            "val": "32",
-            "min": "1",
-            "max": "32",
-            "descr": "Data bus width",
-        },
-        {
-            "name": "AXI_ID_W",
-            "type": "F",
-            "val": "0",
-            "min": "1",
-            "max": "32",
-            "descr": "AXI ID bus width",
-        },
-        {
-            "name": "AXI_ADDR_W",
-            "type": "F",
-            "val": "`IOB_SOC_MEM_ADDR_W",
-            "min": "1",
-            "max": "32",
-            "descr": "AXI address bus width",
-        },
-        {
-            "name": "AXI_DATA_W",
-            "type": "F",
-            "val": "`IOB_SOC_DATA_W",
-            "min": "1",
-            "max": "32",
-            "descr": "AXI data bus width",
-        },
-        {
-            "name": "AXI_LEN_W",
-            "type": "F",
-            "val": "4",
-            "min": "1",
-            "max": "4",
-            "descr": "AXI burst length width",
-        },
-        {
-            "name": "MEM_ADDR_OFFSET",
-            "type": "F",
-            "val": "0",
-            "min": "0",
-            "max": "NA",
-            "descr": "Offset of memory address",
-        },
+        iob_conf(
+            name="ADDR_W",
+            type="F",
+            val="32",
+            min="1",
+            max="32",
+            descr="Address bus width",
+        ),
+        iob_conf(
+            name="DATA_W",
+            type="F",
+            val="32",
+            min="1",
+            max="32",
+            descr="Data bus width",
+        ),
+        iob_conf(
+            name="AXI_ID_W",
+            type="F",
+            val="0",
+            min="1",
+            max="32",
+            descr="AXI ID bus width",
+        ),
+        iob_conf(
+            name="AXI_ADDR_W",
+            type="F",
+            val="`IOB_SOC_MEM_ADDR_W",
+            min="1",
+            max="32",
+            descr="AXI address bus width",
+        ),
+        iob_conf(
+            name="AXI_DATA_W",
+            type="F",
+            val="`IOB_SOC_DATA_W",
+            min="1",
+            max="32",
+            descr="AXI data bus width",
+        ),
+        iob_conf(
+            name="AXI_LEN_W",
+            type="F",
+            val="4",
+            min="1",
+            max="4",
+            descr="AXI burst length width",
+        ),
+        iob_conf(
+            name="MEM_ADDR_OFFSET",
+            type="F",
+            val="0",
+            min="0",
+            max="NA",
+            descr="Offset of memory address",
+        ),
     ]
     ios = [
-        {
-            "name": "clk_en_rst",
-            "type": "slave",
-            "port_prefix": "",
-            "wire_prefix": "",
-            "descr": "Clock, enable, and reset",
-            "ports": [],
-        },
-        {
-            "name": "trap",
-            "type": "master",
-            "port_prefix": "",
-            "wire_prefix": "",
-            "descr": "iob-soc trap signal",
-            "ports": [
-                {
-                    "name": "trap",
-                    "direction": "output",
-                    "width": 1,
-                    "descr": "CPU trap signal",
-                },
+        iob_interface(
+            name="clk_en_rst",
+            type="slave",
+            port_prefix="",
+            wire_prefix="",
+            descr="Clock, enable, and reset",
+            ports=[],
+        ),
+        iob_interface(
+            name="trap",
+            type="master",
+            port_prefix="",
+            wire_prefix="",
+            descr="iob-soc trap signal",
+            ports=[
+                iob_port(
+                    name="trap",
+                    direction="output",
+                    width=1,
+                    descr="CPU trap signal",
+                ),
             ],
-        },
-        {
-            "name": "axi",
-            "type": "master",
-            "wire_prefix": "",
-            "port_prefix": "",
-            "mult": "",  # Will be filled automatically
-            "widths": {
+        ),
+        iob_interface(
+            name="axi",
+            type="master",
+            wire_prefix="",
+            port_prefix="",
+            mult="",  # Will be filled automatically
+            widths={
                 "ID_W": "AXI_ID_W",
                 "ADDR_W": "AXI_ADDR_W",
                 "DATA_W": "AXI_DATA_W",
                 "LEN_W": "AXI_LEN_W",
             },
-            "descr": "Bus of AXI master interfaces for external memory. One interface for this system and others optionally for peripherals.",
-            "if_defined": "USE_EXTMEM",
-            "ports": [],
-        },
+            descr="Bus of AXI master interfaces for external memory. One interface for this system and others optionally for peripherals.",
+            if_defined="USE_EXTMEM",
+            ports=[],
+        ),
     ]
 
     # IOb-SoC has the following set of non standard attributes:
@@ -256,6 +259,7 @@ class iob_soc(iob_module):
         uart.instance("UART0"),
         timer.instance("TIMER0"),
     ]
+    # TODO: deprecate this list
     peripheral_portmap = [  # List of tuples, each tuple corresponds to a port map
         (
             {
@@ -318,30 +322,14 @@ class iob_soc(iob_module):
     num_extmem_connections = -1
     # This is a standard iob_module attribute, but needs to be defined after 'peripherals' because it depends on it
     block_groups = [
-        iob_block_group(
-            name="cpu", description="CPU module", blocks=[cpu.instance("cpu_0")]
-        ),
-        iob_block_group(
-            name="bus_split",
-            description="Split modules for buses",
-            blocks=[
-                split.instance("ibus_split_0"),
-                split.instance("dbus_split_0"),
-                split.instance("int_dbus_split_0"),
-                split.instance("pbus_split_0"),
-            ],
-        ),
-        iob_block_group(
-            name="mem",
-            description="Memory module",
-            blocks=[merge.instance("iob_merge_0"), merge.instance("iob_merge_1")],
-        ),
-        iob_block_group(
-            name="peripheral",
-            description="Peripheral module",
-            blocks=peripherals,
-        ),
-    ]
+        cpu.instance("cpu_0"),
+        split.instance("ibus_split_0"),
+        split.instance("dbus_split_0"),
+        split.instance("int_dbus_split_0"),
+        split.instance("pbus_split_0"),
+        merge.instance("iob_merge_0"),
+        merge.instance("iob_merge_1")
+    ] + peripherals
 
     def __post_init__(self, *args, is_top=True, **kwargs):
         self.is_top_module = is_top
