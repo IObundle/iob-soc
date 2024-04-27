c34ce345ec0f0aa32fb7220516b52f99015a7923
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-04-27 03:39:08 +0100

    fix(lib): Fix lib test.sh; Update lib Makefile

--- lib/scripts/test.sh
+++ lib/scripts/test.sh
@@ -16,8 +16,7 @@ for i in $TB_DIRS; do MODULES+=" `basename $(builtin cd $i/../../..; pwd)`" ; do
 #test first argument is "clean", run make clean for all modules and exit
 if [ "$1" == "clean" ]; then
     for i in $MODULES; do 
-        core_dir=`find -name ${i}.py | xargs dirname`
-        make clean CORE=$i CORE_DIR=$core_dir BUILD_DIR=${CUSTOM_BUILD_DIR}${i}_build
+        make clean CORE=$i BUILD_DIR=${CUSTOM_BUILD_DIR}${i}_build
     done
     exit 0
 fi
@@ -26,9 +25,7 @@ fi
 if [ "$1" == "test" ]; then
     for i in $MODULES; do
         echo -e "\n\033[1;33mTesting module '${i}'\033[0m"
-        core_dir=`find -name ${i}.py | xargs dirname`
-        # echo "Running test for $core_dir"
-        make -f ${LIB_DIR}/Makefile clean setup CORE=$i CORE_DIR=$core_dir BUILD_DIR=${CUSTOM_BUILD_DIR}${i}_build
+        make -f ${LIB_DIR}/Makefile clean setup CORE=$i BUILD_DIR=${CUSTOM_BUILD_DIR}${i}_build
         make -C ${CUSTOM_BUILD_DIR}${i}_build sim-run
     done
     exit 0
@@ -36,13 +33,11 @@ fi
 
 #test if first argument is "build" and run build for single module
 if [ "$1" == "build" ]; then
-    core_dir=`find -name $2.py | xargs dirname`
-    make clean setup CORE=$2 CORE_DIR=$core_dir BUILD_DIR=${CUSTOM_BUILD_DIR}${2}_build
+    make clean setup CORE=$2 BUILD_DIR=${CUSTOM_BUILD_DIR}${2}_build
     make -C ${CUSTOM_BUILD_DIR}${2}_build sim-build
     exit 0
 fi
 
 #run single test
-core_dir=`find -name $1.py | xargs dirname`
-make clean setup CORE=$1 CORE_DIR=$core_dir BUILD_DIR=${CUSTOM_BUILD_DIR}${1}_build
+make clean setup CORE=$1 BUILD_DIR=${CUSTOM_BUILD_DIR}${1}_build
 make -C ${CUSTOM_BUILD_DIR}${1}_build sim-run VCD=$VCD
