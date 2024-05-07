7203e52401f45aa578367b8be306977faebe54c1
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-05-07 18:36:50 +0100

    feat(lib): Move some lib files back from py2hwsw repo. - Move lib specific files, like the Makefile, tests and Nix, from py2hwsw repo back to lib directory.

---
+++ lib/scripts/default.nix
@@ -0,0 +1,52 @@
+{ pkgs ? import (builtins.fetchTarball {
+  # Descriptive name to make the store path easier to identify
+  name = "nixos-22.11";
+  # Commit hash for nixos-22.11
+  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.11.tar.gz";
+  # Hash obtained using `nix-prefetch-url --unpack <url>`
+  sha256 = "11w3wn2yjhaa5pv20gbfbirvjq6i3m7pqrq2msf0g7cv44vijwgw";
+}) {}}:
+
+let
+  yosys = import ./yosys.nix { inherit pkgs; };
+in
+pkgs.mkShell {
+  name = "iob-shell";
+  buildInputs = with pkgs; [
+    bash
+    gnumake
+    verilog
+    verilator
+    gtkwave
+    python3
+    python3Packages.black
+    python3Packages.mypy
+    python3Packages.parse
+    python3Packages.numpy
+    python3Packages.wavedrom
+    python3Packages.matplotlib
+    python3Packages.scipy
+    python3Packages.pyserial
+    (texlive.combine { inherit (texlive) scheme-medium multirow lipsum catchfile nowidow enumitem placeins xltabular ltablex titlesec makecell datetime fmtcount comment textpos csquotes amsmath cancel listings hyperref biblatex; })
+    (callPackage ./riscv-gnu-toolchain.nix { })
+    verible
+    black
+    llvmPackages_14.clangUseLLVM
+    librsvg
+    libreoffice
+    minicom     # Terminal emulator
+    lrzsz       # For Zmodem file transfers via serial connection of the terminal emulator
+    # Add Volare custom Python installation
+    (let
+      volareSrc = pkgs.fetchFromGitHub {
+        owner = "efabless";
+        repo = "volare";
+        rev = "47325949b87e857d75f81d306f02ebccf952cb15";
+        sha256 = "sha256-H9B/vZUs0O2jwmidCTMYhO0JY4DL+gmQNeVawaccvuU=";
+      };
+    in import "${volareSrc}" {
+      inherit pkgs;
+    })
+    yosys
+  ];
+}
