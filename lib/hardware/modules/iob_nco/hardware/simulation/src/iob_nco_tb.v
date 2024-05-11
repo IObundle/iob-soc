da0f4195e9f34b319ee7a88c473881393607d825
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-05-11 22:57:43 +0100

    fix(iob_nco): Add missing 'test.log' file to iob_nco_tb.v

--- lib/hardware/modules/iob_nco/hardware/simulation/src/iob_nco_tb.v
+++ lib/hardware/modules/iob_nco/hardware/simulation/src/iob_nco_tb.v
@@ -9,47 +9,49 @@
 
 module iob_nco_tb;
 
-   localparam CLK_PER=10;
+  localparam CLK_PER = 10;
 
-   reg clk;
-   `IOB_CLOCK(clk, CLK_PER)
-   reg cke = 1'b1;
-   reg arst;
+  reg clk;
+  `IOB_CLOCK(clk, CLK_PER)
+  reg  cke = 1'b1;
+  reg  arst;
 
-   reg ld;
+  reg  ld;
 
-   wire clk_out;
+  wire clk_out;
 
-   initial begin
+  initial begin
 
 `ifdef VCD
-      $dumpfile("uut.vcd");
-      $dumpvars();
+    $dumpfile("uut.vcd");
+    $dumpvars();
 `endif
 
-      `IOB_RESET(clk, arst, 23, 23, 23)
+    `IOB_RESET(clk, arst, 23, 23, 23)
 
-      `IOB_PULSE(ld, 23, 20, 20)
+    `IOB_PULSE(ld, 23, 20, 20)
 
-      $display("%c[1;34m", 27);
-      $display("Test completed successfully.");
-      $display("%c[0m", 27);
-      #1000 $finish();
+    $display("%c[1;34m", 27);
+    $display("Test completed successfully.");
+    $display("%c[0m", 27);
+    fd = $fopen("test.log", "w");
+    $fdisplay(fd, "Test passed!");
+    $fclose(fd);
+    #1000 $finish();
 
-   end
+  end
 
-   iob_nco
-     #(
-       .DATA_W(16),
-       .FRAC_W(8)
-       ) nco (
-              `include "clk_en_rst_s_portmap.vs"
-              .rst_i(1'b0),
-              .en_i(1'b1),
-              .period_i(16'h1280),
-              .ld_i(ld),
-              .clk_o(clk_out)
-              );
+  iob_nco #(
+      .DATA_W(16),
+      .FRAC_W(8)
+  ) nco (
+      `include "clk_en_rst_s_portmap.vs"
+      .rst_i(1'b0),
+      .en_i(1'b1),
+      .period_i(16'h1280),
+      .ld_i(ld),
+      .clk_o(clk_out)
+  );
 
 
 endmodule
