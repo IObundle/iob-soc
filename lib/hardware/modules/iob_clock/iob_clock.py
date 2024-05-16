1e9220dd3ec5aa269aa2ea76131a576db1ee2141
Edw590 <40151365+Edw590@users.noreply.github.com>
2024-05-16 17:24:39 +0100

    Add iob_clock

---
+++ lib/hardware/modules/iob_clock/iob_clock.py
@@ -0,0 +1,38 @@
+def setup(py_params_dict):
+    attributes_dict = {
+        "original_name": "iob_clock",
+        "name": "iob_clock",
+        "version": "0.1",
+        "confs": [
+            {
+                "name": "CLK_PERIOD",
+                "type": "P",
+                "val": "10",
+                "min": "",
+                "max": "",
+                "descr": "Clock period",
+            },
+        ],
+        "ports": [
+            {
+                "name": "clk",
+                "descr": "Output clock",
+                "signals": [
+                    {"name": "clk", "width": "1", "direction": "output"},
+                ],
+            },
+        ],
+        "snippets": [
+            {
+                "outputs": ["clk_o"],
+                "verilog_code": """
+   reg clk;
+   assign clk_o = clk;
+
+   initial clk = 0; always #(CLK_PERIOD/2) clk = ~clk;
+        """,
+            }
+        ],
+    }
+
+    return attributes_dict
