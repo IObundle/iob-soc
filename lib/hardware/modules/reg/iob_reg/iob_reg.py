9a478806948e22e99072abe12f126328dc78362d
Martim Esteves <fimaales@gmail.com>
2024-07-18 21:48:50 +0100

    iob_reg.py
    automatically generates iob_reg.v

--- lib/hardware/modules/reg/iob_reg/iob_reg.py
+++ lib/hardware/modules/reg/iob_reg/iob_reg.py
@@ -3,7 +3,6 @@ def setup(py_params_dict):
         "original_name": "iob_reg",
         "name": "iob_reg",
         "version": "0.1",
-        "generate_hw": False,
         "confs": [
             {
                 "name": "DATA_W",
@@ -23,7 +22,7 @@ def setup(py_params_dict):
             },
             {
                 "name": "RST_POL",
-                "type": "M",
+                "type": "F",
                 "val": "1",
                 "min": "0",
                 "max": "1",
@@ -34,16 +33,12 @@ def setup(py_params_dict):
             {
                 "name": "clk_en_rst",
                 "type": "slave",
-                "port_prefix": "",
-                "wire_prefix": "",
                 "descr": "Clock, enable, and reset",
                 "signals": [],
             },
             {
                 "name": "io",
                 "type": "master",
-                "port_prefix": "",
-                "wire_prefix": "",
                 "descr": "Input and output",
                 "signals": [
                     {
@@ -59,6 +54,34 @@ def setup(py_params_dict):
                         "descr": "Output",
                     },
                 ],
+            },
+        ],
+        "snippets": [
+            {
+                "outputs": ["data_o_reg", "data_o"],
+                "verilog_code": """
+    reg [DATA_W-1:0] data_o_reg;
+    assign data_o = data_o_reg;
+  generate
+    if (RST_POL == 1) begin : g_rst_pol_1
+      always @(posedge clk_i, posedge arst_i) begin
+        if (arst_i) begin
+          data_o_reg <= RST_VAL;
+        end else if (cke_i) begin
+          data_o_reg <= data_i;
+        end
+      end
+    end else begin : g_rst_pol_0
+      always @(posedge clk_i, negedge arst_i) begin
+        if (~arst_i) begin
+          data_o_reg <= RST_VAL;
+        end else if (cke_i) begin
+          data_o_reg <= data_i;
+        end
+      end
+    end
+  endgenerate
+         """,
             },
         ],
     }
