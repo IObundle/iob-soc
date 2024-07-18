97de6ec0c0e2eb6c00469620ed79760ae15e1e25
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-07-18 02:10:24 +0100

    feat(iob_merge): Update iob_merge to support py2hwsw

--- lib/hardware/modules/iob_merge/iob_merge.py
+++ lib/hardware/modules/iob_merge/iob_merge.py
@@ -1,225 +1,65 @@
-from iob_core import iob_core
+def setup(py_params_dict):
+    assert "name" in py_params_dict, print(
+        "Error: Missing name for generated merge module."
+    )
+    assert "num_inputs" in py_params_dict, print(
+        "Error: Missing number of inputs for generated merge module."
+    )
 
+    NUM_INPUTS = int(py_params_dict["num_inputs"])
+    # Number of bits required for input selection
+    NBITS = NUM_INPUTS.bit_length()
 
-class iob_merge(iob_core):
-    def __init__(self, *args, **kwargs):
-        self.set_default_attribute("version", "0.1")
-        self.set_default_attribute("generate_hw", False)
-
-        self.set_default_attribute("name_prefix", "")
-        self.set_default_attribute("data_w", "DATA_W")
-        self.set_default_attribute("addr_w", "ADDR_W")
-        self.set_default_attribute("split_ptr", "ADDR_W-2")
-        self.set_default_attribute("input_ios", {})
-        self.set_default_attribute("output_io", {})
-
-        self.set_default_attribute("num_merges", len(self.input_ios))
-        # self.set_default_attribute("name", f"iob_{self.name_prefix}_merge")
-
-        self.create_conf(
-            name="ADDR_W",
-            type="P",
-            val="32",
-            min="0",
-            max="NA",
-            descr="Address bus width",
-        )
-        self.create_conf(
-            name="DATA_W",
-            type="P",
-            val="32",
-            min="0",
-            max="NA",
-            descr="Data bus width",
-        )
-
-        self.create_port(
-            name="clk_en_rst",
-            type="slave",
-            port_prefix="",
-            wire_prefix="",
-            descr="Clock, clock enable and async reset",
-            signals=[],
-            connect_to_port=True,
-        )
-        self.create_port(
-            name="rst",
-            type="master",
-            port_prefix="",
-            wire_prefix=f"{self.name}_",
-            descr="Sync reset",
-            signals=[
-                {
-                    "name": "rst",
-                    "direction": "input",
-                    "width": "1",
-                    "descr": "Sync reset",
-                },
-            ],
-        )
-        for input_io in self.input_ios:
-            copy_input_io = input_io.copy()
-            copy_input_io["is_io"] = True
-            self.ios.append(copy_input_io)
-        self.output_io = self.output_io.copy()
-        self.output_io["is_io"] = True
-        self.ios.append(self.output_io)
-
-        self.create_instance(
-            "iob_reg_r",
-            "iob_reg_r_inst",
-        )
-
-        self.create_instance(
-            "iob_mux",
-            "iob_mux_inst",
-        )
-
-        self.create_instance(
-            "iob_demux",
-            "iob_demux_inst",
-        )
-
-        self.create_instance(
-            "iob_prio_enc",
-            "iob_prio_enc_inst",
-        )
-
-        # top_dir = args[2]
-        top_dir = "."
-        self.gen_verilog_module(top_dir)
-        self.gen_verilog_instance(top_dir)
-
-        super().__init__(*args, **kwargs)
-
-    def gen_vlog_header(self, f):
-        f.write("`timescale 1ns / 1ps\n\n")
-        f.write(f'`include "{self.name}_conf.vh"\n\n')
-        f.write(f"module {self.name} #(\n")
-        f.write(f'\t`include "{self.name}_params.vs"\n')
-        f.write(") (\n")
-        f.write(f'\t`include "{self.name}_io.vs"\n')
-        f.write(");\n\n")
-        f.write(f"\tlocalparam NBITS = {(self.num_merges-1).bit_length()};\n")
-        f.write("\twire [NBITS-1:0] sel, sel_reg;\n\n")
-
-    def gen_vlog_aux_signals(self, f):
-        f.write("\tiob_prio_enc #(\n")
-        f.write(f"\t  .W ({self.num_merges}),\n")
-        f.write('\t  .MODE("HIGH")\n')
-        f.write("\t) sel_enc0 (\n")
-        f.write("\t  .unencoded_i(mux_valid_din),\n")
-        f.write("\t  .encoded_o(sel)\n")
-        f.write("\t);\n\n")
-        f.write("\tiob_reg_r #(\n")
-        f.write("\t  .DATA_W (NBITS),\n")
-        f.write("\t  .RST_VAL(0)\n")
-        f.write("\t) sel_reg0 (\n")
-        f.write('\t  `include "clk_en_rst_s_s_portmap.vs"\n')
-        f.write("\t  .rst_i(rst_i),\n")
-        f.write("\t  .data_i(sel),\n")
-        f.write("\t  .data_o(sel_reg)\n")
-        f.write("\t);\n\n")
-
-    def gen_vlog_demux(self, f, data_w, signal, sel="sel"):
-        f.write(f"\t//{signal}\n")
-        demux_data_o = f"demux_{signal}_dout"
-        demux_data_i = f'{self.output_io["port_prefix"]}iob_{signal}_i'
-        f.write(f"\twire[{self.num_merges}*{data_w}-1:0] {demux_data_o};\n")
-        idx = 0
-        for input in self.input_ios:
-            output_wire = f'{input["port_prefix"]}iob_{signal}_o'
-            f.write(
-                f"\tassign {output_wire} = {demux_data_o}[{idx}*{data_w}+:{data_w}];\n"
-            )
-            idx += 1
-        f.write("\n\tiob_demux #(\n")
-        f.write(f"\t  .DATA_W ({data_w}),\n")
-        f.write(f"\t  .N ({self.num_merges})\n")
-        f.write(f"\t) iob_demux_{signal} (\n")
-        f.write(f"\t  .sel_i({sel}),\n")
-        f.write(f"\t  .data_i({demux_data_i}),\n")
-        f.write(f"\t  .data_o({demux_data_o})\n")
-        f.write("\t);\n\n")
-
-    def gen_vlog_mux(self, f, data_w, signal):
-        f.write(f"\t//{signal}\n")
-        mux_data_i = f"mux_{signal}_din"
-        mux_data_o = f'{self.output_io["port_prefix"]}iob_{signal}_o'
-        f.write(f"\twire [{self.num_merges}*{data_w}-1:0] {mux_data_i};\n")
-        f.write(f"\tassign {mux_data_i} = {{\n")
-        first_wire = True
-        # reverse: most significant to least significant signal
-        for input in reversed(self.input_ios):
-            input_wire = f'{input["port_prefix"]}iob_{signal}_i'
-            if not first_wire:
-                f.write(",\n")
-            f.write(f"\t\t{input_wire}")
-            first_wire = False
-        f.write("\n\t};\n")
-        f.write("\n\tiob_mux #(\n")
-        f.write(f"\t  .DATA_W ({data_w}),\n")
-        f.write(f"\t  .N ({self.num_merges})\n")
-        f.write(f"\t) iob_mux_{signal} (\n")
-        f.write("\t  .sel_i(sel),\n")
-        f.write(f"\t  .data_i({mux_data_i}),\n")
-        f.write(f"\t  .data_o({mux_data_o})\n")
-        f.write("\t);\n\n")
-
-    def gen_verilog_module(self, top_dir="."):
-        file_name = f"{top_dir}/hardware/src/{self.name}.v"
-        with open(file_name, "w") as f:
-            self.gen_vlog_header(f)
-            self.gen_vlog_mux(f, 1, "valid")
-            self.gen_vlog_mux(f, "ADDR_W", "addr")
-            self.gen_vlog_mux(f, "DATA_W", "wdata")
-            self.gen_vlog_mux(f, "DATA_W/8", "wstrb")
-            self.gen_vlog_demux(f, "DATA_W", "rdata", sel="sel_reg")
-            self.gen_vlog_demux(f, 1, "rvalid", sel="sel_reg")
-            self.gen_vlog_demux(f, 1, "ready")
-            self.gen_vlog_aux_signals(f)
-            f.write("endmodule")
-
-    def gen_verilog_instance(self, top_dir="."):
-        file_name = f"{top_dir}/hardware/src/{self.name}_inst.vs"
-        with open(file_name, "w") as f:
-            f.write(f"\n\t{self.name} #(\n")
-            f.write(f"\t\t.ADDR_W({self.addr_w}),\n")
-            f.write(f"\t\t.DATA_W({self.data_w})\n")
-            f.write(f"\t) {self.name} (\n")
-            f.write(f'\t`include "{self.name}_io_portmap.vs"\n')
-            f.write("\t);\n\n")
-
-
-if __name__ == "__main__":
-    iob_merge(
-        name_prefix="test",
-        data_w="32",
-        addr_w="32",
-        split_ptr="32 - 2",
-        input_ios=[
+    attributes_dict = {
+        "original_name": "iob_merge",
+        "version": "0.1",
+        "confs": [
             {
-                "name": "iob",
-                "type": "slave",
-                "file_prefix": "merge_in_ram_w_",
-                "port_prefix": "ram_w_",
-                "wire_prefix": "ram_w_",
-                "param_prefix": "",
-                "descr": "merge input ram_w",
-                "signals": [],
-                "widths": {
-                    "DATA_W": "DATA_W",
-                    "ADDR_W": "ADDR_W",
-                },
+                "name": "ADDR_W",
+                "type": "P",
+                "val": "32",
+                "min": "0",
+                "max": "NA",
+                "descr": "Address bus width",
             },
             {
-                "name": "iob",
+                "name": "DATA_W",
+                "type": "P",
+                "val": "32",
+                "min": "0",
+                "max": "NA",
+                "descr": "Data bus width",
+            },
+        ],
+        "ports": [
+            {
+                "name": "clk_en_rst",
                 "type": "slave",
-                "file_prefix": "merge_in_ram_r_",
-                "port_prefix": "ram_r_",
-                "wire_prefix": "ram_r_",
+                "port_prefix": "",
+                "wire_prefix": "",
+                "descr": "Clock, clock enable and async reset",
+                "signals": [],
+                "connect_to_port": True,
+            },
+            {
+                "name": "reset",
+                "descr": "Reset signal",
+                "signals": [
+                    {
+                        "name": "rst",
+                        "direction": "input",
+                        "width": "1",
+                    },
+                ],
+            },
+            {
+                "name": "output",
+                "if_gen": "iob",
+                "type": "slave",
+                "file_prefix": py_params_dict["name"] + "_output_",
+                "port_prefix": "output_",
                 "param_prefix": "",
-                "descr": "merge input ram_r",
+                "descr": "Merge output",
                 "signals": [],
                 "widths": {
                     "DATA_W": "DATA_W",
@@ -227,18 +67,263 @@ if __name__ == "__main__":
                 },
             },
         ],
-        output_io={
-            "name": "iob",
-            "type": "master",
-            "file_prefix": "merge_out_ram_i_",
-            "port_prefix": "ram_i_",
-            "wire_prefix": "ram_i_",
-            "param_prefix": "",
-            "descr": "merge output ram_i",
-            "ports": [],
-            "widths": {
-                "DATA_W": "DATA_W",
-                "ADDR_W": "ADDR_W",
+    }
+    for port_idx in range(NUM_INPUTS):
+        attributes_dict["ports"].append(
+            {
+                "name": f"input_{port_idx}",
+                "if_gen": "iob",
+                "type": "master",
+                "file_prefix": f"{py_params_dict["name"]}_input{port_idx}_",
+                "port_prefix": f"input{port_idx}_",
+                "param_prefix": "",
+                "descr": "Merge input interfaces",
+                "signals": [],
+                "widths": {
+                    "DATA_W": "DATA_W",
+                    "ADDR_W": "ADDR_W",
+                },
+            },
+        )
+    attributes_dict["wires"] = [
+        # Output selection signals
+        {
+            "name": "sel_reg_rst",
+            "descr": "Enable and reset signal for sel_reg",
+            "signals": [
+                {"name": "rst"},
+            ],
+        },
+        {
+            "name": "sel_reg_io",
+            "descr": "I/O of sel_reg",
+            "signals": [
+                {"name": "sel", "width": NBITS},
+                {"name": "sel_reg", "width": NBITS},
+            ],
+        },
+        {
+            "name": "input_sel",
+            "descr": "Select output interface",
+            "signals": [
+                {"name": "sel"},
+            ],
+        },
+        {
+            "name": "input_sel_reg",
+            "descr": "Registered select output interface",
+            "signals": [
+                {"name": "sel_reg"},
+            ],
+        },
+        # Mux signals
+        {
+            "name": "mux_valid_io",
+            "descr": "I/O of valid mux",
+            "signals": [
+                {"name": "mux_valid_input", "width": NUM_INPUTS*1},
+                {"name": "output_iob_valid"},
+            ],
+        },
+        {
+            "name": "mux_addr_io",
+            "descr": "I/O of address mux",
+            "signals": [
+                {"name": "mux_addr_input", "width": f"{NUM_INPUTS}*ADDR_W"},
+                {"name": "output_iob_addr"},
+            ],
+        },
+        {
+            "name": "mux_wdata_io",
+            "descr": "I/O of wdata mux",
+            "signals": [
+                {"name": "mux_wdata_input", "width": f"{NUM_INPUTS}*DATA_W"},
+                {"name": "output_iob_wdata"},
+            ],
+        },
+        {
+            "name": "mux_wstrb_io",
+            "descr": "I/O of wstrb mux",
+            "signals": [
+                {"name": "mux_wstrb_input", "width": f"{NUM_INPUTS}*(DATA_W/8)"},
+                {"name": "output_iob_wstrb"},
+            ],
+        },
+        # Demux signals
+        {
+            "name": "demux_rdata_io",
+            "descr": "I/O of rdata demux",
+            "signals": [
+                {"name": "output_iob_rdata"},
+                {"name": "demux_rdata_output", "width": f"{NUM_INPUTS}*DATA_W"},
+            ],
+        },
+        {
+            "name": "demux_rvalid_io",
+            "descr": "I/O of rvalid demux",
+            "signals": [
+                {"name": "output_iob_rvalid"},
+                {"name": "demux_rvalid_output", "width": f"{NUM_INPUTS}*1"},
+            ],
+        },
+        {
+            "name": "demux_ready_io",
+            "descr": "I/O of ready demux",
+            "signals": [
+                {"name": "output_iob_ready"},
+                {"name": "demux_ready_output", "width": f"{NUM_INPUTS}*1"},
+            ],
+        },
+        # Priority encoder signals
+        {
+            "name": "prio_enc_io",
+            "descr": "I/O of priority encoder",
+            "signals": [
+                {"name": "mux_valid_input"},
+                {"name": "sel"},
+            ],
+        },
+
+    ]
+    attributes_dict["blocks"] = [
+        {
+            "core_name": "iob_reg_r",
+            "instance_name": "sel_reg",
+            "parameters": {
+                "DATA_W": NBITS,
+                "RST_VAL": 0,
+            },
+            "connect": {
+                "clk_en_rst": "clk_en_rst",
+                "rst": "sel_reg_rst",
+                "io": "sel_reg_io",
             },
         },
-    )
+        # muxers
+        {
+            "core_name": "iob_mux",
+            "instance_name": "iob_mux_valid",
+            "parameters": {
+                "DATA_W": 1,
+                "N": NUM_INPUTS,
+            },
+            "connect": {
+                "sel": "input_sel",
+                "io": "mux_valid_io",
+            },
+        },
+        {
+            "core_name": "iob_mux",
+            "instance_name": "iob_mux_addr",
+            "parameters": {
+                "DATA_W": "ADDR_W",
+                "N": NUM_INPUTS,
+            },
+            "connect": {
+                "sel": "input_sel",
+                "io": "mux_addr_io",
+            },
+        },
+        {
+            "core_name": "iob_mux",
+            "instance_name": "iob_mux_wdata",
+            "parameters": {
+                "DATA_W": "DATA_W",
+                "N": NUM_INPUTS,
+            },
+            "connect": {
+                "sel": "input_sel",
+                "io": "mux_wdata_io",
+            },
+        },
+        {
+            "core_name": "iob_mux",
+            "instance_name": "iob_mux_wstrb",
+            "parameters": {
+                "DATA_W": "DATA_W/8",
+                "N": NUM_INPUTS,
+            },
+            "connect": {
+                "sel": "input_sel",
+                "io": "mux_wstrb_io",
+            },
+        },
+        {
+            "core_name": "iob_demux",
+            "instance_name": "iob_demux_rdata",
+            "parameters": {
+                "DATA_W": "DATA_W",
+                "N": NUM_INPUTS,
+            },
+            "connect": {
+                "sel": "input_sel_reg",
+                "io": "demux_rdata_io",
+            },
+        },
+        # demuxers
+        {
+            "core_name": "iob_demux",
+            "instance_name": "iob_demux_rvalid",
+            "parameters": {
+                "DATA_W": "DATA_W",
+                "N": NUM_INPUTS,
+            },
+            "connect": {
+                "sel": "input_sel_reg",
+                "io": "demux_rvalid_io",
+            },
+        },
+        {
+            "core_name": "iob_demux",
+            "instance_name": "iob_demux_ready",
+            "parameters": {
+                "DATA_W": "DATA_W",
+                "N": NUM_INPUTS,
+            },
+            "connect": {
+                "sel": "input_sel",
+                "io": "demux_ready_io",
+            },
+        },
+        # priority encoder
+        {
+            "core_name": "iob_prio_enc",
+            "instance_name": "sel_enc",
+            "parameters": {
+                "W": NUM_INPUTS,
+                "MODE": "HIGH",
+            },
+            "connect": {
+                "io": "prio_enc_io",
+            },
+        },
+    ]
+
+    # Connect demuxer outputs
+    verilog_code = ""
+    verilog_outputs = []
+    for port_idx in range(NUM_INPUTS):
+        verilog_code += ""
+        f"assign input{port_idx}_iob_rdata_o = demux_rdata_output[{port_idx}*DATA_W+:DATA_W];"
+        f"assign input{port_idx}_iob_rvalid_o = demux_rvalid_output[{port_idx}*1+:1];"
+        f"assign input{port_idx}_iob_ready_o = demux_ready_output[{port_idx}*1+:1];"
+        verilog_outputs.append(f"input{port_idx}_iob_rdata")
+        verilog_outputs.append(f"input{port_idx}_iob_rvalid")
+        verilog_outputs.append(f"input{port_idx}_iob_ready")
+    verilog_code += "\n"
+    # Connect muxer inputs
+    for signal in ["valid", "addr", "wdata", "wstrb"]:
+        verilog_code += f"mux_{signal}_input = {{"
+        for port_idx in range(NUM_INPUTS):
+            verilog_code += f"input{port_idx}_iob_{signal}_i, "
+        verilog_code = verilog_code[:-2] + "};\n"
+        verilog_outputs.append(f"mux_{signal}_input")
+    # Create snippet with demuxer and muxer connections
+    attributes_dict["snippets"] = [
+        {
+            "outputs": verilog_outputs,
+            "verilog_code": verilog_code,
+        },
+    ]
+
+    return attributes_dict
