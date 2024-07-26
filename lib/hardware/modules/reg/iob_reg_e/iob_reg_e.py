eaf79fff403220c1816ee13ed5ddab46be06cc63
Martim Esteves <fimaales@gmail.com>
2024-07-26 16:43:32 +0100

    reg module generated automatically with py2hwsw

--- lib/hardware/modules/reg/iob_reg_e/iob_reg_e.py
+++ lib/hardware/modules/reg/iob_reg_e/iob_reg_e.py
@@ -3,11 +3,95 @@ def setup(py_params_dict):
         "original_name": "iob_reg_e",
         "name": "iob_reg_e",
         "version": "0.1",
-        "generate_hw": False,
+        "confs": [
+            {
+                "name": "DATA_W",
+                "type": "P",
+                "val": "21",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Data bus width",
+            },
+            {
+                "name": "RST_VAL",
+                "type": "P",
+                "val": "{DATA_W{1'b0}}",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Reset value.",
+            },
+        ],
+        "ports": [
+            {
+                "name": "clk_en_rst",
+                "type": "slave",
+                "descr": "Clock, clock enable and reset",
+                "signals": [],
+            },
+            {
+                "name": "en_i",
+                "descr": "Input port",
+                "signals": [
+                    {
+                        "name": "en",
+                        "width": 1,
+                        "direction": "input",
+                    },
+                ],
+            },
+            {
+                "name": "data_i",
+                "descr": "Input port",
+                "signals": [
+                    {
+                        "name": "data",
+                        "width": "DATA_W",
+                        "direction": "input",
+                    },
+                ],
+            },
+            {
+                "name": "data_o",
+                "descr": "Output port",
+                "signals": [
+                    {
+                        "name": "data",
+                        "width": "DATA_W",
+                        "direction": "output",
+                    },
+                ],
+            },
+        ],
+        "wires": [
+            {
+                "name": "data_int",
+                "descr": "data wire",
+                "signals": [
+                    {"name": "data_int", "width": "DATA_W"},
+                ],
+            },
+        ],
         "blocks": [
             {
                 "core_name": "iob_reg",
-                "instance_name": "iob_reg_inst",
+                "instance_name": "reg0",
+                "parameters": {
+                    "DATA_W": "DATA_W",
+                    "RST_VAL": "RST_VAL",
+                },
+                "connect": {
+                    "clk_en_rst": "clk_en_rst",
+                    "data_i": "data_int",
+                    "data_o": "data_o",
+                },
+            },
+        ],
+        "snippets": [
+            {
+                "outputs": ["data"],
+                "verilog_code": f"""
+        assign data_int = en_i ? data_i : data_o;
+            """,
             },
         ],
     }
--- lib/hardware/modules/reg/iob_reg_re/iob_reg_re.py
+++ lib/hardware/modules/reg/iob_reg_re/iob_reg_re.py
@@ -3,7 +3,24 @@ def setup(py_params_dict):
         "original_name": "iob_reg_re",
         "name": "iob_reg_re",
         "version": "0.1",
-        "generate_hw": False,
+        "confs": [
+            {
+                "name": "DATA_W",
+                "type": "P",
+                "val": "21",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Data bus width",
+            },
+            {
+                "name": "RST_VAL",
+                "type": "P",
+                "val": "{DATA_W{1'b0}}",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Reset value.",
+            },
+        ],
         "ports": [
             {
                 "name": "clk_en_rst",
@@ -35,31 +52,66 @@ def setup(py_params_dict):
                 ],
             },
             {
-                "name": "io",
-                "type": "master",
-                "port_prefix": "",
-                "wire_prefix": "",
-                "descr": "Data interface",
+                "name": "data_i",
+                "descr": "Input port",
                 "signals": [
                     {
                         "name": "data",
-                        "direction": "input",
                         "width": "DATA_W",
-                        "descr": "Write data",
+                        "direction": "input",
                     },
+                ],
+            },
+            {
+                "name": "data_o",
+                "descr": "Output port",
+                "signals": [
                     {
                         "name": "data",
-                        "direction": "output",
                         "width": "DATA_W",
-                        "descr": "Read data",
+                        "direction": "output",
                     },
+                ],
+            },
+        ],
+        "wires": [
+            {
+                "name": "data_int",
+                "descr": "data_int wire",
+                "signals": [
+                    {"name": "data_int", "width": "DATA_W"},
+                ],
+            },
+            {
+                "name": "iob_reg_re_rst",
+                "descr": "iob_reg_re_rst wire",
+                "signals": [
+                    {"name": "rst"},
                 ],
             },
         ],
         "blocks": [
             {
                 "core_name": "iob_reg_r",
-                "instance_name": "iob_reg_r_inst",
+                "instance_name": "reg0",
+                "parameters": {
+                    "DATA_W": "DATA_W",
+                    "RST_VAL": "RST_VAL",
+                },
+                "connect": {
+                    "clk_en_rst": "clk_en_rst",
+                    "rst_i": "iob_reg_re_rst",
+                    "data_i": "data_int",
+                    "data_o": "data_o",
+                },
+            },
+        ],
+        "snippets": [
+            {
+                "outputs": ["data_int"],
+                "verilog_code": f"""
+        assign data_int = en_i ? data_i : data_o;
+            """,
             },
         ],
     }
