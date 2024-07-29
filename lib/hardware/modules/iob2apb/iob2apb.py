567204bf1aae4db413e1e7400b7ab90358856d76
Martim Esteves <fimaales@gmail.com>
2024-07-29 21:44:21 +0100

    changes in iob2apb

--- lib/hardware/modules/iob2apb/iob2apb.py
+++ lib/hardware/modules/iob2apb/iob2apb.py
@@ -3,13 +3,44 @@ def setup(py_params_dict):
         "original_name": "iob2apb",
         "name": "iob2apb",
         "version": "0.1",
-        "generate_hw": False,
+        "confs": [
+            {
+                "name": "APB_ADDR_W",
+                "type": "P",
+                "val": "22",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Data bus width",
+            },
+            {
+                "name": "APB_DATA_W",
+                "type": "P",
+                "val": "22",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Reset value.",
+            },
+            {
+                "name": "ADDR_W",
+                "type": "P",
+                "val": "APB_ADDR_W",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Reset value.",
+            },
+            {
+                "name": "DATA_W",
+                "type": "P",
+                "val": "APB_DATA_W",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Reset value.",
+            },
+        ],
         "ports": [
             {
                 "name": "clk_en_rst",
                 "type": "slave",
-                "port_prefix": "",
-                "wire_prefix": "",
                 "descr": "Clock, clock enable and reset",
                 "signals": [],
             },
@@ -26,10 +57,48 @@ def setup(py_params_dict):
                 "signals": [],
             },
         ],
+        "wires": [
+            {
+                "name": "pc_int",
+                "descr": "pc_int wire",
+                "signals": [
+                    {"name": "pc_int", "width": 1},
+                ],
+            },
+            {
+                "name": "pc_nxt_int",
+                "descr": "pc_nxt_int wire",
+                "signals": [
+                    {"name": "pc_nxt_int", "width": 1},
+                ],
+            },
+        ],
         "blocks": [
             {
                 "core_name": "iob_reg",
-                "instance_name": "iob_reg_inst",
+                "instance_name": "pc_reg",
+                "parameters": {
+                    "DATA_W": 2,
+                    "RST_VAL": 0,
+                },
+                "connect": {
+                    "clk_en_rst": "clk_en_rst",
+                    "data_i": "pc_nxt_int",
+                    "data_o": "pc_int",
+                },
+            },
+            {
+                "core_name": "iob_reg",
+                "instance_name": "pc_reg",
+                "parameters": {
+                    "DATA_W": "DATA_W",
+                    "RST_VAL": 0,
+                },
+                "connect": {
+                    "clk_en_rst": "clk_en_rst",
+                    "data_i": "apb_rdata_i",
+                    "data_o": "iob_rdata_o",
+                },
             },
         ],
     }
