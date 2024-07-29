2513b26a3fa5bd865756f00031432b7449200514
Martim Esteves <fimaales@gmail.com>
2024-07-29 20:27:02 +0100

    module alt_iobuf generated with py2hwsw

--- lib/hardware/modules/alt_iobuf/alt_iobuf.py
+++ lib/hardware/modules/alt_iobuf/alt_iobuf.py
@@ -3,7 +3,60 @@ def setup(py_params_dict):
         "original_name": "alt_iobuf",
         "name": "alt_iobuf",
         "version": "0.1",
-        "generate_hw": False,
+        "ports": [
+            {
+                "name": "in_i",
+                "descr": "Input port",
+                "signals": [
+                    {
+                        "name": "in",
+                        "width": 1,
+                        "direction": "input",
+                    },
+                ],
+            },
+            {
+                "name": "en_i",
+                "descr": "Input port",
+                "signals": [
+                    {
+                        "name": "en",
+                        "width": 1,
+                        "direction": "input",
+                    },
+                ],
+            },
+            {
+                "name": "out_o",
+                "descr": "Output port",
+                "signals": [
+                    {
+                        "name": "data",
+                        "width": 1,
+                        "direction": "output",
+                    },
+                ],
+            },
+            {
+                "name": "in_out_io",
+                "descr": "In/Output port",
+                "signals": [
+                    {
+                        "name": "in_out",
+                        "width": 1,
+                        "direction": "inout",
+                    },
+                ],
+            },
+        ],
+        "snippets": [
+            {
+                "verilog_code": f"""
+            assign in_out_io = en_i ? in_i : 1'bz;
+            assign out_o     = in_out_io;
+         """,
+            },
+        ],
     }
 
     return attributes_dict
