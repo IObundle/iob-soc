9eacd99425241ff080c7b11dfbd65dbcf647ba26
Martim Esteves <fimaales@gmail.com>
2024-07-29 21:52:16 +0100

    module iob_prio_enc generated by py2hwsw

--- lib/hardware/modules/iob_prio_enc/iob_prio_enc.py
+++ lib/hardware/modules/iob_prio_enc/iob_prio_enc.py
@@ -3,11 +3,74 @@ def setup(py_params_dict):
         "original_name": "iob_prio_enc",
         "name": "iob_prio_enc",
         "version": "0.1",
-        "generate_hw": False,
-        "blocks": [
+        "confs": [
             {
-                "core_name": "iob_reverse",
-                "instance_name": "iob_reverse_inst",
+                "name": "W",
+                "type": "P",
+                "val": "21",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Data bus width",
+            },
+            {
+                "name": "MODE",
+                "type": "P",
+                "val": "LOW",
+                "min": "NA",
+                "max": "NA",
+                "descr": "",
+            },
+        ],
+        "ports": [
+            {
+                "name": "unencoded_i",
+                "descr": "Input port",
+                "signals": [
+                    {
+                        "name": "unencoded",
+                        "width": "W",
+                        "direction": "input",
+                    },
+                ],
+            },
+            {
+                "name": "encoded_o",
+                "descr": "Output port",
+                "signals": [
+                    {
+                        "name": "encoded",
+                        "width": "$clog2(W)",
+                        "direction": "output",
+                    },
+                ],
+            },
+        ],
+        "snippets": [
+            {
+                "verilog_code": """
+         integer pos;
+   generate
+      if (MODE == "LOW") begin : gen_low_prio
+         always @* begin
+            encoded_o = {$clog2(W) {1'd0}};  //In case input is 0
+            for (pos = W - 1; pos != -1; pos = pos - 1) begin
+               if (unencoded_i[pos]) begin
+                  encoded_o = pos;
+               end
+            end
+         end
+      end else begin : gen_highest_prio  //MODE == "HIGH"
+         always @* begin
+            encoded_o = {$clog2(W) {1'd0}};  //In case input is 0
+            for (pos = {W{1'd0}}; pos < W; pos = pos + 1) begin
+               if (unencoded_i[pos]) begin
+                  encoded_o = pos;
+               end
+            end
+         end
+      end
+   endgenerate    
+         """,
             },
         ],
     }
