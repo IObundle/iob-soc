b3d9958cae0e4ba15347b2e1f15d899c35a33128
Edw590 <40151365+Edw590@users.noreply.github.com>
2024-07-31 20:34:50 +0100

    Remove double iob_functions includes

--- lib/hardware/modules/fifo/iob_fifo_sync/iob_fifo_sync.py
+++ lib/hardware/modules/fifo/iob_fifo_sync/iob_fifo_sync.py
@@ -151,10 +151,6 @@ def setup(py_params_dict):
                 "core_name": "iob_ram_2p",
                 "instance_name": "iob_ram_2p_inst",
             },
-            {
-                "core_name": "iob_functions",
-                "instance_name": "iob_functions_inst",
-            },
         ],
     }
 
