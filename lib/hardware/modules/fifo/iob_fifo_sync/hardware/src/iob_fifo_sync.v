2717bd93f9dfcd7ab3b5545ab6df75bf866c1dd0
Edw590 <40151365+Edw590@users.noreply.github.com>
2024-07-31 20:31:52 +0100

    Remove macros from some modules

--- lib/hardware/modules/fifo/iob_fifo_sync/hardware/src/iob_fifo_sync.v
+++ lib/hardware/modules/fifo/iob_fifo_sync/hardware/src/iob_fifo_sync.v
@@ -1,15 +1,14 @@
 `timescale 1ns / 1ps
 
-`define IOB_MAX(a, b) (((a) > (b)) ? (a) : (b))
-`define IOB_MIN(a, b) (((a) < (b)) ? (a) : (b))
+`include "iob_functions.vs"
 
 module iob_fifo_sync #(
     parameter W_DATA_W = 21,
     R_DATA_W = 21,
     ADDR_W = 21,  //higher ADDR_W lower DATA_W
     //determine W_ADDR_W and R_ADDR_W
-    MAXDATA_W = `IOB_MAX(W_DATA_W, R_DATA_W),
-    MINDATA_W = `IOB_MIN(W_DATA_W, R_DATA_W),
+    MAXDATA_W = iob_max(W_DATA_W, R_DATA_W),
+    MINDATA_W = iob_min(W_DATA_W, R_DATA_W),
     R = MAXDATA_W / MINDATA_W,
     MINADDR_W = ADDR_W - $clog2(R),  //lower ADDR_W (higher DATA_W)
     W_ADDR_W = (W_DATA_W == MAXDATA_W) ? MINADDR_W : ADDR_W,
