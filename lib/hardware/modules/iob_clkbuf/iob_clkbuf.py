6b709fbd57e04ab770ee15befad640f207156b1d
Martim Esteves <fimaales@gmail.com>
2024-07-31 01:25:04 +0100

    module iob_clkbuf generated with py2hwsw

--- lib/hardware/modules/iob_clkbuf/iob_clkbuf.py
+++ lib/hardware/modules/iob_clkbuf/iob_clkbuf.py
@@ -3,7 +3,65 @@ def setup(py_params_dict):
         "original_name": "iob_clkbuf",
         "name": "iob_clkbuf",
         "version": "0.1",
-        "generate_hw": False,
+        "ports": [
+            {
+                "name": "clk_i",
+                "descr": "Input port",
+                "signals": [
+                    {
+                        "name": "clk",
+                        "width": 1,
+                        "direction": "input",
+                    },
+                ],
+            },
+            {
+                "name": "n_i",
+                "descr": "Input port",
+                "signals": [
+                    {
+                        "name": "n",
+                        "width": 1,
+                        "direction": "input",
+                    },
+                ],
+            },
+            {
+                "name": "clk_o",
+                "descr": "Output port",
+                "signals": [
+                    {
+                        "name": "clk",
+                        "width": 1,
+                        "direction": "output",
+                    },
+                ],
+            },
+        ],
+        "wires": [
+            {
+                "name": "clk_int",
+                "descr": "clk_int wire",
+                "signals": [
+                    {"name": "clk_int", "width": 1},
+                ],
+            },
+        ],
+        "snippets": [
+            {
+                "verilog_code": f"""
+        assign clk_int = n_i ? ~clk_i : clk_i;
+        `ifdef XILINX
+   BUFG BUFG_inst (
+      .I(clk_int),
+      .O(clk_o)
+   );
+`else
+   assign clk_o = clk_int;
+`endif
+            """,
+            },
+        ],
     }
 
     return attributes_dict
