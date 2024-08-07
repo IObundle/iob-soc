99c407eb793ef4239f46d0f90fbb704da150dc2f
Martim Esteves <fimaales@gmail.com>
2024-08-08 00:30:15 +0100

    module counter generated with py2hwsw

--- lib/hardware/modules/counter/iob_counter_ld/iob_counter_ld.py
+++ lib/hardware/modules/counter/iob_counter_ld/iob_counter_ld.py
@@ -3,7 +3,24 @@ def setup(py_params_dict):
         "original_name": "iob_counter_ld",
         "name": "iob_counter_ld",
         "version": "0.1",
-        "generate_hw": False,
+        "confs": [
+            {
+                "name": "DATA_W",
+                "type": "P",
+                "val": "21",
+                "min": "1",
+                "max": "NA",
+                "descr": "",
+            },
+            {
+                "name": "RST_VAL",
+                "type": "P",
+                "val": "{DATA_W{1'b0}}",
+                "min": "0",
+                "max": "NA",
+                "descr": "",
+            },
+        ],
         "ports": [
             {
                 "name": "clk_en_rst",
@@ -13,12 +30,76 @@ def setup(py_params_dict):
                 },
                 "descr": "Clock, clock enable and reset",
             },
-            # TODO: Remaining ports
+            {
+                "name": "en_rst",
+                "descr": "Enable and Synchronous reset interface",
+                "signals": [
+                    {
+                        "name": "en",
+                        "direction": "input",
+                        "width": 1,
+                        "descr": "Enable input",
+                    },
+                    {
+                        "name": "rst",
+                        "direction": "input",
+                        "width": 1,
+                        "descr": "Synchronous reset input",
+                    },
+                ],
+            },
+            {
+                "name": "ld_i",
+                "descr": "Input port",
+                "signals": [
+                    {"name": "ld", "width": 1, "direction": "input"},
+                ],
+            },
+            {
+                "name": "ld_val_i",
+                "descr": "Input port",
+                "signals": [
+                    {"name": "ld_val", "width": "DATA_W", "direction": "input"},
+                ],
+            },
+            {
+                "name": "data_o",
+                "descr": "Output port",
+                "signals": [
+                    {"name": "data", "width": "DATA_W", "direction": "output"},
+                ],
+            },
+        ],
+        "wires": [
+            {
+                "name": "data_int",
+                "descr": "data wire",
+                "signals": [
+                    {"name": "data_int", "width": "DATA_W"},
+                ],
+            },
         ],
         "blocks": [
             {
                 "core_name": "iob_reg_re",
-                "instance_name": "iob_reg_re_inst",
+                "instance_name": "reg0",
+                "parameters": {
+                    "DATA_W": "DATA_W",
+                    "RST_VAL": "RST_VAL",
+                },
+                "connect": {
+                    "clk_en_rst": "clk_en_rst",
+                    "en_rst": "en_rst",
+                    "data_i": "data_int",
+                    "data_o": "data_o",
+                },
+            },
+        ],
+        "snippets": [
+            {
+                "verilog_code": """
+        assign data_int = ld_i ? ld_val_i : data_o + 1'b1;
+            """,
             },
         ],
     }
