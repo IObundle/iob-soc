0587c3897412c680f26836ba66ca821893e7225d
Martim Esteves <fimaales@gmail.com>
2024-08-14 13:31:35 +0100

    module rom generated with py2hwsw

--- lib/hardware/modules/rom/iob_rom_sp/iob_rom_sp.py
+++ lib/hardware/modules/rom/iob_rom_sp/iob_rom_sp.py
@@ -3,12 +3,11 @@ def setup(py_params_dict):
         "original_name": "iob_rom_sp",
         "name": "iob_rom_sp",
         "version": "0.1",
-        "generate_hw": False,
         "confs": [
             {
                 "name": "HEXFILE",
                 "type": "P",
-                "val": "none",
+                "val": '"none"',
                 "min": "NA",
                 "max": "NA",
                 "descr": "Name of file to load into RAM",
@@ -29,6 +28,14 @@ def setup(py_params_dict):
                 "max": "NA",
                 "descr": "Data bus width",
             },
+            {
+                "name": "MEM_INIT_FILE_INT",
+                "type": "F",
+                "val": "HEXFILE",
+                "min": "NA",
+                "max": "NA",
+                "descr": "",
+            },
         ],
         "ports": [
             {
@@ -46,6 +53,21 @@ def setup(py_params_dict):
                     {"name": "addr", "width": "ADDR_W", "direction": "input"},
                     {"name": "r_data", "width": "DATA_W", "direction": "output"},
                 ],
+            },
+        ],
+        "snippets": [
+            {
+                "verilog_code": """
+            // Declare the ROM
+   reg [DATA_W-1:0] rom[(2**ADDR_W)-1:0];
+
+   // Initialize the ROM
+   initial if ( MEM_INIT_FILE_INT != "none") $readmemh( MEM_INIT_FILE_INT, rom, 0, (2 ** ADDR_W) - 1);
+
+   // Operate the ROM
+   always @(posedge clk_i) if (r_en_i) 
+   r_data_o <= rom[addr_i];
+            """,
             },
         ],
     }
