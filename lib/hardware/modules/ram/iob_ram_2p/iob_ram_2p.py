984b7ac082a49967254ca12769fc650dc0bf0904
Martim Esteves <fimaales@gmail.com>
2024-08-20 18:00:06 +0100

    module iob_ram_2p generated with py2hwsw

--- lib/hardware/modules/ram/iob_ram_2p/iob_ram_2p.py
+++ lib/hardware/modules/ram/iob_ram_2p/iob_ram_2p.py
@@ -3,7 +3,52 @@ def setup(py_params_dict):
         "original_name": "iob_ram_2p",
         "name": "iob_ram_2p",
         "version": "0.1",
-        "generate_hw": False,
-    }
+        "confs": [
+            {
+                "name": "HEXFILE",
+                "type": "P",
+                "val": '"none"',
+                "min": "0",
+                "max": "NA",
+                "descr": "",
+            },
+            {
+                "name": "DATA_W",
+                "type": "P",
+                "val": "0",
+                "min": "0",
+                "max": "NA",
+                "descr": "",
+            },
+            {
+                "name": "ADDR_W",
+                "type": "P",
+                "val": "0",
+                "min": "0",
+                "max": "NA",
+                "descr": "",
+            },
+            {
+                "name": "MEM_INIT_FILE_INT",
+                "type": "F",
+                "val": "HEXFILE",
+                "min": "0",
+                "max": "NA",
+                "descr": "",
+            },
+           
+        ],
+        "ports": [
+            {
+                "name": "w_en_i",
+                "descr": "Input port",
+                "signals": [
+                    {"name": "w_en", "width": 1, "direction": "input"},
+                ],
+            },
+            
+        ],
+
+}
 
     return attributes_dict
