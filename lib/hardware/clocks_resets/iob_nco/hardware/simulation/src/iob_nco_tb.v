b168909edfa51a89f579d686e869f5f3fdb32bf7
PMiranda <pmiranda.iobundle@gmail.com>
2024-08-21 11:28:50 +0100

    fix(nco): move iob_nco_sync module, nco tb - move iob_nco_sync as a iob_nco module - use include in nco tesbench

--- lib/hardware/clocks_resets/iob_nco/hardware/simulation/src/iob_nco_tb.v
+++ lib/hardware/clocks_resets/iob_nco/hardware/simulation/src/iob_nco_tb.v
@@ -80,13 +80,7 @@ module iob_nco_tb;
 
   iob_nco nco (
       `include "iob_nco_clk_en_rst_s_portmap.vs"
-      .iob_valid_i(iob_valid_i),
-      .iob_addr_i(iob_addr_i),
-      .iob_wdata_i(iob_wdata_i),
-      .iob_wstrb_i(iob_wstrb_i),
-      .iob_rdata_o(iob_rdata_o),
-      .iob_ready_o(iob_ready_o),
-      .iob_rvalid_o(iob_rvalid_o),
+      `include "iob_nco_iob_s_s_portmap.vs"
       .clk_in_i(clk),
       .clk_out_o(clk_out)
   );
