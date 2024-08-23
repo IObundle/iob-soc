e132467025b61c86f06d00dcd7e0bddc32a9b277
PMiranda <pmiranda.iobundle@gmail.com>
2024-08-23 18:47:03 +0100

    feat(ram_2p): implement iob_ram_2p - implement iob_ram_2p using iob_ram_sp     - write_first by default

---
+++ lib/hardware/memories/ram/iob_ram_2p/iob_ram_2p.py
@@ -0,0 +1,15 @@
+def setup(py_params_dict):
+    attributes_dict = {
+        "original_name": "iob_ram_2p",
+        "name": "iob_ram_2p",
+        "version": "0.1",
+        "generate_hw": False,
+        "blocks": [
+            {
+                "core_name": "iob_ram_sp",
+                "instance_name": "iob_ram_sp_inst",
+            },
+        ],
+    }
+
+    return attributes_dict
