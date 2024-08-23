8c2c8a274e2874ebb4b23c113ba5de0a9a048133
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-08-23 01:06:00 +0100

    fix(lib): Fix lib modules. These modules are already fixed/outdated in if_gen2, but this commit is needed for ghactions tests to pass.

--- submodules/LIB/hardware/modules/iob_inv/hardware/src/iob_inv.v
+++ submodules/LIB/hardware/modules/iob_inv/hardware/src/iob_inv.v
@@ -1,15 +1,12 @@
 `timescale 1ns / 1ps
 
 
-module iob_inv
-  #(
-    parameter W = 21
+module iob_inv #(
+   parameter W = 21
 ) (
-   input [W-1:0] in_i,
-   output [W-1:0]  out_o
+   input  [W-1:0] in_i,
+   output [W-1:0] out_o
 );
-
-   wire [W-1:0]  out_o;
 
    assign out_o = ~in_i;
 
