9988c11b46793c7ebf3b53eccd7aaccb40ca7fe3
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-09-09 16:52:10 +0100

    refactor(rom): Apply suggestion from https://github.com/IObundle/iob-soc/pull/938

--- lib/hardware/memories/rom/iob_rom_2p/hardware/src/iob_rom_2p.v
+++ lib/hardware/memories/rom/iob_rom_2p/hardware/src/iob_rom_2p.v
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 
 module iob_rom_2p #(
-   parameter HEXFILE  = "none",
-   parameter DATA_W   = 0,
-   parameter ADDR_W   = 0
+   parameter HEXFILE = "none",
+   parameter DATA_W  = 0,
+   parameter ADDR_W  = 0
 ) (
    input clk_i,
 
@@ -20,14 +20,14 @@ module iob_rom_2p #(
    output [DATA_W-1:0] r_data_o
 );
 
-   wire en_int;
+   wire              en_int;
    wire [ADDR_W-1:0] addr_int;
 
    // Internal Single Port ROM
    iob_rom_sp #(
       .HEXFILE(HEXFILE),
-      .DATA_W(DATA_W),
-      .ADDR_W(ADDR_W)
+      .DATA_W (DATA_W),
+      .ADDR_W (ADDR_W)
    ) iob_rom_sp_inst (
       .clk_i   (clk_i),
       .r_en_i  (en_int),
@@ -35,9 +35,9 @@ module iob_rom_2p #(
       .r_data_o(r_data_o)
    );
 
-   assign en_int = r1_en_i | r2_en_i;
-   assign addr_int = r1_en_i ? r1_addr_i : r2_addr_i;
-   assign r1_ready_o = r1_en_i;
-   assign r2_ready_o = r1_en_i ? 0 : r2_en_i;
+   assign en_int     = r1_en_i | r2_en_i;
+   assign addr_int   = r1_en_i ? r1_addr_i : r2_addr_i;
+   assign r1_ready_o = 1'b1;
+   assign r2_ready_o = ~r1_en_i;
 
 endmodule
