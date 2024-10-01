baaf041273b96180155d33b526e53a4b0c5c3b02
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-10-01 17:52:52 +0100

    feat(iob-soc): Add uart and timer blocks. These override ones from iob_system.
    TODO: Find a way of automating cbus connections of child module (iob-soc).
    These connections are normally automated by iob-system utils, but those
    scripts don't have access to the child module attributes.

--- iob_soc.py
+++ iob_soc.py
@@ -4,8 +4,45 @@ def setup(py_params_dict):
         "name": "iob_soc",
         "parent": {"core_name": "iob_system", **py_params_dict},
         "version": "0.1",
-        "confs": [],
-        "ports": [],
+        "ports": [
+            {
+                "name": "rs232_m",
+                "descr": "iob-system uart interface",
+                "interface": {
+                    "type": "rs232",
+                },
+            },
+        ],
+        "blocks": [
+            {
+                "core_name": "iob_uart",
+                "instance_name": "UART0",
+                "instance_description": "UART peripheral",
+                "is_peripheral": True,
+                "parameters": {},
+                "connect": {
+                    "clk_en_rst_s": "clk_en_rst_s",
+                    # TODO: Cbus should be connected automatically
+                    # The iob_system blocks are handled by iob_system_utils.py, but
+                    # the iob_system scripts do not have access to info in iob_soc.py,
+                    # nor do they have permission to modify it (even if iob_system receives info about child module, it can't modify its dictionary).
+                    "cbus_s": "uart0_cbus",
+                    "rs232_m": "rs232_m",
+                },
+            },
+            {
+                "core_name": "iob_timer",
+                "instance_name": "TIMER0",
+                "instance_description": "Timer peripheral",
+                "is_peripheral": True,
+                "parameters": {},
+                "connect": {
+                    "clk_en_rst_s": "clk_en_rst_s",
+                    # TODO: Cbus should be connected automatically
+                    "cbus_s": "timer0_cbus",
+                },
+            },
+        ],
     }
 
     return attributes_dict
