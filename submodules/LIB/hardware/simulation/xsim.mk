7275b96254f09790748a49747756ab6e8ce5640d
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2024-10-07 18:58:50 +0100

    add support for xsim

---
+++ submodules/LIB/hardware/simulation/xsim.mk
@@ -0,0 +1,44 @@
+
+SFLAGS = --runall
+EFLAGS = --snapshot
+
+VFLAGS+= -sv 
+
+ifneq ($(wildcard ../src),)
+VFLAGS+=-i . -i ../src
+endif
+
+ifneq ($(wildcard src),)
+VFLAGS+=-i ./src
+endif
+
+ifneq ($(wildcard hardware/src),)
+VFLAGS+=-i hardware/src
+endif
+
+ifeq ($(VCD),1)
+VFLAGS+=-d VCD
+endif
+
+ifeq ($(SYN),1)
+VFLAGS+=-d SYN
+endif
+
+xvlog.log: $(VHDR) $(VSRC) $(HEX)
+	xvlog $(VFLAGS) $(VSRC)
+
+xelab.log : xvlog.log 
+	xelab $(EFLAGS) worklib $(NAME)_tb
+
+comp: xelab.log
+
+exec: comp
+	sync && sleep 2 && xsim $(SFLAGS) worklib
+
+clean: gen-clean
+	@rm -f xelab.log  xsim.log  xvlog.log webtalk* xelab.pb xvlog.pb xsim.jou
+
+very-clean: clean
+
+.PHONY: comp exec clean very-clean
+
