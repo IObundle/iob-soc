4f610a754a06ed4b941a129deb9dfbee6a1a0018
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-10-10 04:15:55 +0100

    feat(nix): Improve default.nix to import the one from py2hwsw. Remove unused nix scripts from lib.

--- default.nix
+++ default.nix
@@ -3,4 +3,46 @@
 # SPDX-License-Identifier: MIT
 
 { pkgs ? import <nixpkgs> {} }:
-import lib/scripts/default.nix { inherit pkgs; }
+
+let
+  py2hwsw_commit = "fbadb1502178d90cf1e009e04b291e80c773d401"; # Replace with the desired commit.
+  py2hwsw_sha256 = "fSLu2BoSzkHTlwQjheWFk75tGKDJSJrJ/xAIxtyahqc="; # Replace with the actual SHA256 hash.
+  # Get local py2hwsw path from `PY2HWSW_PATH` env variable
+  py2hwswPath = builtins.getEnv "PY2HWSW_PATH";
+
+  # For debug
+  disable_py2_build = 0;
+
+  py2hwsw = 
+    if disable_py2_build == 0 then
+      pkgs.python3.pkgs.buildPythonPackage rec {
+        pname = "py2hwsw";
+        version = py2hwsw_commit;
+        src =
+          if py2hwswPath != "" then
+            pkgs.lib.cleanSource py2hwswPath
+          else
+            (pkgs.fetchFromGitHub {
+              owner = "IObundle";
+              repo = "py2hwsw";
+              rev = py2hwsw_commit;
+              sha256 = py2hwsw_sha256;
+              fetchSubmodules = true;
+            }).overrideAttrs (_: {
+              GIT_CONFIG_COUNT = 1;
+              GIT_CONFIG_KEY_0 = "url.https://github.com/.insteadOf";
+              GIT_CONFIG_VALUE_0 = "git@github.com:";
+            });
+        # Add any necessary dependencies here.
+        #propagatedBuildInputs = [ pkgs.python38Packages.someDependency ];
+      }
+    else
+      null;
+
+
+in
+
+if disable_py2_build == 0 then
+  import "${py2hwsw}/lib/python${builtins.substring 0 4 pkgs.python3.version}/site-packages/py2hwsw/lib/scripts/default.nix" { inherit pkgs; py2hwsw_pkg = py2hwsw; }
+else
+  import "${py2hwswPath}/py2hwsw/lib/scripts/default.nix" { inherit pkgs; py2hwsw_pkg = py2hwsw; }
