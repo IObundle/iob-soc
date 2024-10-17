ac7526a28dd746c0b5396f1c3c6b8ddb8717f206
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-10-17 16:56:46 +0100

    fix(cyclonev): Update py2hwsw version

--- default.nix
+++ default.nix
@@ -5,8 +5,8 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  py2hwsw_commit = "e3241dffdaea5ab4b9726d3cf8836e2d5d0bdb7b"; # Replace with the desired commit.
-  py2hwsw_sha256 = "m0lu+f2Tfo//ZWqqH7ZOqQm9Fi2I9uagTBRCiRmR4OU="; # Replace with the actual SHA256 hash.
+  py2hwsw_commit = "f41d2673e9cff6d4e86af4696338c2cb5d88e111"; # Replace with the desired commit.
+  py2hwsw_sha256 = "8tACTO1yWJYE+2bQCcksDqUHEKldzabDSnIWbKt16bY="; # Replace with the actual SHA256 hash.
   # Get local py2hwsw path from `PY2HWSW_PATH` env variable
   py2hwswPath = builtins.getEnv "PY2HWSW_PATH";
 
