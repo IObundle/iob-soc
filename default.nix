04498d7eb3f09a12d495f6a5e0199650f7abf591
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2024-10-20 23:34:43 +0100

    update py2hwsw

--- default.nix
+++ default.nix
@@ -5,8 +5,8 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  py2hwsw_commit = "77838b07b9dc285524aaf7b998f94da80faa8c09"; # Replace with the desired commit.
-  py2hwsw_sha256 = "sha256-NSOqvnL7Ux7D7UHy3KpSejnkcBwO/NciUYUU8MO9+Qs="; # Replace with the actual SHA256 hash.
+  py2hwsw_commit = "764b4e7c7d8941f5693e9ed5ea925ab5ebe5c80c"; # Replace with the desired commit.
+  py2hwsw_sha256 = "sha256-2A4cmvB+TCXX2p6Z+nvz55rsMLPjCaEbUPbH/KnSWzU="; # Replace with the actual SHA256 hash.
   # Get local py2hwsw path from `PY2HWSW_PATH` env variable
   py2hwswPath = builtins.getEnv "PY2HWSW_PATH";
 
