769bc029e3ae7bed72e14f51ffead5531b7a38b6
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-11-14 00:40:27 +0000

    docs(iob_soc): Add comments to iob_soc.py

--- iob_soc.py
+++ iob_soc.py
@@ -8,12 +8,17 @@ def setup(py_params_dict):
     core_dict = {
         "version": "0.1",
         "parent": {
+            # IOb-SoC is a child core of iob_system: https://github.com/IObundle/py2hwsw/tree/main/py2hwsw/lib/hardware/iob_system
+            # IOb-SoC will inherit all attributes/files from the iob_system core.
             "core_name": "iob_system",
+            # Every parameter in the lines below will be passed to the iob_system parent core.
             **py_params_dict,
             "system_attributes": {
+                # Every attribute in this dictionary will override/append to the ones of the iob_system parent core.
                 "board_list": ["aes_ku040_db_g", "cyclonev_gt_dk", "zybo_z7"],
                 "ports": [
                     {
+                        # Add new rs232 port for uart
                         "name": "rs232_m",
                         "descr": "iob-system uart interface",
                         "signals": {
@@ -23,6 +28,7 @@ def setup(py_params_dict):
                 ],
                 "blocks": [
                     {
+                        # Instantiate a UART core from: https://github.com/IObundle/py2hwsw/tree/main/py2hwsw/lib/hardware/iob_uart
                         "core_name": "iob_uart",
                         "instance_name": "UART0",
                         "instance_description": "UART peripheral",
@@ -35,6 +41,7 @@ def setup(py_params_dict):
                         },
                     },
                     {
+                        # Instantiate a TIMER core from: https://github.com/IObundle/py2hwsw/tree/main/py2hwsw/lib/hardware/iob_timer
                         "core_name": "iob_timer",
                         "instance_name": "TIMER0",
                         "instance_description": "Timer peripheral",
