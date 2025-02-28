7f7f789a83ee1df9b02d6538348a3b081ab047d4
PMiranda <pmiranda.iobundle@gmail.com>
2025-02-28 13:34:31 +0000

    fix(coverage): update modules to improve coverage - update modules to improve coverage: - accumulators now have `INCR_W` parameter to set increment width,   different from `DATA_W` - update modcnt to remove internal `ld_val` always 0. Keep same   functionality

--- submodules/LIB/hardware/modules/acc/iob_acc_ld/hardware/src/iob_acc_ld.v
+++ submodules/LIB/hardware/modules/acc/iob_acc_ld/hardware/src/iob_acc_ld.v
@@ -2,6 +2,7 @@
 
 module iob_acc_ld #(
    parameter DATA_W  = 21,
+   parameter INCR_W  = DATA_W,
    parameter RST_VAL = {DATA_W{1'b0}}
 ) (
 `include "clk_en_rst_s_port.vs"
@@ -9,7 +10,7 @@ module iob_acc_ld #(
    input               en_i,
    input               ld_i,
    input [DATA_W-1:0]  ld_val_i,
-   input [DATA_W-1:0]  incr_i,
+   input [INCR_W-1:0]  incr_i,
    output [DATA_W-1:0] data_o,
    output [DATA_W:0]   data_nxt_o
    );
