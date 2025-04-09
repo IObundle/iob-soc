75a703cae5aed14c8fdf6beb85841b840b37d49e
Artur Nóbrega <arturnobrega1999@gmail.com>
2025-04-10 00:12:17 +0100

    feat(py2): Update sim_build.mk according to new py2hwsw version

--- hardware/simulation/sim_build.mk
+++ hardware/simulation/sim_build.mk
@@ -10,21 +10,11 @@ HEX+=iob_soc_bootrom.hex iob_soc_firmware.hex
 ROOT_DIR :=../..
 include $(ROOT_DIR)/software/sw_build.mk
 
-VTOP:=iob_soc_tb
-
-# SOURCES
-ifeq ($(SIMULATOR),verilator)
-
-VSRC+=./src/iob_uart_csrs.c
-
 ifeq ($(USE_ETHERNET),1)
 VSRC+=./src/iob_eth_csrs_emb_verilator.c ./src/iob_eth_driver_tb.cpp
 endif
 
-# verilator top module
-VTOP:=iob_soc_sim
-
-endif
+CSRS = ../../software/src/iob_uart_csrs.c
 
 CONSOLE_CMD ?=rm -f soc2cnsl cnsl2soc; ../../scripts/console.py -L
 
