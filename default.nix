7cf6bc55fe04c5a5871e55bd5bf64ddbd00cd39a
Artur Nóbrega <arturnobrega1999@gmail.com>
2025-04-11 01:00:25 +0100

    feat(py2hwsw): Update py2hwsw version

--- default.nix
+++ default.nix
@@ -5,8 +5,8 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  py2hwsw_commit = "95138d69ee260e1339c720826ddfae890df9757c"; # Replace with the desired commit.
-  py2hwsw_sha256 = "5haGl0rhu+7V14E18ldaQlB0N1RVLQ2T4BFqqqP+Wlg="; # Replace with the actual SHA256 hash.
+  py2hwsw_commit = "f593e52e523189aa41839eab29602dd3321d1d6b"; # Replace with the desired commit.
+  py2hwsw_sha256 = "1kyhJIHXD6hmJETwtw4QO7DL4Xn7J+pSKH+lbKmcimY="; # Replace with the actual SHA256 hash.
   # Get local py2hwsw root from `PY2HWSW_ROOT` env variable
   py2hwswRoot = builtins.getEnv "PY2HWSW_ROOT";
 
