613821fe4e365e66a8e5bc39a853aefdb97d879d
Artur Nóbrega <arturnobrega1999@gmail.com>
2025-07-31 15:31:29 +0100

    fix(nix): Downgrade nixpkgs version to match one from py2hwsw.
    Seems like most recent nixpkgs is not compatible with py2hwsw's version of it.
    Running iob-soc setup with newest nixpkgs would throw the following error:
    '''
    Original error was: No module named 'numpy.core._multiarray_umath'
    '''

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/24.05.tar.gz") {} }:
 
 let
   py2hwsw_commit = "6e36a33994694c76c4ef2636fd2515214311bd92"; # Replace with the desired commit.
